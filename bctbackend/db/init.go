77b536e66f94a92f0df978d9c13f3a8cc6f1b5d4
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-11-01 17:14:43 +0100

    item tests

--- bctbackend/db/init.go
+++ bctbackend/db/init.go
@@ -3,6 +3,7 @@ package db
 import (
 	models "bctbackend/db/models"
 	"database/sql"
+	"fmt"
 )
 
 func InitializeDatabase(db *sql.DB) {
@@ -61,7 +62,7 @@ func createItemTable(db *sql.DB) {
 			item_id             INTEGER NOT NULL,
 			timestamp           INTEGER NOT NULL,
 			description         TEXT NOT NULL,
-			price_in_cents      INTEGER NOT NULL,
+			price_in_cents      INTEGER NOT NULL CHECK (price_in_cents > 0),
 			item_category_id    INTEGER NOT NULL,
 			seller_id           INTEGER NOT NULL,
 			donation            BOOLEAN NOT NULL,
@@ -106,7 +107,7 @@ func populateTables(db *sql.DB) {
 }
 
 func populateRoleTable(db *sql.DB) {
-	db.Exec(`
+	_, err := db.Exec(`
 			INSERT INTO roles (role_id, name)
 			VALUES
 				($1, 'admin'),
@@ -117,23 +118,44 @@ func populateRoleTable(db *sql.DB) {
 		models.SellerRoleId,
 		models.CashierRoleId,
 	)
+
+	if err != nil {
+		panic(fmt.Errorf("failed to populate roles: %v", err))
+	}
 }
 
 func populateItemCategoryTable(db *sql.DB) {
-	db.Exec(`
-		INSERT INTO item_categories (name)
+	_, err := db.Exec(`
+		INSERT INTO item_categories (item_category_id, name)
 		VALUES
-			('Clothing 0-3 mos (50-56)'),
-			('Clothing 3-6 mos (56-62)'),
-			('Clothing 6-12 mos (68-80)'),
-			('Clothing 12-24 mos (86-92)'),
-			('Clothing 2-3 yrs (92-98)'),
-			('Clothing 4-6 yrs (104-116)'),
-			('Clothing 7-8 yrs (122-128)'),
-			('Clothing 9-10 yrs (128-140)'),
-			('Clothing 11-12 yrs (140-152)'),
-			('Shoes (infant to 12 yrs)'),
-			('Toys'),
-			('Baby/Child Equipment')
-	`)
+			($1, 'Clothing 0-3 mos (50-56)'),
+			($2, 'Clothing 3-6 mos (56-62)'),
+			($3, 'Clothing 6-12 mos (68-80)'),
+			($4, 'Clothing 12-24 mos (86-92)'),
+			($5, 'Clothing 2-3 yrs (92-98)'),
+			($6, 'Clothing 4-6 yrs (104-116)'),
+			($7, 'Clothing 7-8 yrs (122-128)'),
+			($8, 'Clothing 9-10 yrs (128-140)'),
+			($9, 'Clothing 11-12 yrs (140-152)'),
+			($10, 'Shoes (infant to 12 yrs)'),
+			($11, 'Toys'),
+			($12, 'Baby/Child Equipment')
+		`,
+		models.Clothing50_56,
+		models.Clothing56_62,
+		models.Clothing68_80,
+		models.Clothing86_92,
+		models.Clothing92_98,
+		models.Clothing104_116,
+		models.Clothing122_128,
+		models.Clothing128_140,
+		models.Clothing140_152,
+		models.Shoes,
+		models.Toys,
+		models.BabyChildEquipment,
+	)
+
+	if err != nil {
+		panic(fmt.Errorf("failed to populate item categories: %v", err))
+	}
 }
