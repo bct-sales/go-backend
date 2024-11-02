c31cef21f137aee1f2fcaeb221773ed6cbacc375
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-11-02 10:00:40 +0100

    TestRoleToString

--- bctbackend/database/models/role_test.go
+++ bctbackend/database/models/role_test.go
@@ -37,3 +37,35 @@ func TestRoleParsing(t *testing.T) {
 		assert.Error(t, err)
 	})
 }
+
+func TestRoleToString(t *testing.T) {
+	t.Run("admin", func(t *testing.T) {
+		roleName, err := RoleToString(AdminRoleId)
+
+		if assert.NoError(t, err) {
+			assert.Equal(t, AdminName, roleName)
+		}
+	})
+
+	t.Run("seller", func(t *testing.T) {
+		roleName, err := RoleToString(SellerRoleId)
+
+		if assert.NoError(t, err) {
+			assert.Equal(t, SellerName, roleName)
+		}
+	})
+
+	t.Run("cashier", func(t *testing.T) {
+		roleName, err := RoleToString(CashierRoleId)
+
+		if assert.NoError(t, err) {
+			assert.Equal(t, CashierName, roleName)
+		}
+	})
+
+	t.Run("unknown", func(t *testing.T) {
+		_, err := RoleToString(4)
+
+		assert.Error(t, err)
+	})
+}
