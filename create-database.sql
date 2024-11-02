358be6dd0ab8aa5f436743a4bf9337a9cec171e5
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-11-02 22:47:20 +0100

    updated create-database.sql

--- create-database.sql
+++ create-database.sql
@@ -28,7 +28,7 @@ CREATE TABLE items (
     item_id             INTEGER NOT NULL,
     timestamp           INTEGER NOT NULL,
     description         TEXT NOT NULL,
-    price_in_cents      INTEGER NOT NULL,
+    price_in_cents      INTEGER NOT NULL CHECK (price_in_cents > 0),
     item_category_id    INTEGER NOT NULL,
     seller_id           INTEGER NOT NULL,
     donation            BOOLEAN NOT NULL,
