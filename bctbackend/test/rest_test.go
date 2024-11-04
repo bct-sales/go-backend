6407627595d9789cfea76dc596b66ab828378845
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-11-05 00:01:34 +0100

    TestListItems

--- bctbackend/test/rest_test.go
+++ bctbackend/test/rest_test.go
@@ -6,25 +6,55 @@ import (
 	"testing"
 
 	models "bctbackend/database/models"
+	"bctbackend/database/queries"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestListItems(t *testing.T) {
-	db, router := createRestRouter()
-	w := httptest.NewRecorder()
-	defer db.Close()
+	t.Run("No items", func(t *testing.T) {
+		db, router := createRestRouter()
+		writer := httptest.NewRecorder()
+		defer db.Close()
 
-	// Create an example user for testing
-	request, err := http.NewRequest("GET", "/api/v1/items", nil)
+		request, err := http.NewRequest("GET", "/api/v1/items", nil)
 
-	if assert.NoError(t, err) {
-		router.ServeHTTP(w, request)
+		if assert.NoError(t, err) {
+			router.ServeHTTP(writer, request)
 
-		assert.Equal(t, 200, w.Code)
+			assert.Equal(t, 200, writer.Code)
 
-		expected := []models.Item{}
-		actual := fromJson[[]models.Item](w.Body.String())
-		assert.Equal(t, expected, *actual)
-	}
+			expected := []models.Item{}
+			actual := fromJson[[]models.Item](writer.Body.String())
+			assert.Equal(t, expected, *actual)
+		}
+	})
+
+	t.Run("One item", func(t *testing.T) {
+		db, router := createRestRouter()
+		writer := httptest.NewRecorder()
+		defer db.Close()
+
+		sellerId := addTestSeller(db)
+		item := models.NewItem(0, 100, "test item", 1000, models.Shoes, sellerId, false, false)
+		itemId, err := queries.AddItem(db, item.Timestamp, item.Description, item.PriceInCents, item.CategoryId, item.SellerId, item.Donation, item.Charity)
+
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		item.ItemId = itemId
+
+		request, err := http.NewRequest("GET", "/api/v1/items", nil)
+
+		if assert.NoError(t, err) {
+			router.ServeHTTP(writer, request)
+
+			assert.Equal(t, 200, writer.Code)
+
+			expected := []models.Item{*item}
+			actual := fromJson[[]models.Item](writer.Body.String())
+			assert.Equal(t, expected, *actual)
+		}
+	})
 }
