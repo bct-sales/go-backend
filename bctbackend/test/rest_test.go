5fdba8cc50320f870353c1d68208c9376ff977a0
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-11-05 16:25:57 +0100

    TestListItems

--- bctbackend/test/rest_test.go
+++ bctbackend/test/rest_test.go
@@ -57,4 +57,38 @@ func TestListItems(t *testing.T) {
 			assert.Equal(t, expected, *actual)
 		}
 	})
+
+	t.Run("Two item", func(t *testing.T) {
+		db, router := createRestRouter()
+		writer := httptest.NewRecorder()
+		defer db.Close()
+
+		sellerId := addTestSeller(db)
+		item1 := models.NewItem(0, 100, "test item", 1000, models.Shoes, sellerId, false, false)
+		item2 := models.NewItem(0, 100, "test item", 1000, models.Shoes, sellerId, false, false)
+
+		itemId, err := queries.AddItem(db, item1.Timestamp, item1.Description, item1.PriceInCents, item1.CategoryId, item1.SellerId, item1.Donation, item1.Charity)
+		if !assert.NoError(t, err) {
+			return
+		}
+		item1.ItemId = itemId
+
+		itemId, err = queries.AddItem(db, item2.Timestamp, item2.Description, item2.PriceInCents, item2.CategoryId, item2.SellerId, item2.Donation, item2.Charity)
+		if !assert.NoError(t, err) {
+			return
+		}
+		item2.ItemId = itemId
+
+		request, err := http.NewRequest("GET", "/api/v1/items", nil)
+
+		if assert.NoError(t, err) {
+			router.ServeHTTP(writer, request)
+
+			assert.Equal(t, 200, writer.Code)
+
+			expected := []models.Item{*item1, *item2}
+			actual := fromJson[[]models.Item](writer.Body.String())
+			assert.Equal(t, expected, *actual)
+		}
+	})
 }
