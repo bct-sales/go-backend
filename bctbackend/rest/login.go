d2e32cde76819f91515ea6ea28b9508a9b6a7a11
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-11-09 17:34:28 +0100

    file reorganization

---
+++ bctbackend/rest/login.go
@@ -0,0 +1,58 @@
+package rest
+
+import (
+	"bctbackend/database/models"
+	"bctbackend/database/queries"
+	"bctbackend/security"
+	"database/sql"
+	"net/http"
+
+	_ "bctbackend/docs"
+
+	"github.com/gin-gonic/gin"
+)
+
+type loginRequest struct {
+	Username string `form:"username" binding:"required" json:"username"`
+	Password string `form:"password" binding:"required" json:"password"`
+}
+
+// @Summary Login
+// @Description Login
+// @Success 200 {object} string
+// @Router /login [post]
+// @param username formData string true "username"
+// @param password formData string true "password"
+func login(context *gin.Context, db *sql.DB) {
+	var loginRequest loginRequest
+
+	if err := context.ShouldBind(&loginRequest); err != nil {
+		context.String(http.StatusBadRequest, "Bad request: %v", err)
+		return
+	}
+
+	userId, err := models.ParseId(loginRequest.Username)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid user ID"})
+		return
+	}
+
+	password := loginRequest.Password
+
+	if err := queries.AuthenticateUser(db, userId, password); err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Login failed"})
+		return
+	}
+
+	sessionId, err := queries.AddSession(db, userId)
+
+	if err != nil {
+		context.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	ensureSecure := false // TODO: set to true when using HTTPS
+	context.SetCookie(security.SessionCookieName, sessionId, security.SessionDurationInSeconds, "/", "localhost", ensureSecure, true)
+	context.JSON(http.StatusOK, gin.H{"message": "Login successful"})
+}
--- bctbackend/rest/main.go
+++ bctbackend/rest/main.go
@@ -14,51 +14,6 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-type LoginRequest struct {
-	Username string `form:"username" binding:"required" json:"username"`
-	Password string `form:"password" binding:"required" json:"password"`
-}
-
-// @Summary Login
-// @Description Login
-// @Success 200 {object} string
-// @Router /login [post]
-// @param username formData string true "username"
-// @param password formData string true "password"
-func login(context *gin.Context, db *sql.DB) {
-	var loginRequest LoginRequest
-
-	if err := context.ShouldBind(&loginRequest); err != nil {
-		context.String(http.StatusBadRequest, "Bad request: %v", err)
-		return
-	}
-
-	userId, err := models.ParseId(loginRequest.Username)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid user ID"})
-		return
-	}
-
-	password := loginRequest.Password
-
-	if err := queries.AuthenticateUser(db, userId, password); err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Login failed"})
-		return
-	}
-
-	sessionId, err := queries.AddSession(db, userId)
-
-	if err != nil {
-		context.AbortWithStatus(http.StatusInternalServerError)
-		return
-	}
-
-	ensureSecure := false // TODO: set to true when using HTTPS
-	context.SetCookie(security.SessionCookieName, sessionId, security.SessionDurationInSeconds, "/", "localhost", ensureSecure, true)
-	context.JSON(http.StatusOK, gin.H{"message": "Login successful"})
-}
-
 // @title           BCT Sales
 // @version         1.0
 // @description     BCT Sales REST API
