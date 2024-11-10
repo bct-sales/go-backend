7fc11427386d821896dc7d74fd123946e76c0cff
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-11-10 10:13:43 +0100

    updated TestAddSellerItem

--- bctbackend/test/rest/seller_items_test.go
+++ bctbackend/test/rest/seller_items_test.go
@@ -2,6 +2,7 @@ package rest
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -48,51 +49,64 @@ func TestListSellerItems(t *testing.T) {
 
 func TestAddSellerItem(t *testing.T) {
 	for _, sellerId := range []models.Id{models.NewId(1), models.NewId(2), models.NewId(100)} {
-		db, router := createRestRouter()
-		writer := httptest.NewRecorder()
-		defer db.Close()
+		for _, price := range []models.MoneyInCents{1, 100, 10000} {
+			for _, description := range []string{"Xyz", "Test Description"} {
+				for _, categoryId := range models.Categories() {
+					for _, donation := range []bool{true, false} {
+						for _, charity := range []bool{true, false} {
+							t.Run(fmt.Sprintf("sellerId=%d price=%d description=%s categoryId=%d donation=%t charity=%t", sellerId, price, description, categoryId, donation, charity), func(t *testing.T) {
+								db, router := createRestRouter()
+								writer := httptest.NewRecorder()
+								defer db.Close()
 
-		seller := addTestSellerWithId(db, sellerId)
-		sessionId := addTestSession(db, seller.UserId)
+								seller := addTestSellerWithId(db, sellerId)
+								sessionId := addTestSession(db, seller.UserId)
 
-		price := models.MoneyInCents(100)
-		donation := false
-		description := "Test Description"
-		categoryId := models.Clothing104_116
-		charity := false
-		payload := restapi.AddSellerItemPayload{
-			Price:       price,
-			Description: description,
-			CategoryId:  categoryId,
-			Donation:    &donation,
-			Charity:     &charity,
-		}
+								payload := restapi.AddSellerItemPayload{
+									Price:       price,
+									Description: description,
+									CategoryId:  categoryId,
+									Donation:    &donation,
+									Charity:     &charity,
+								}
 
-		payloadJson := toJson(payload)
+								payloadJson := toJson(payload)
 
-		url := fmt.Sprintf("/api/v1/sellers/%d/items", seller.UserId)
-		request, err := http.NewRequest("POST", url, strings.NewReader(payloadJson))
-		request.Header.Set("Content-Type", "application/json")
-		request.AddCookie(createCookie(sessionId))
+								url := fmt.Sprintf("/api/v1/sellers/%d/items", seller.UserId)
+								request, err := http.NewRequest("POST", url, strings.NewReader(payloadJson))
 
-		if assert.NoError(t, err) {
-			router.ServeHTTP(writer, request)
-			assert.Equal(t, http.StatusCreated, writer.Code)
+								if assert.NoError(t, err) {
+									request.Header.Set("Content-Type", "application/json")
+									request.AddCookie(createCookie(sessionId))
 
-			response := fromJson[restapi.AddSellerItemResponse](writer.Body.String())
+									if assert.NoError(t, err) {
+										router.ServeHTTP(writer, request)
 
-			itemsInDatabase, err := queries.GetItems(db)
-			if assert.NoError(t, err) {
-				assert.Equal(t, 1, len(itemsInDatabase))
+										log.Println(writer.Body.String())
 
-				itemInDatabase := itemsInDatabase[0]
-				assert.Equal(t, response.ItemId, itemInDatabase.ItemId)
-				assert.Equal(t, seller.UserId, itemInDatabase.SellerId)
-				assert.Equal(t, price, itemInDatabase.PriceInCents)
-				assert.Equal(t, description, itemInDatabase.Description)
-				assert.Equal(t, categoryId, itemInDatabase.CategoryId)
-				assert.Equal(t, donation, itemInDatabase.Donation)
-				assert.Equal(t, charity, itemInDatabase.Charity)
+										if assert.Equal(t, http.StatusCreated, writer.Code) {
+											response := fromJson[restapi.AddSellerItemResponse](writer.Body.String())
+
+											itemsInDatabase, err := queries.GetItems(db)
+											if assert.NoError(t, err) {
+												if assert.Equal(t, 1, len(itemsInDatabase)) {
+													itemInDatabase := itemsInDatabase[0]
+													assert.Equal(t, response.ItemId, itemInDatabase.ItemId)
+													assert.Equal(t, seller.UserId, itemInDatabase.SellerId)
+													assert.Equal(t, price, itemInDatabase.PriceInCents)
+													assert.Equal(t, description, itemInDatabase.Description)
+													assert.Equal(t, categoryId, itemInDatabase.CategoryId)
+													assert.Equal(t, donation, itemInDatabase.Donation)
+													assert.Equal(t, charity, itemInDatabase.Charity)
+												}
+											}
+										}
+									}
+								}
+							})
+						}
+					}
+				}
 			}
 		}
 	}
