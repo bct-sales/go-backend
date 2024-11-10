c4b341e438816c9ed3e1a8da5d0430b58fd88aa8
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-11-10 14:30:51 +0100

    failing  add seller item test

--- bctbackend/test/rest/seller_items_test.go
+++ bctbackend/test/rest/seller_items_test.go
@@ -47,64 +47,111 @@ func TestListSellerItems(t *testing.T) {
 }
 
 func TestAddSellerItem(t *testing.T) {
-	for _, sellerId := range []models.Id{models.NewId(1), models.NewId(2), models.NewId(100)} {
-		for _, price := range []models.MoneyInCents{1, 100, 10000} {
-			for _, description := range []string{"Xyz", "Test Description"} {
-				for _, categoryId := range models.Categories() {
-					for _, donation := range []bool{true, false} {
-						for _, charity := range []bool{true, false} {
-							t.Run(fmt.Sprintf("sellerId=%d price=%d description=%s categoryId=%d donation=%t charity=%t", sellerId, price, description, categoryId, donation, charity), func(t *testing.T) {
-								db, router := createRestRouter()
-								writer := httptest.NewRecorder()
-								defer db.Close()
+	t.Run("Successful", func(t *testing.T) {
+		for _, sellerId := range []models.Id{models.NewId(1), models.NewId(2), models.NewId(100)} {
+			for _, price := range []models.MoneyInCents{1, 100, 10000} {
+				for _, description := range []string{"Xyz", "Test Description"} {
+					for _, categoryId := range models.Categories() {
+						for _, donation := range []bool{true, false} {
+							for _, charity := range []bool{true, false} {
+								t.Run(fmt.Sprintf("sellerId=%d price=%d description=%s categoryId=%d donation=%t charity=%t", sellerId, price, description, categoryId, donation, charity), func(t *testing.T) {
+									db, router := createRestRouter()
+									writer := httptest.NewRecorder()
+									defer db.Close()
 
-								seller := addTestSellerWithId(db, sellerId)
-								sessionId := addTestSession(db, seller.UserId)
+									seller := addTestSellerWithId(db, sellerId)
+									sessionId := addTestSession(db, seller.UserId)
 
-								payload := restapi.AddSellerItemPayload{
-									Price:       price,
-									Description: description,
-									CategoryId:  categoryId,
-									Donation:    &donation,
-									Charity:     &charity,
-								}
+									payload := restapi.AddSellerItemPayload{
+										Price:       price,
+										Description: description,
+										CategoryId:  categoryId,
+										Donation:    &donation,
+										Charity:     &charity,
+									}
 
-								payloadJson := toJson(payload)
+									payloadJson := toJson(payload)
 
-								url := fmt.Sprintf("/api/v1/sellers/%d/items", seller.UserId)
-								request, err := http.NewRequest("POST", url, strings.NewReader(payloadJson))
-
-								if assert.NoError(t, err) {
-									request.Header.Set("Content-Type", "application/json")
-									request.AddCookie(createCookie(sessionId))
+									url := fmt.Sprintf("/api/v1/sellers/%d/items", seller.UserId)
+									request, err := http.NewRequest("POST", url, strings.NewReader(payloadJson))
 
 									if assert.NoError(t, err) {
-										router.ServeHTTP(writer, request)
+										request.Header.Set("Content-Type", "application/json")
+										request.AddCookie(createCookie(sessionId))
 
-										if assert.Equal(t, http.StatusCreated, writer.Code) {
-											response := fromJson[restapi.AddSellerItemResponse](writer.Body.String())
+										if assert.NoError(t, err) {
+											router.ServeHTTP(writer, request)
 
-											itemsInDatabase, err := queries.GetItems(db)
-											if assert.NoError(t, err) {
-												if assert.Equal(t, 1, len(itemsInDatabase)) {
-													itemInDatabase := itemsInDatabase[0]
-													assert.Equal(t, response.ItemId, itemInDatabase.ItemId)
-													assert.Equal(t, seller.UserId, itemInDatabase.SellerId)
-													assert.Equal(t, price, itemInDatabase.PriceInCents)
-													assert.Equal(t, description, itemInDatabase.Description)
-													assert.Equal(t, categoryId, itemInDatabase.CategoryId)
-													assert.Equal(t, donation, itemInDatabase.Donation)
-													assert.Equal(t, charity, itemInDatabase.Charity)
+											if assert.Equal(t, http.StatusCreated, writer.Code) {
+												response := fromJson[restapi.AddSellerItemResponse](writer.Body.String())
+
+												itemsInDatabase, err := queries.GetItems(db)
+												if assert.NoError(t, err) {
+													if assert.Equal(t, 1, len(itemsInDatabase)) {
+														itemInDatabase := itemsInDatabase[0]
+														assert.Equal(t, response.ItemId, itemInDatabase.ItemId)
+														assert.Equal(t, seller.UserId, itemInDatabase.SellerId)
+														assert.Equal(t, price, itemInDatabase.PriceInCents)
+														assert.Equal(t, description, itemInDatabase.Description)
+														assert.Equal(t, categoryId, itemInDatabase.CategoryId)
+														assert.Equal(t, donation, itemInDatabase.Donation)
+														assert.Equal(t, charity, itemInDatabase.Charity)
+													}
 												}
 											}
 										}
 									}
-								}
-							})
+								})
+							}
 						}
 					}
 				}
 			}
 		}
-	}
+	})
+
+	t.Run("Failing", func(t *testing.T) {
+		t.Run("Zero price", func(t *testing.T) {
+			price := models.MoneyInCents(0)
+			description := "Test Description"
+			categoryId := models.Clothing50_56
+			donation := false
+			charity := false
+
+			db, router := createRestRouter()
+			writer := httptest.NewRecorder()
+			defer db.Close()
+
+			seller := addTestSeller(db)
+			sessionId := addTestSession(db, seller.UserId)
+
+			payload := restapi.AddSellerItemPayload{
+				Price:       price,
+				Description: description,
+				CategoryId:  categoryId,
+				Donation:    &donation,
+				Charity:     &charity,
+			}
+
+			payloadJson := toJson(payload)
+
+			url := fmt.Sprintf("/api/v1/sellers/%d/items", seller.UserId)
+			request, err := http.NewRequest("POST", url, strings.NewReader(payloadJson))
+
+			if assert.NoError(t, err) {
+				request.Header.Set("Content-Type", "application/json")
+				request.AddCookie(createCookie(sessionId))
+
+				if assert.NoError(t, err) {
+					router.ServeHTTP(writer, request)
+
+					assert.Equal(t, http.StatusBadRequest, writer.Code)
+					itemsInDatabase, err := queries.GetItems(db)
+					if assert.NoError(t, err) {
+						assert.Equal(t, 0, len(itemsInDatabase))
+					}
+				}
+			}
+		})
+	})
 }
