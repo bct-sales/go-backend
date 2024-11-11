1c0c3a24ce2b48da9280e3f051790ac77d9d51af
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-11-11 14:28:47 +0100

    closes #19

---
+++ bctbackend/database/queries/sale_item_info.go
@@ -0,0 +1,46 @@
+package queries
+
+import (
+	models "bctbackend/database/models"
+	"database/sql"
+	"errors"
+)
+
+type SaleItemInformation struct {
+	Description    string
+	ItemCategoryId models.Id
+	PriceInCents   models.MoneyInCents
+	SellCount      int64
+}
+
+func GetSaleItemInformation(
+	db *sql.DB,
+	itemId models.Id) (*SaleItemInformation, error) {
+
+	row := db.QueryRow(
+		`
+			SELECT description, price_in_cents, item_category_id, COUNT(si.sale_id)
+			FROM items i LEFT JOIN sale_items si ON i.item_id = si.item_id
+			GROUP BY i.item_id
+			HAVING item_id = ?
+		`,
+		itemId)
+
+	var saleItemInformation SaleItemInformation
+	err := row.Scan(
+		&saleItemInformation.Description,
+		&saleItemInformation.PriceInCents,
+		&saleItemInformation.ItemCategoryId,
+		&saleItemInformation.SellCount,
+	)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, &ItemNotFoundError{Id: itemId}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &saleItemInformation, nil
+}
