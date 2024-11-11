f53786d6ad95ea861e5659e4d9a732e4228ae14b
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-11-11 16:55:13 +0100

    reorganized test files

--- bctbackend/test/rest/seller/items_test.go
+++ bctbackend/test/rest/seller/items_test.go
@@ -18,37 +18,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestListSellerItems(t *testing.T) {
-	for _, sellerId := range []models.Id{models.NewId(1), models.NewId(2), models.NewId(100)} {
-		for _, itemCount := range []int{0, 1, 5, 100} {
-			db, router := test.CreateRestRouter()
-			writer := httptest.NewRecorder()
-			defer db.Close()
-
-			seller := test.AddSellerWithIdToDatabase(db, sellerId)
-			sessionId := test.AddSessionToDatabase(db, seller.UserId)
-
-			expectedItems := []models.Item{}
-			for i := 0; i < itemCount; i++ {
-				expectedItems = append(expectedItems, *test.AddItemToDatabase(db, seller.UserId, i))
-			}
-
-			url := fmt.Sprintf("/api/v1/sellers/%d/items", seller.UserId)
-			request, err := http.NewRequest("GET", url, nil)
-			request.AddCookie(test.CreateCookie(sessionId))
-
-			if assert.NoError(t, err) {
-				router.ServeHTTP(writer, request)
-
-				if assert.Equal(t, http.StatusOK, writer.Code) {
-					actual := test.FromJson[[]models.Item](writer.Body.String())
-					assert.Equal(t, expectedItems, *actual)
-				}
-			}
-		}
-	}
-}
-
 func TestAddSellerItem(t *testing.T) {
 	t.Run("Successful", func(t *testing.T) {
 		for _, sellerId := range []models.Id{models.NewId(1), models.NewId(2), models.NewId(100)} {
---
+++ bctbackend/test/rest/seller/list_items_test.go
@@ -0,0 +1,46 @@
+//go:build test
+
+package rest
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	models "bctbackend/database/models"
+	"bctbackend/test"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestListSellerItems(t *testing.T) {
+	for _, sellerId := range []models.Id{models.NewId(1), models.NewId(2), models.NewId(100)} {
+		for _, itemCount := range []int{0, 1, 5, 100} {
+			db, router := test.CreateRestRouter()
+			writer := httptest.NewRecorder()
+			defer db.Close()
+
+			seller := test.AddSellerWithIdToDatabase(db, sellerId)
+			sessionId := test.AddSessionToDatabase(db, seller.UserId)
+
+			expectedItems := []models.Item{}
+			for i := 0; i < itemCount; i++ {
+				expectedItems = append(expectedItems, *test.AddItemToDatabase(db, seller.UserId, i))
+			}
+
+			url := fmt.Sprintf("/api/v1/sellers/%d/items", seller.UserId)
+			request, err := http.NewRequest("GET", url, nil)
+			request.AddCookie(test.CreateCookie(sessionId))
+
+			if assert.NoError(t, err) {
+				router.ServeHTTP(writer, request)
+
+				if assert.Equal(t, http.StatusOK, writer.Code) {
+					actual := test.FromJson[[]models.Item](writer.Body.String())
+					assert.Equal(t, expectedItems, *actual)
+				}
+			}
+		}
+	}
+}
