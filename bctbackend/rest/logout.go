7b825202c25b7be2bf31704d0c51d6ae0bec7880
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-11-13 21:22:51 +0100

    logout endpoint

---
+++ bctbackend/rest/logout.go
@@ -0,0 +1,32 @@
+package rest
+
+import (
+	"database/sql"
+	"net/http"
+
+	"bctbackend/database/queries"
+	_ "bctbackend/docs"
+	"bctbackend/security"
+
+	"github.com/gin-gonic/gin"
+)
+
+type LogoutPayload struct{}
+
+func logout(context *gin.Context, db *sql.DB) {
+	sessionId, err := context.Cookie(security.SessionCookieName)
+
+	if err != nil {
+		context.JSON(http.StatusOK, gin.H{"message": "Unauthorized: missing session ID"})
+		return
+	}
+
+	err = queries.DeleteSession(db, sessionId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to delete session"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Successfully logged out"})
+}
