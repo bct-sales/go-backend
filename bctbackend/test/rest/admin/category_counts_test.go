9e4bbd9ce2367078223736a9bd88ebd5535e9432
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-12-21 21:39:02 +0100

    category counts tests

--- bctbackend/test/rest/admin/category_counts_test.go
+++ bctbackend/test/rest/admin/category_counts_test.go
@@ -98,4 +98,34 @@ func TestCategoryCounts(t *testing.T) {
 			assert.Equal(t, expected, *actual)
 		})
 	}
+
+	for _, categoryId1 := range defs.ListCategories() {
+		for _, categoryId2 := range defs.ListCategories() {
+			t.Run("Two items in potentially probably categories", func(t *testing.T) {
+				db, router := test.CreateRestRouter()
+				writer := httptest.NewRecorder()
+				defer db.Close()
+
+				admin := test.AddAdminToDatabase(db)
+				seller := test.AddSellerToDatabase(db)
+				test.AddItemInCategoryToDatabase(db, seller.UserId, categoryId1)
+				test.AddItemInCategoryToDatabase(db, seller.UserId, categoryId2)
+				sessionId := test.AddSessionToDatabase(db, admin.UserId)
+
+				url := path.CategoryCounts().String()
+				request := test.CreateGetRequest(url)
+				request.AddCookie(test.CreateCookie(sessionId))
+
+				router.ServeHTTP(writer, request)
+				expected := rest_admin.CategoryCountResponse{
+					Counts: createEmptyCategoryMap(),
+				}
+				expected.Counts[categoryId1] += 1
+				expected.Counts[categoryId2] += 1
+
+				actual := test.FromJson[rest_admin.CategoryCountResponse](writer.Body.String())
+				assert.Equal(t, expected, *actual)
+			})
+		}
+	}
 }
