fdc09cfc6e010bba417746350d6d68d4d90a9cea
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-12-31 01:14:18 +0100

    split code

---
+++ bctbackend/test/queries/authentication_test.go
@@ -0,0 +1,56 @@
+//go:build test
+
+package queries
+
+import (
+	models "bctbackend/database/models"
+	"bctbackend/database/queries"
+	"bctbackend/test"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	_ "modernc.org/sqlite"
+)
+
+func TestAuthenticatingSuccessfully(t *testing.T) {
+	db := test.OpenInitializedDatabase()
+
+	password := "xyz"
+	userId := models.NewId(1)
+	roleId := models.SellerRoleId
+	createdAt := models.NewTimestamp(0)
+	var lastActivity *models.Timestamp = nil
+
+	queries.AddUserWithId(db, userId, roleId, createdAt, lastActivity, password)
+
+	actualRoleId, err := queries.AuthenticateUser(db, userId, password)
+	if assert.NoError(t, err) {
+		assert.Equal(t, roleId, actualRoleId)
+	}
+}
+
+func TestAuthenticatingNonExistingUser(t *testing.T) {
+	db := test.OpenInitializedDatabase()
+
+	password := "xyz"
+	userId := models.NewId(5)
+
+	assert.False(t, queries.UserWithIdExists(db, userId))
+
+	_, err := queries.AuthenticateUser(db, userId, password)
+	assert.Error(t, err)
+}
+
+func TestAuthenticatingWrongPassword(t *testing.T) {
+	db := test.OpenInitializedDatabase()
+
+	password := "xyz"
+	wrongPassword := "abc"
+	userId := models.NewId(5)
+	roleId := models.SellerRoleId
+
+	queries.AddUserWithId(db, userId, roleId, 0, nil, password)
+
+	_, err := queries.AuthenticateUser(db, userId, wrongPassword)
+	assert.Error(t, err)
+}
