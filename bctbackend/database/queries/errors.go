26b7067133a7d5dc5880c0258b605de5e39ebf4b
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-01-01 17:37:40 +0100

    rearranged code

--- bctbackend/database/queries/errors.go
+++ bctbackend/database/queries/errors.go
@@ -9,16 +9,26 @@ type AuthenticationError struct {
 	Reason error
 }
 
+type ItemNotFoundError struct {
+	Id models.Id
+}
+
+type SaleMissingItemsError struct{}
+
+type SaleRequiresCashierError struct{}
+
+type UnknownUserError struct {
+	UserId models.Id
+}
+
+type WrongPasswordError struct{}
+
 func (e *AuthenticationError) Error() string {
 	return fmt.Sprintf("authentication error: %v", e.Reason)
 }
 
 func (e *AuthenticationError) Unwrap() error {
 	return e.Reason
-}
-
-type ItemNotFoundError struct {
-	Id models.Id
 }
 
 func (e *ItemNotFoundError) Error() string {
@@ -29,25 +39,17 @@ func (e *ItemNotFoundError) Unwrap() error {
 	return nil
 }
 
-type SaleMissingItemsError struct{}
-
 func (e *SaleMissingItemsError) Error() string {
 	return "sale must have at least one item"
 }
-
-type SaleRequiresCashierError struct{}
 
 func (e *SaleRequiresCashierError) Error() string {
 	return "sale requires a cashier"
 }
 
-type UnknownUserError struct{}
-
 func (e *UnknownUserError) Error() string {
-	return "unknown user"
+	return fmt.Sprintf("unknown user %d", e.UserId)
 }
-
-type WrongPasswordError struct{}
 
 func (e *WrongPasswordError) Error() string {
 	return "wrong password"
