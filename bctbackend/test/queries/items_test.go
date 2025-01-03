bfc43895e66117457f2c0a8732bc16752a8ad7e8
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-01-03 16:07:08 +0100

    fixed tests

--- bctbackend/test/queries/items_test.go
+++ bctbackend/test/queries/items_test.go
@@ -3,7 +3,6 @@
 package queries
 
 import (
-	"bctbackend/database/models"
 	"bctbackend/database/queries"
 	"bctbackend/test"
 	"testing"
@@ -12,51 +11,41 @@ import (
 	_ "modernc.org/sqlite"
 )
 
-func TestRemoveItemWithId(t *testing.T) {
-	t.Run("Nonexisting item", func(t *testing.T) {
+func TestCountItems(t *testing.T) {
+	t.Run("Empty", func(t *testing.T) {
 		db := test.OpenInitializedDatabase()
 		defer db.Close()
 
-		itemId := models.NewId(1)
-		err := queries.RemoveItemWithId(db, itemId)
-
-		var itemNotFoundError *queries.ItemNotFoundError
-		assert.ErrorAs(t, err, &itemNotFoundError)
-	})
-
-	t.Run("Existing item", func(t *testing.T) {
-		db := test.OpenInitializedDatabase()
-		defer db.Close()
-
-		sellerId := test.AddSellerToDatabase(db).UserId
-		itemId := test.AddItemToDatabase(db, sellerId, 1).ItemId
-
-		err := queries.RemoveItemWithId(db, itemId)
+		count, err := queries.CountItems(db)
 		if assert.NoError(t, err) {
-			itemExists, err := queries.ItemWithIdExists(db, itemId)
-
-			if assert.NoError(t, err) {
-				assert.False(t, itemExists)
-			}
+			assert.Equal(t, 0, count)
 		}
 	})
 
-	t.Run("Existing item with sale", func(t *testing.T) {
+	t.Run("One item", func(t *testing.T) {
 		db := test.OpenInitializedDatabase()
 		defer db.Close()
 
 		sellerId := test.AddSellerToDatabase(db).UserId
-		cashierId := test.AddCashierToDatabase(db).UserId
-		itemId := test.AddItemToDatabase(db, sellerId, 1).ItemId
+		test.AddItemToDatabase(db, sellerId, 1)
 
-		test.AddSaleToDatabase(db, cashierId, []models.Id{itemId})
-
-		err := queries.RemoveItemWithId(db, itemId)
-		assert.Error(t, err)
-
-		itemExists, err := queries.ItemWithIdExists(db, itemId)
+		count, err := queries.CountItems(db)
 		if assert.NoError(t, err) {
-			assert.True(t, itemExists)
+			assert.Equal(t, 1, count)
+		}
+	})
+
+	t.Run("Two items", func(t *testing.T) {
+		db := test.OpenInitializedDatabase()
+		defer db.Close()
+
+		sellerId := test.AddSellerToDatabase(db).UserId
+		test.AddItemToDatabase(db, sellerId, 1)
+		test.AddItemToDatabase(db, sellerId, 2)
+
+		count, err := queries.CountItems(db)
+		if assert.NoError(t, err) {
+			assert.Equal(t, 2, count)
 		}
 	})
 }
