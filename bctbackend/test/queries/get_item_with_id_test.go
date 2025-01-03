6a8bcdc088a6314f884d7deb1b93f3d8a889e122
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-01-03 16:02:02 +0100

    split file

---
+++ bctbackend/test/queries/get_item_with_id_test.go
@@ -0,0 +1,39 @@
+//go:build test
+
+package queries
+
+import (
+	"bctbackend/database/models"
+	"bctbackend/database/queries"
+	"bctbackend/test"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	_ "modernc.org/sqlite"
+)
+
+func TestGetItemWithId(t *testing.T) {
+	t.Run("Nonexisting item", func(t *testing.T) {
+		db := test.OpenInitializedDatabase()
+		defer db.Close()
+
+		itemId := models.NewId(1)
+		_, err := queries.GetItemWithId(db, itemId)
+
+		var itemNotFoundError *queries.ItemNotFoundError
+		assert.ErrorAs(t, err, &itemNotFoundError)
+	})
+
+	t.Run("Existing item", func(t *testing.T) {
+		db := test.OpenInitializedDatabase()
+		defer db.Close()
+
+		sellerId := test.AddSellerToDatabase(db).UserId
+		itemId := test.AddItemToDatabase(db, sellerId, 1).ItemId
+
+		item, err := queries.GetItemWithId(db, itemId)
+		if assert.NoError(t, err) {
+			assert.Equal(t, itemId, item.ItemId)
+		}
+	})
+}
---
+++ bctbackend/test/queries/get_items_test.go
@@ -0,0 +1,54 @@
+//go:build test
+
+package queries
+
+import (
+	"bctbackend/database/queries"
+	"bctbackend/test"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	_ "modernc.org/sqlite"
+)
+
+func TestGetItems(t *testing.T) {
+	t.Run("Empty", func(t *testing.T) {
+		db := test.OpenInitializedDatabase()
+		defer db.Close()
+
+		items, err := queries.GetItems(db)
+		if assert.NoError(t, err) {
+			assert.Equal(t, 0, len(items))
+		}
+	})
+
+	t.Run("One item", func(t *testing.T) {
+		db := test.OpenInitializedDatabase()
+		defer db.Close()
+
+		sellerId := test.AddSellerToDatabase(db).UserId
+		itemId := test.AddItemToDatabase(db, sellerId, 1).ItemId
+
+		items, err := queries.GetItems(db)
+		if assert.NoError(t, err) {
+			assert.Equal(t, 1, len(items))
+			assert.Equal(t, itemId, items[0].ItemId)
+		}
+	})
+
+	t.Run("Two items", func(t *testing.T) {
+		db := test.OpenInitializedDatabase()
+		defer db.Close()
+
+		sellerId := test.AddSellerToDatabase(db).UserId
+		item1Id := test.AddItemToDatabase(db, sellerId, 1).ItemId
+		item2Id := test.AddItemToDatabase(db, sellerId, 2).ItemId
+
+		items, err := queries.GetItems(db)
+		if assert.NoError(t, err) {
+			assert.Equal(t, 2, len(items))
+			assert.Equal(t, item1Id, items[0].ItemId)
+			assert.Equal(t, item2Id, items[1].ItemId)
+		}
+	})
+}
