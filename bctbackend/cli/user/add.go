e382acb06681ab1168999ab28a2b9255d14fcb7f
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-01-03 21:22:02 +0100

    AddUser returns NoSuchRoleError

--- bctbackend/cli/user/add.go
+++ bctbackend/cli/user/add.go
@@ -4,6 +4,8 @@ import (
 	"bctbackend/database"
 	"bctbackend/database/models"
 	"bctbackend/database/queries"
+	"errors"
+	"fmt"
 	"time"
 
 	_ "modernc.org/sqlite"
@@ -22,6 +24,17 @@ func AddUser(databasePath string, userId models.Id, role models.Id, password str
 	var lastActivity *models.Timestamp = nil
 
 	if err := queries.AddUserWithId(db, userId, role, timestamp, lastActivity, password); err != nil {
+		{
+			var userIdAlreadyInUseError *queries.UserIdAlreadyInUseError
+			if errors.As(err, &userIdAlreadyInUseError) {
+				fmt.Printf("User with id %d already exists\n", userId)
+				return err
+			}
+		}
+
+		
+
+
 		return err
 	}
 
