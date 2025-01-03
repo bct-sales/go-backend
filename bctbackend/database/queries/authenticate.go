2756ec76e888a1a6db0f64967df0b634205bae27
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-01-03 19:15:47 +0100

    improved AuthenticateUser

--- bctbackend/database/queries/authenticate.go
+++ bctbackend/database/queries/authenticate.go
@@ -6,6 +6,12 @@ import (
 	"errors"
 )
 
+// AuthenticateUser authenticates a user with the given user id and password.
+// If the user is authenticated, the function returns the role id of the user.
+// If the user is not authenticated, the function returns an error.
+// If the user does not exist, the function returns an UnknownUserError.
+// If the password is wrong, the function returns a WrongPasswordError.
+// If there is an error while querying the database, the function returns the error.
 func AuthenticateUser(db *sql.DB, userId models.Id, password string) (models.Id, error) {
 	row := db.QueryRow(
 		`
@@ -27,9 +33,9 @@ func AuthenticateUser(db *sql.DB, userId models.Id, password string) (models.Id,
 		return 0, err
 	}
 
-	if expectedPassword == password {
-		return roleId, nil
-	} else {
+	if expectedPassword != password {
 		return 0, &WrongPasswordError{}
 	}
+
+	return roleId, nil
 }
