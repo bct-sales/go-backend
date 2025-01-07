dd00975a17b9a9d95ebfdf7bae3bb1f2ae5a1859
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-01-07 21:59:24 +0100

    GetMultiplySoldItem tests

--- bctbackend/test/queries/get_multiply_sold_items_test.go
+++ bctbackend/test/queries/get_multiply_sold_items_test.go
@@ -70,4 +70,97 @@ func TestGetMultiplySoldItems(t *testing.T) {
 		require.Equal(t, sale1, multiplySoldItem.Sales[0].SaleId)
 		require.Equal(t, sale2, multiplySoldItem.Sales[1].SaleId)
 	})
+
+	t.Run("Item sold thrice", func(t *testing.T) {
+		db := test.OpenInitializedDatabase()
+		defer db.Close()
+
+		sellerId := test.AddSellerToDatabase(db).UserId
+		cashierId := test.AddCashierToDatabase(db).UserId
+
+		items := []*models.Item{
+			test.AddItemToDatabase(db, sellerId, 1),
+		}
+
+		sale1 := test.AddSaleToDatabase(db, cashierId, []models.Id{items[0].ItemId})
+		sale2 := test.AddSaleToDatabase(db, cashierId, []models.Id{items[0].ItemId})
+		sale3 := test.AddSaleToDatabase(db, cashierId, []models.Id{items[0].ItemId})
+
+		multiplySoldItems, err := queries.GetMultiplySoldItems(db)
+
+		require.NoError(t, err)
+		require.Len(t, multiplySoldItems, 1)
+
+		multiplySoldItem := multiplySoldItems[0]
+		require.Equal(t, *(items[0]), multiplySoldItem.Item)
+		require.Len(t, multiplySoldItem.Sales, 3)
+		require.Equal(t, sale1, multiplySoldItem.Sales[0].SaleId)
+		require.Equal(t, sale2, multiplySoldItem.Sales[1].SaleId)
+		require.Equal(t, sale3, multiplySoldItem.Sales[2].SaleId)
+	})
+
+	t.Run("Two items sold twice", func(t *testing.T) {
+		db := test.OpenInitializedDatabase()
+		defer db.Close()
+
+		sellerId := test.AddSellerToDatabase(db).UserId
+		cashierId := test.AddCashierToDatabase(db).UserId
+
+		items := []*models.Item{
+			test.AddItemToDatabase(db, sellerId, 1),
+			test.AddItemToDatabase(db, sellerId, 2),
+		}
+
+		sale1 := test.AddSaleToDatabase(db, cashierId, []models.Id{items[0].ItemId, items[1].ItemId})
+		sale2 := test.AddSaleToDatabase(db, cashierId, []models.Id{items[0].ItemId, items[1].ItemId})
+
+		multiplySoldItems, err := queries.GetMultiplySoldItems(db)
+
+		require.NoError(t, err)
+		require.Len(t, multiplySoldItems, 2)
+
+		require.Equal(t, *(items[0]), multiplySoldItems[0].Item)
+		require.Len(t, multiplySoldItems[0].Sales, 2)
+		require.Equal(t, sale1, multiplySoldItems[0].Sales[0].SaleId)
+		require.Equal(t, sale2, multiplySoldItems[0].Sales[1].SaleId)
+
+		require.Equal(t, *(items[1]), multiplySoldItems[1].Item)
+		require.Len(t, multiplySoldItems[1].Sales, 2)
+		require.Equal(t, sale1, multiplySoldItems[1].Sales[0].SaleId)
+		require.Equal(t, sale2, multiplySoldItems[1].Sales[1].SaleId)
+	})
+
+	t.Run("Sales [1], [1, 2], [1, 2, 3]", func(t *testing.T) {
+		db := test.OpenInitializedDatabase()
+		defer db.Close()
+
+		sellerId := test.AddSellerToDatabase(db).UserId
+		cashierId := test.AddCashierToDatabase(db).UserId
+
+		items := []*models.Item{
+			test.AddItemToDatabase(db, sellerId, 1),
+			test.AddItemToDatabase(db, sellerId, 2),
+			test.AddItemToDatabase(db, sellerId, 3),
+		}
+
+		sale1 := test.AddSaleToDatabase(db, cashierId, []models.Id{items[0].ItemId})
+		sale2 := test.AddSaleToDatabase(db, cashierId, []models.Id{items[0].ItemId, items[1].ItemId})
+		sale3 := test.AddSaleToDatabase(db, cashierId, []models.Id{items[0].ItemId, items[1].ItemId, items[2].ItemId})
+
+		multiplySoldItems, err := queries.GetMultiplySoldItems(db)
+
+		require.NoError(t, err)
+		require.Len(t, multiplySoldItems, 2)
+
+		require.Equal(t, *(items[0]), multiplySoldItems[0].Item)
+		require.Len(t, multiplySoldItems[0].Sales, 3)
+		require.Equal(t, sale1, multiplySoldItems[0].Sales[0].SaleId)
+		require.Equal(t, sale2, multiplySoldItems[0].Sales[1].SaleId)
+		require.Equal(t, sale3, multiplySoldItems[0].Sales[2].SaleId)
+
+		require.Equal(t, *(items[1]), multiplySoldItems[1].Item)
+		require.Len(t, multiplySoldItems[1].Sales, 2)
+		require.Equal(t, sale2, multiplySoldItems[1].Sales[0].SaleId)
+		require.Equal(t, sale3, multiplySoldItems[1].Sales[1].SaleId)
+	})
 }
