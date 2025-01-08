13ed3e79d8ff448ae3aadd2d62b6c62c9a2d0823
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-01-09 00:46:10 +0100

    GetSalesOverView

---
+++ bctbackend/database/queries/sales_overview.go
@@ -0,0 +1,52 @@
+package queries
+
+import (
+	models "bctbackend/database/models"
+	"database/sql"
+)
+
+type CategorySaleTotal struct {
+	CategoryId   models.Id
+	CategoryName string
+	TotalInCents models.MoneyInCents
+}
+
+func GetSalesOverview(db *sql.DB) ([]CategorySaleTotal, error) {
+	rows, err := db.Query(
+		`
+			SELECT item_categories.item_category_id, item_categories.name, SUM(COALESCE(i.price_in_cents, 0))
+			FROM item_categories
+			LEFT JOIN (
+				items INNER JOIN sale_items ON items.item_id = sale_items.item_id
+			) AS i ON i.item_category_id = item_categories.item_category_id
+			GROUP BY item_categories.item_category_id
+			ORDER BY item_categories.item_category_id
+		`,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var categorySaleTotals []CategorySaleTotal
+
+	for rows.Next() {
+		var categorySaleTotal CategorySaleTotal
+
+		err := rows.Scan(
+			&categorySaleTotal.CategoryId,
+			&categorySaleTotal.CategoryName,
+			&categorySaleTotal.TotalInCents,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		categorySaleTotals = append(categorySaleTotals, categorySaleTotal)
+	}
+
+	return categorySaleTotals, nil
+}
