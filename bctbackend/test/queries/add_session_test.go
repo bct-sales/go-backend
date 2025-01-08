53dc999c3251c45a7e0842962b99da2984f52ab7
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-01-08 20:45:29 +0100

    refactored tests

---
+++ bctbackend/test/queries/add_session_test.go
@@ -0,0 +1,32 @@
+//go:build test
+
+package queries
+
+import (
+	models "bctbackend/database/models"
+	"bctbackend/database/queries"
+	"bctbackend/test"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	_ "modernc.org/sqlite"
+)
+
+func TestAddSession(t *testing.T) {
+	for _, roleId := range []models.Id{models.AdminRoleId, models.CashierRoleId, models.SellerRoleId} {
+		db := test.OpenInitializedDatabase()
+		defer db.Close()
+
+		userId := test.AddUserToDatabase(db, roleId).UserId
+		expirationTime := models.Timestamp(0)
+		sessionId, err := queries.AddSession(db, userId, expirationTime)
+		require.NoError(t, err)
+
+		session, err := queries.GetSessionById(db, sessionId)
+		require.NoError(t, err)
+		require.Equal(t, sessionId, session.SessionId)
+		require.Equal(t, userId, session.UserId)
+		require.Equal(t, expirationTime, session.ExpirationTime)
+	}
+}
--- bctbackend/test/queries/sessions_test.go
+++ bctbackend/test/queries/sessions_test.go
@@ -13,24 +13,6 @@ import (
 	_ "modernc.org/sqlite"
 )
 
-func TestAddSession(t *testing.T) {
-	for _, roleId := range []models.Id{models.AdminRoleId, models.CashierRoleId, models.SellerRoleId} {
-		db := test.OpenInitializedDatabase()
-		defer db.Close()
-
-		userId := test.AddUserToDatabase(db, roleId).UserId
-		expirationTime := models.Timestamp(0)
-		sessionId, err := queries.AddSession(db, userId, expirationTime)
-		require.NoError(t, err)
-
-		session, err := queries.GetSessionById(db, sessionId)
-		require.NoError(t, err)
-		require.Equal(t, sessionId, session.SessionId)
-		require.Equal(t, userId, session.UserId)
-		require.Equal(t, expirationTime, session.ExpirationTime)
-	}
-}
-
 func TestDeleteSession(t *testing.T) {
 	db := test.OpenInitializedDatabase()
 	defer db.Close()
