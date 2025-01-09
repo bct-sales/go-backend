42c83d5e8e25e875ebc0133b89dc66d51ae8c771
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-01-09 01:12:22 +0100

    GetSalesOverview tests

--- bctbackend/test/queries/get_sales_overview_test.go
+++ bctbackend/test/queries/get_sales_overview_test.go
@@ -12,6 +12,14 @@ import (
 	_ "modernc.org/sqlite"
 )
 
+func createTotalMap(categories []models.ItemCategory) map[models.Id]models.MoneyInCents {
+	totalMap := make(map[models.Id]models.MoneyInCents)
+	for _, category := range categories {
+		totalMap[category.CategoryId] = models.MoneyInCents(0)
+	}
+	return totalMap
+}
+
 func TestGetSalesOverview(t *testing.T) {
 	t.Run("Zero items", func(t *testing.T) {
 		db := test.OpenInitializedDatabase()
@@ -51,6 +59,156 @@ func TestGetSalesOverview(t *testing.T) {
 			require.Equal(t, category.CategoryId, categorySaleTotals[categoryIndex].CategoryId)
 			require.Equal(t, category.Name, categorySaleTotals[categoryIndex].CategoryName)
 			require.Equal(t, models.MoneyInCents(0), categorySaleTotals[categoryIndex].TotalInCents)
+		}
+	})
+
+	t.Run("One sold item", func(t *testing.T) {
+		db := test.OpenInitializedDatabase()
+		defer db.Close()
+
+		categories, err := queries.GetCategories(db)
+		require.NoError(t, err)
+
+		totals := createTotalMap(categories)
+
+		seller := test.AddSellerToDatabase(db)
+		item := test.AddItemInCategoryToDatabase(db, seller.UserId, categories[0].CategoryId)
+		totals[item.CategoryId] += item.PriceInCents
+
+		cashier := test.AddCashierToDatabase(db)
+		test.AddSaleToDatabase(db, cashier.UserId, []models.Id{item.ItemId})
+
+		categorySaleTotals, err := queries.GetSalesOverview(db)
+		t.Log(categorySaleTotals)
+		require.NoError(t, err)
+		require.Equal(t, len(categories), len(categorySaleTotals))
+
+		for categoryIndex, category := range categories {
+			require.Equal(t, category.CategoryId, categorySaleTotals[categoryIndex].CategoryId)
+			require.Equal(t, category.Name, categorySaleTotals[categoryIndex].CategoryName)
+			require.Equal(t, totals[category.CategoryId], categorySaleTotals[categoryIndex].TotalInCents)
+		}
+	})
+
+	t.Run("Two sold same-category items in single sale", func(t *testing.T) {
+		db := test.OpenInitializedDatabase()
+		defer db.Close()
+
+		categories, err := queries.GetCategories(db)
+		require.NoError(t, err)
+
+		totals := createTotalMap(categories)
+
+		seller := test.AddSellerToDatabase(db)
+		item1 := test.AddItemInCategoryToDatabase(db, seller.UserId, categories[0].CategoryId)
+		item2 := test.AddItemInCategoryToDatabase(db, seller.UserId, categories[0].CategoryId)
+		totals[item1.CategoryId] += item1.PriceInCents
+		totals[item2.CategoryId] += item1.PriceInCents
+
+		cashier := test.AddCashierToDatabase(db)
+		test.AddSaleToDatabase(db, cashier.UserId, []models.Id{item1.ItemId, item2.ItemId})
+
+		categorySaleTotals, err := queries.GetSalesOverview(db)
+		t.Log(categorySaleTotals)
+		require.NoError(t, err)
+		require.Equal(t, len(categories), len(categorySaleTotals))
+
+		for categoryIndex, category := range categories {
+			require.Equal(t, category.CategoryId, categorySaleTotals[categoryIndex].CategoryId)
+			require.Equal(t, category.Name, categorySaleTotals[categoryIndex].CategoryName)
+			require.Equal(t, totals[category.CategoryId], categorySaleTotals[categoryIndex].TotalInCents)
+		}
+	})
+
+	t.Run("Two sold different-category items in single sale", func(t *testing.T) {
+		db := test.OpenInitializedDatabase()
+		defer db.Close()
+
+		categories, err := queries.GetCategories(db)
+		require.NoError(t, err)
+
+		totals := createTotalMap(categories)
+
+		seller := test.AddSellerToDatabase(db)
+		item1 := test.AddItemInCategoryToDatabase(db, seller.UserId, categories[0].CategoryId)
+		item2 := test.AddItemInCategoryToDatabase(db, seller.UserId, categories[1].CategoryId)
+		totals[item1.CategoryId] += item1.PriceInCents
+		totals[item2.CategoryId] += item1.PriceInCents
+
+		cashier := test.AddCashierToDatabase(db)
+		test.AddSaleToDatabase(db, cashier.UserId, []models.Id{item1.ItemId, item2.ItemId})
+
+		categorySaleTotals, err := queries.GetSalesOverview(db)
+		t.Log(categorySaleTotals)
+		require.NoError(t, err)
+		require.Equal(t, len(categories), len(categorySaleTotals))
+
+		for categoryIndex, category := range categories {
+			require.Equal(t, category.CategoryId, categorySaleTotals[categoryIndex].CategoryId)
+			require.Equal(t, category.Name, categorySaleTotals[categoryIndex].CategoryName)
+			require.Equal(t, totals[category.CategoryId], categorySaleTotals[categoryIndex].TotalInCents)
+		}
+	})
+
+	t.Run("Two sold same-category items in separate sales", func(t *testing.T) {
+		db := test.OpenInitializedDatabase()
+		defer db.Close()
+
+		categories, err := queries.GetCategories(db)
+		require.NoError(t, err)
+
+		totals := createTotalMap(categories)
+
+		seller := test.AddSellerToDatabase(db)
+		item1 := test.AddItemInCategoryToDatabase(db, seller.UserId, categories[0].CategoryId)
+		item2 := test.AddItemInCategoryToDatabase(db, seller.UserId, categories[0].CategoryId)
+		totals[item1.CategoryId] += item1.PriceInCents
+		totals[item2.CategoryId] += item1.PriceInCents
+
+		cashier := test.AddCashierToDatabase(db)
+		test.AddSaleToDatabase(db, cashier.UserId, []models.Id{item1.ItemId})
+		test.AddSaleToDatabase(db, cashier.UserId, []models.Id{item2.ItemId})
+
+		categorySaleTotals, err := queries.GetSalesOverview(db)
+		t.Log(categorySaleTotals)
+		require.NoError(t, err)
+		require.Equal(t, len(categories), len(categorySaleTotals))
+
+		for categoryIndex, category := range categories {
+			require.Equal(t, category.CategoryId, categorySaleTotals[categoryIndex].CategoryId)
+			require.Equal(t, category.Name, categorySaleTotals[categoryIndex].CategoryName)
+			require.Equal(t, totals[category.CategoryId], categorySaleTotals[categoryIndex].TotalInCents)
+		}
+	})
+
+	t.Run("Two sold different-category items in separate sales", func(t *testing.T) {
+		db := test.OpenInitializedDatabase()
+		defer db.Close()
+
+		categories, err := queries.GetCategories(db)
+		require.NoError(t, err)
+
+		totals := createTotalMap(categories)
+
+		seller := test.AddSellerToDatabase(db)
+		item1 := test.AddItemInCategoryToDatabase(db, seller.UserId, categories[0].CategoryId)
+		item2 := test.AddItemInCategoryToDatabase(db, seller.UserId, categories[1].CategoryId)
+		totals[item1.CategoryId] += item1.PriceInCents
+		totals[item2.CategoryId] += item1.PriceInCents
+
+		cashier := test.AddCashierToDatabase(db)
+		test.AddSaleToDatabase(db, cashier.UserId, []models.Id{item1.ItemId})
+		test.AddSaleToDatabase(db, cashier.UserId, []models.Id{item2.ItemId})
+
+		categorySaleTotals, err := queries.GetSalesOverview(db)
+		t.Log(categorySaleTotals)
+		require.NoError(t, err)
+		require.Equal(t, len(categories), len(categorySaleTotals))
+
+		for categoryIndex, category := range categories {
+			require.Equal(t, category.CategoryId, categorySaleTotals[categoryIndex].CategoryId)
+			require.Equal(t, category.Name, categorySaleTotals[categoryIndex].CategoryName)
+			require.Equal(t, totals[category.CategoryId], categorySaleTotals[categoryIndex].TotalInCents)
 		}
 	})
 }
