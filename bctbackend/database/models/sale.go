da2200e7601d49c11349e7bb1a7f9c84b7178b93
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-18 15:04:18 +0100

    removed redundant code

--- bctbackend/database/models/sale.go
+++ bctbackend/database/models/sale.go
@@ -25,19 +25,3 @@ func NewSale(
 		TransactionTime: transactionTime,
 	}
 }
-
-func NewSaleSummary(
-	saleId Id,
-	cashierId Id,
-	transactionTime Timestamp,
-	totalItems int,
-	totalPrice MoneyInCents) *SaleSummary {
-
-	return &SaleSummary{
-		SaleId:            saleId,
-		CashierId:         cashierId,
-		TransactionTime:   transactionTime,
-		ItemCount:         totalItems,
-		TotalPriceInCents: totalPrice,
-	}
-}
