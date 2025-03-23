7c2b962a40514715a93f541d08c922da759668da
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-23 15:19:52 +0100

    refactored tests

--- bctbackend/test/queries/add_user_test.go
+++ bctbackend/test/queries/add_user_test.go
@@ -14,37 +14,39 @@ import (
 )
 
 func TestAddUser(t *testing.T) {
-	for _, password := range []string{"a", "xyz"} {
-		for _, roleId := range []models.Id{models.AdminRoleId, models.CashierRoleId, models.SellerRoleId} {
-			t.Run(fmt.Sprintf("With role id %d", roleId), func(t *testing.T) {
-				db := OpenInitializedDatabase()
-				defer db.Close()
+	t.Run("Success", func(t *testing.T) {
+		for _, password := range []string{"a", "xyz"} {
+			for _, roleId := range []models.Id{models.AdminRoleId, models.CashierRoleId, models.SellerRoleId} {
+				t.Run(fmt.Sprintf("With role id %d", roleId), func(t *testing.T) {
+					db := OpenInitializedDatabase()
+					defer db.Close()
 
-				userId, err := queries.AddUser(db, roleId, 0, nil, password)
-				require.NoError(t, err)
+					userId, err := queries.AddUser(db, roleId, 0, nil, password)
+					require.NoError(t, err)
 
-				userExists, err := queries.UserWithIdExists(db, userId)
-				require.NoError(t, err)
-				require.True(t, userExists)
-			})
+					userExists, err := queries.UserWithIdExists(db, userId)
+					require.NoError(t, err)
+					require.True(t, userExists)
+				})
+			}
 		}
-	}
-}
+	})
 
-func TestAddUserWithInvalidRole(t *testing.T) {
-	db := OpenInitializedDatabase()
-	defer db.Close()
+	t.Run("Fail due to invalid role", func(t *testing.T) {
+		db := OpenInitializedDatabase()
+		defer db.Close()
 
-	roleId := models.Id(10)
-	password := "xyz"
-	createdAt := models.Timestamp(0)
-	var lastActivity *models.Timestamp = nil
+		roleId := models.Id(10)
+		password := "xyz"
+		createdAt := models.Timestamp(0)
+		var lastActivity *models.Timestamp = nil
 
-	require.False(t, models.IsValidRole(roleId), "sanity test: role id should be invalid")
+		require.False(t, models.IsValidRole(roleId), "sanity test: role id should be invalid")
 
-	{
-		_, err := queries.AddUser(db, roleId, createdAt, lastActivity, password)
-		var noSuchRoleError *queries.NoSuchRoleError
-		require.ErrorAs(t, err, &noSuchRoleError)
-	}
+		{
+			_, err := queries.AddUser(db, roleId, createdAt, lastActivity, password)
+			var noSuchRoleError *queries.NoSuchRoleError
+			require.ErrorAs(t, err, &noSuchRoleError)
+		}
+	})
 }
