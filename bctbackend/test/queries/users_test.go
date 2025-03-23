da805d3dbd5e14ff5623da11a8a558ff47208f3b
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-23 15:14:31 +0100

    closes #110

--- bctbackend/test/queries/users_test.go
+++ bctbackend/test/queries/users_test.go
@@ -148,27 +148,3 @@ func TestListUsers(t *testing.T) {
 	require.Equal(t, roleId, users[0].RoleId)
 	require.Equal(t, password, users[0].Password)
 }
-
-func TestUpdatePassword(t *testing.T) {
-	db := OpenInitializedDatabase()
-	defer db.Close()
-
-	password1 := "xyz"
-	password2 := "abc"
-	newPassword1 := "123"
-
-	user1Id, err := queries.AddUser(db, models.SellerRoleId, 0, nil, password1)
-	require.NoError(t, err)
-
-	user2Id, err := queries.AddUser(db, models.SellerRoleId, 0, nil, password2)
-	require.NoError(t, err)
-
-	err = queries.UpdateUserPassword(db, user1Id, newPassword1)
-	require.NoError(t, err)
-
-	_, err = queries.AuthenticateUser(db, user1Id, newPassword1)
-	require.NoError(t, err)
-
-	_, err = queries.AuthenticateUser(db, user2Id, password2)
-	require.NoError(t, err)
-}
