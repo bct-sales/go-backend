0a04aef78519177bac11cf5bce150113ae6ed9ec
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-23 09:07:28 +0100

    improved admin/items

--- bctbackend/rest/admin/items.go
+++ bctbackend/rest/admin/items.go
@@ -1,8 +1,10 @@
 package admin
 
 import (
+	"bctbackend/algorithms"
 	"bctbackend/database/models"
 	"bctbackend/database/queries"
+	rest "bctbackend/rest/shared"
 	"database/sql"
 	"net/http"
 
@@ -11,7 +13,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type GetItemsItemData struct {
+	ItemId       models.Id                `json:"itemId"`
+	AddedAt      rest.StructuredTimestamp `json:"addedAt"`
+	Description  string                   `json:"description"`
+	PriceInCents models.MoneyInCents      `json:"priceInCents"`
+	CategoryId   models.Id                `json:"categoryId"`
+	SellerId     models.Id                `json:"sellerId"`
+	Donation     bool                     `json:"donation"`
+	Charity      bool                     `json:"charity"`
+}
+
+type GetItemsSuccessResponse struct {
+	Items []GetItemsItemData `json:"items"`
+}
+
 type GetItemsFailureResponse struct {
+	Type    string `json:"type"`
 	Message string `json:"message"`
 }
 
@@ -20,12 +38,12 @@ type GetItemsFailureResponse struct {
 // @Tags items
 // @Accept json
 // @Produce json
-// @Success 200 {object} []models.Item "Items successfully fetched"
+// @Success 200 {object} GetItemsSuccessResponse "Items successfully fetched"
 // @Failure 500 {object} GetItemsFailureResponse "Failed to fetch items"
 // @Router /admin/items [get]
 func GetItems(context *gin.Context, db *sql.DB, userId models.Id, roleId models.Id) {
 	if roleId != models.AdminRoleId {
-		failureResponse := GetItemsFailureResponse{Message: "Only accessible to admins"}
+		failureResponse := GetItemsFailureResponse{Type: "forbidden", Message: "Only accessible to admins"}
 		context.JSON(http.StatusForbidden, failureResponse)
 		return
 	}
@@ -37,5 +55,18 @@ func GetItems(context *gin.Context, db *sql.DB, userId models.Id, roleId models.
 		return
 	}
 
-	context.IndentedJSON(http.StatusOK, items)
+	response := GetItemsSuccessResponse{Items: algorithms.Map(items, func(item *models.Item) GetItemsItemData {
+		return GetItemsItemData{
+			ItemId:       item.ItemId,
+			AddedAt:      rest.FromUnix(item.AddedAt),
+			Description:  item.Description,
+			PriceInCents: item.PriceInCents,
+			CategoryId:   item.CategoryId,
+			SellerId:     item.SellerId,
+			Donation:     item.Donation,
+			Charity:      item.Charity,
+		}
+	})}
+
+	context.IndentedJSON(http.StatusOK, response)
 }
