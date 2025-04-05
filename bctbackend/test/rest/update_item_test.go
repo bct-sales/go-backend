a4aad2165849ad942f486212a2b82fdc2530c282
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-04-06 00:35:30 +0200

    UpdateItem test

--- bctbackend/test/rest/update_item_test.go
+++ bctbackend/test/rest/update_item_test.go
@@ -191,4 +191,29 @@ func TestUpdateItem(t *testing.T) {
 		router.ServeHTTP(writer, request)
 		require.Equal(t, http.StatusUnauthorized, writer.Code)
 	})
+
+	t.Run("Failing due to nonowner seller", func(t *testing.T) {
+		setup, router, writer := SetupRestTest()
+		defer setup.Close()
+
+		ownerSeller := setup.Seller()
+		_, sessionId := setup.LoggedIn(setup.Seller())
+		originalItem := setup.Item(ownerSeller.UserId, aux.WithDummyData(1), aux.WithFrozen(true))
+
+		url := path.Items().Id(originalItem.ItemId)
+		payload := struct {
+			PriceInCents int `json:"priceInCents"`
+		}{
+			PriceInCents: 100,
+		}
+		request := CreatePutRequest(url, &payload, WithCookie(sessionId))
+		router.ServeHTTP(writer, request)
+		require.Equal(t, http.StatusUnauthorized, writer.Code)
+
+		actualItem, err := queries.GetItemWithId(setup.Db, originalItem.ItemId)
+		require.NoError(t, err)
+
+		expectedItem := *originalItem
+		require.Equal(t, expectedItem, *actualItem)
+	})
 }
