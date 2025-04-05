ca913413e89a19418de2a97b494410d5ce9f356f
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-04-05 21:04:09 +0200

    added path for updating items

--- bctbackend/rest/path/items.go
+++ bctbackend/rest/path/items.go
@@ -1,5 +1,10 @@
 package path
 
+import (
+	"bctbackend/database/models"
+	"fmt"
+)
+
 type itemsPath struct{}
 
 func Items() *itemsPath {
@@ -8,4 +13,12 @@ func Items() *itemsPath {
 
 func (path *itemsPath) String() string {
 	return "/api/v1/items"
+}
+
+func (path *itemsPath) Id(id models.Id) string {
+	return path.WithRawItemId(models.IdToString(id))
+}
+
+func (path *itemsPath) WithRawItemId(id string) string {
+	return fmt.Sprintf("/api/v1/items/%s", id)
 }
