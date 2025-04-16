21755328234c2db2b547a97f760ba91f18a7de3f
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-04-16 23:24:17 +0200

    added list seller items test

--- bctbackend/test/rest/list_seller_items_test.go
+++ bctbackend/test/rest/list_seller_items_test.go
@@ -15,12 +15,38 @@ import (
 )
 
 func TestListSellerItems(t *testing.T) {
-	for _, sellerId := range []models.Id{models.NewId(1), models.NewId(2), models.NewId(100)} {
-		for _, itemCount := range []int{0, 1, 5, 100} {
+	t.Run("Success", func(t *testing.T) {
+		for _, sellerId := range []models.Id{models.NewId(1), models.NewId(2), models.NewId(100)} {
+			for _, itemCount := range []int{0, 1, 5, 100} {
+				setup, router, writer := SetupRestTest()
+				defer setup.Close()
+
+				seller, sessionId := setup.LoggedIn(setup.Seller(aux.WithUserId(sellerId)))
+
+				expectedItems := []models.Item{}
+				for i := 0; i < itemCount; i++ {
+					item := setup.Item(seller.UserId, aux.WithDummyData(i))
+					expectedItems = append(expectedItems, *item)
+				}
+
+				url := path.SellerItems().WithSellerId(seller.UserId)
+				request := CreateGetRequest(url, WithCookie(sessionId))
+				router.ServeHTTP(writer, request)
+				require.Equal(t, http.StatusOK, writer.Code)
+
+				actual := FromJson[[]models.Item](writer.Body.String())
+				require.Equal(t, expectedItems, *actual)
+			}
+		}
+	})
+
+	t.Run("Failure", func(t *testing.T) {
+		t.Run("Not logged in", func(t *testing.T) {
 			setup, router, writer := SetupRestTest()
 			defer setup.Close()
 
-			seller, sessionId := setup.LoggedIn(setup.Seller(aux.WithUserId(sellerId)))
+			seller := setup.Seller()
+			itemCount := 10
 
 			expectedItems := []models.Item{}
 			for i := 0; i < itemCount; i++ {
@@ -29,12 +55,9 @@ func TestListSellerItems(t *testing.T) {
 			}
 
 			url := path.SellerItems().WithSellerId(seller.UserId)
-			request := CreateGetRequest(url, WithCookie(sessionId))
+			request := CreateGetRequest(url)
 			router.ServeHTTP(writer, request)
-			require.Equal(t, http.StatusOK, writer.Code)
-
-			actual := FromJson[[]models.Item](writer.Body.String())
-			require.Equal(t, expectedItems, *actual)
-		}
-	}
+			RequireFailureType(t, writer, http.StatusUnauthorized, "missing_session_id")
+		})
+	})
 }
