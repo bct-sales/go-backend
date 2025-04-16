70f4b6098e3c668c3d9d54f91d574de6ba4c1e1a
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-04-17 00:03:59 +0200

    added list seller items test

--- bctbackend/test/rest/list_seller_items_test.go
+++ bctbackend/test/rest/list_seller_items_test.go
@@ -17,32 +17,57 @@ import (
 
 func TestListSellerItems(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
-		for _, sellerId := range []models.Id{models.NewId(1), models.NewId(2), models.NewId(100)} {
-			for _, itemCount := range []int{0, 1, 5, 100} {
-				testLabel := fmt.Sprintf("SellerId: %d, ItemCount: %d", sellerId, itemCount)
+		t.Run("View own items", func(t *testing.T) {
+			for _, sellerId := range []models.Id{models.NewId(1), models.NewId(2), models.NewId(100)} {
+				for _, itemCount := range []int{0, 1, 5, 100} {
+					testLabel := fmt.Sprintf("SellerId: %d, ItemCount: %d", sellerId, itemCount)
 
-				t.Run(testLabel, func(t *testing.T) {
-					setup, router, writer := SetupRestTest()
-					defer setup.Close()
+					t.Run(testLabel, func(t *testing.T) {
+						setup, router, writer := SetupRestTest()
+						defer setup.Close()
 
-					seller, sessionId := setup.LoggedIn(setup.Seller(aux.WithUserId(sellerId)))
+						seller, sessionId := setup.LoggedIn(setup.Seller(aux.WithUserId(sellerId)))
 
-					expectedItems := []models.Item{}
-					for i := 0; i < itemCount; i++ {
-						item := setup.Item(seller.UserId, aux.WithDummyData(i))
-						expectedItems = append(expectedItems, *item)
-					}
+						expectedItems := []models.Item{}
+						for i := 0; i < itemCount; i++ {
+							item := setup.Item(seller.UserId, aux.WithDummyData(i))
+							expectedItems = append(expectedItems, *item)
+						}
 
-					url := path.SellerItems().WithSellerId(seller.UserId)
-					request := CreateGetRequest(url, WithCookie(sessionId))
-					router.ServeHTTP(writer, request)
-					require.Equal(t, http.StatusOK, writer.Code)
+						url := path.SellerItems().WithSellerId(seller.UserId)
+						request := CreateGetRequest(url, WithCookie(sessionId))
+						router.ServeHTTP(writer, request)
+						require.Equal(t, http.StatusOK, writer.Code)
 
-					actual := FromJson[[]models.Item](writer.Body.String())
-					require.Equal(t, expectedItems, *actual)
-				})
+						actual := FromJson[[]models.Item](writer.Body.String())
+						require.Equal(t, expectedItems, *actual)
+					})
+				}
 			}
-		}
+		})
+
+		t.Run("As admin", func(t *testing.T) {
+			setup, router, writer := SetupRestTest()
+			defer setup.Close()
+
+			seller := setup.Seller()
+			_, sessionId := setup.LoggedIn(setup.Admin())
+			itemCount := 10
+
+			expectedItems := []models.Item{}
+			for i := 0; i < itemCount; i++ {
+				item := setup.Item(seller.UserId, aux.WithDummyData(i))
+				expectedItems = append(expectedItems, *item)
+			}
+
+			url := path.SellerItems().WithSellerId(seller.UserId)
+			request := CreateGetRequest(url, WithCookie(sessionId))
+			router.ServeHTTP(writer, request)
+			require.Equal(t, http.StatusOK, writer.Code)
+
+			actual := FromJson[[]models.Item](writer.Body.String())
+			require.Equal(t, expectedItems, *actual)
+		})
 	})
 
 	t.Run("Failure", func(t *testing.T) {
