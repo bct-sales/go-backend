67fc2e034dd8aee7148c592184cdd2cd433191c8
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-04-26 15:58:43 +0200

    algorithms.Filter

--- bctbackend/algorithms/main.go
+++ bctbackend/algorithms/main.go
@@ -47,3 +47,15 @@ func Repeat(count int, function func() error) error {
 
 	return nil
 }
+
+func Filter[T any](values []T, predicate func(T) bool) []T {
+	result := make([]T, 0)
+
+	for _, value := range values {
+		if predicate(value) {
+			result = append(result, value)
+		}
+	}
+
+	return result
+}
