ab63d85ff54725ba0a3969a4ed6955bd906f30a6
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-06 12:29:37 +0200

    closes #189

--- bctbackend/test/queries/get_seller_total_price_of_all_items_test.go
+++ bctbackend/test/queries/get_seller_total_price_of_all_items_test.go
@@ -38,5 +38,30 @@ func TestSellerTotalPriceOfAllTimes(t *testing.T) {
 				})
 			}
 		})
+
+		t.Run("Multiple seller", func(t *testing.T) {
+			for _, itemCount := range []int64{0, 1, 2, 10, 100} {
+				testLabel := fmt.Sprintf("Seller with %d items", itemCount)
+				t.Run(testLabel, func(t *testing.T) {
+					setup, db := NewDatabaseFixture()
+					defer setup.Close()
+
+					seller := setup.Seller()
+					otherSeller := setup.Seller()
+
+					expectedTotal := models.MoneyInCents(0)
+					for i := int64(0); i < itemCount; i++ {
+						price := models.MoneyInCents((i + 1) * 50)
+						setup.Item(seller.UserId, aux.WithDummyData(int(i)), aux.WithPriceInCents(price))
+						setup.Item(otherSeller.UserId, aux.WithDummyData(int(i)), aux.WithPriceInCents(price))
+						expectedTotal += price
+					}
+
+					actualTotal, err := queries.GetSellerTotalPriceOfAllItems(db, seller.UserId)
+					require.NoError(t, err)
+					require.Equal(t, expectedTotal, actualTotal)
+				})
+			}
+		})
 	})
 }
