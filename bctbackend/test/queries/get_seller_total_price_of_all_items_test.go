ebb13e64572148c79aefa80fdff23bce9d6a1256
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-06 12:28:50 +0200

    closes #188

---
+++ bctbackend/test/queries/get_seller_total_price_of_all_items_test.go
@@ -0,0 +1,42 @@
+//go:build test
+
+package queries
+
+import (
+	"bctbackend/database/models"
+	"bctbackend/database/queries"
+	aux "bctbackend/test/helpers"
+	. "bctbackend/test/setup"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	_ "modernc.org/sqlite"
+)
+
+func TestSellerTotalPriceOfAllTimes(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		t.Run("Single seller", func(t *testing.T) {
+			for _, itemCount := range []int64{0, 1, 2, 10, 100} {
+				testLabel := fmt.Sprintf("Seller with %d items", itemCount)
+				t.Run(testLabel, func(t *testing.T) {
+					setup, db := NewDatabaseFixture()
+					defer setup.Close()
+
+					seller := setup.Seller()
+
+					expectedTotal := models.MoneyInCents(0)
+					for i := int64(0); i < itemCount; i++ {
+						price := models.MoneyInCents((i + 1) * 50)
+						setup.Item(seller.UserId, aux.WithDummyData(int(i)), aux.WithPriceInCents(price))
+						expectedTotal += price
+					}
+
+					actualTotal, err := queries.GetSellerTotalPriceOfAllItems(db, seller.UserId)
+					require.NoError(t, err)
+					require.Equal(t, expectedTotal, actualTotal)
+				})
+			}
+		})
+	})
+}
