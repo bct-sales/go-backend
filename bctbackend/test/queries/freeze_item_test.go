3802ed6b2f9fa19e063b30ac75b39882ea3a7b68
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-12 22:49:11 +0200

    refactoring

--- bctbackend/test/queries/freeze_item_test.go
+++ bctbackend/test/queries/freeze_item_test.go
@@ -13,28 +13,30 @@ import (
 )
 
 func TestFreezeItem(t *testing.T) {
-	t.Run("Successful freezing", func(t *testing.T) {
-		setup, db := NewDatabaseFixture()
-		defer setup.Close()
+	t.Run("Success", func(t *testing.T) {
+		t.Run("Freezing unfrozen item", func(t *testing.T) {
+			setup, db := NewDatabaseFixture()
+			defer setup.Close()
 
-		seller := setup.Seller()
-		item := setup.Item(seller.UserId, aux.WithDummyData(1), aux.WithFrozen(false))
+			seller := setup.Seller()
+			item := setup.Item(seller.UserId, aux.WithDummyData(1), aux.WithFrozen(false))
 
-		{
-			isFrozen, err := queries.IsItemFrozen(db, item.ItemId)
-			require.NoError(t, err)
-			require.False(t, isFrozen)
-		}
+			{
+				isFrozen, err := queries.IsItemFrozen(db, item.ItemId)
+				require.NoError(t, err)
+				require.False(t, isFrozen)
+			}
 
-		{
-			err := queries.FreezeItem(db, item.ItemId)
-			require.NoError(t, err)
-		}
+			{
+				err := queries.FreezeItem(db, item.ItemId)
+				require.NoError(t, err)
+			}
 
-		{
-			isFrozen, err := queries.IsItemFrozen(db, item.ItemId)
-			require.NoError(t, err)
-			require.True(t, isFrozen)
-		}
+			{
+				isFrozen, err := queries.IsItemFrozen(db, item.ItemId)
+				require.NoError(t, err)
+				require.True(t, isFrozen)
+			}
+		})
 	})
 }
