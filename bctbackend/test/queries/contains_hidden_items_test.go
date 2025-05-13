227f2a0ed5b6ea78483e701a96457e7a3aff7e04
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-13 14:37:05 +0200

    contain hidden items test

--- bctbackend/test/queries/contains_hidden_items_test.go
+++ bctbackend/test/queries/contains_hidden_items_test.go
@@ -28,5 +28,19 @@ func TestContainsHiddenItems(t *testing.T) {
 			require.NoError(t, err)
 			require.False(t, result)
 		})
+
+		t.Run("One hidden item", func(t *testing.T) {
+			setup, db := NewDatabaseFixture()
+			defer setup.Close()
+
+			seller := setup.Seller()
+			items := setup.Items(seller.UserId, 10, aux.WithHidden(false))
+			items = append(items, setup.Item(seller.UserId, aux.WithHidden(true)))
+			itemIds := algorithms.Map(items, func(item *models.Item) models.Id { return item.ItemId })
+
+			result, err := queries.ContainsHiddenItems(db, itemIds)
+			require.NoError(t, err)
+			require.True(t, result)
+		})
 	})
 }
