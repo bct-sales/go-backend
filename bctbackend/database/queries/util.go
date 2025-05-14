4cbf9c6bc9b170182f967df86452d1cdc273ca2d
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-14 17:51:12 +0200

    TransactionHelper

--- bctbackend/database/queries/util.go
+++ bctbackend/database/queries/util.go
@@ -1,6 +1,9 @@
 package queries
 
-import "strings"
+import (
+	"database/sql"
+	"strings"
+)
 
 func placeholderString(placeholderCount int) string {
 	placeholders := make([]string, placeholderCount)
@@ -9,4 +12,41 @@ func placeholderString(placeholderCount int) string {
 	}
 
 	return strings.Join(placeholders, ", ")
+}
+
+type TransactionHelper struct {
+	Transaction *sql.Tx
+	committed   bool
+}
+
+func NewTransaction(db *sql.DB) (*TransactionHelper, error) {
+	tx, err := db.Begin()
+	if err != nil {
+		return nil, err
+	}
+
+	return &TransactionHelper{
+		Transaction: tx,
+	}, nil
+}
+
+func (th *TransactionHelper) Commit() error {
+	if err := th.Transaction.Commit(); err != nil {
+		return err
+	}
+
+	th.committed = true
+	return nil
+}
+
+func (th *TransactionHelper) Rollback() error {
+	if th.committed {
+		return nil
+	}
+
+	if err := th.Transaction.Rollback(); err != nil {
+		return err
+	}
+
+	return nil
 }
