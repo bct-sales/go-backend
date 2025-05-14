81c62c028b1c816f595923241d0a4f0fc9949ad2
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-14 14:36:44 +0200

    update hidden status of items tests

---
+++ bctbackend/test/queries/update_hidden_status_of_items_test.go
@@ -0,0 +1,111 @@
+//go:build test
+
+package queries
+
+import (
+	"bctbackend/database/models"
+	"bctbackend/database/queries"
+	aux "bctbackend/test/helpers"
+	. "bctbackend/test/setup"
+	"fmt"
+	"slices"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	_ "modernc.org/sqlite"
+)
+
+func TestUpdateHiddenStatusOfItems(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		selections := [][]models.Id{
+			{},
+			{1},
+			{2},
+			{1, 2},
+			{1, 2, 3},
+			{1, 2, 3, 4},
+			{1, 2, 3, 5},
+		}
+		for _, selection := range selections {
+			testLabel := fmt.Sprintf("Selection: %v", selection)
+			t.Run(testLabel, func(t *testing.T) {
+				setup, db := NewDatabaseFixture()
+				defer setup.Close()
+
+				seller := setup.Seller()
+
+				itemIds := []models.Id{}
+				for i := 0; i != 10; i++ {
+					itemIds = append(itemIds, setup.Item(seller.UserId, aux.WithDummyData(i), aux.WithHidden(false), aux.WithFrozen(false)).ItemId)
+				}
+
+				err := queries.UpdateHiddenStatusOfItems(db, selection, true)
+				require.NoError(t, err)
+
+				for _, itemId := range itemIds {
+					isHidden, err := queries.IsItemHidden(db, itemId)
+					expectedHidden := slices.Contains(selection, itemId)
+					assert.NoError(t, err)
+					assert.Equal(t, expectedHidden, isHidden, "item %d should have hidden=%v", itemId, expectedHidden)
+				}
+			})
+		}
+	})
+
+	t.Run("Failure", func(t *testing.T) {
+		t.Run("No such item", func(t *testing.T) {
+			setup, db := NewDatabaseFixture()
+			defer setup.Close()
+
+			err := queries.UpdateHiddenStatusOfItems(db, []models.Id{1}, true)
+			require.Error(t, err)
+		})
+
+		t.Run("Cannot hide frozen item", func(t *testing.T) {
+			setup, db := NewDatabaseFixture()
+			defer setup.Close()
+
+			seller := setup.Seller()
+
+			itemIds := []models.Id{}
+			for i := 0; i != 10; i++ {
+				itemIds = append(itemIds, setup.Item(seller.UserId, aux.WithDummyData(i), aux.WithHidden(false), aux.WithFrozen(false)).ItemId)
+			}
+			itemIds = append(itemIds, setup.Item(seller.UserId, aux.WithDummyData(10), aux.WithHidden(false), aux.WithFrozen(true)).ItemId)
+
+			err := queries.UpdateHiddenStatusOfItems(db, itemIds, true)
+			var itemFrozenError *queries.ItemFrozenError
+			require.ErrorAs(t, err, &itemFrozenError)
+
+			for _, itemId := range itemIds {
+				isHidden, err := queries.IsItemHidden(db, itemId)
+				assert.NoError(t, err)
+				assert.Equal(t, false, isHidden, "item with id %d should not be hidden", itemId)
+			}
+		})
+
+		t.Run("Cannot unhide frozen item", func(t *testing.T) {
+			setup, db := NewDatabaseFixture()
+			defer setup.Close()
+
+			seller := setup.Seller()
+
+			itemIds := []models.Id{}
+			for i := 0; i != 10; i++ {
+				itemIds = append(itemIds, setup.Item(seller.UserId, aux.WithDummyData(i), aux.WithHidden(true), aux.WithFrozen(false)).ItemId)
+			}
+			itemIds = append(itemIds, setup.Item(seller.UserId, aux.WithDummyData(10), aux.WithHidden(true), aux.WithFrozen(true)).ItemId)
+
+			err := queries.UpdateHiddenStatusOfItems(db, itemIds, false)
+			var itemFrozenError *queries.ItemFrozenError
+			require.ErrorAs(t, err, &itemFrozenError)
+
+			for _, itemId := range itemIds {
+				isHidden, err := queries.IsItemHidden(db, itemId)
+				assert.NoError(t, err)
+				assert.Equal(t, true, isHidden, "item with id %d should be hidden", itemId)
+			}
+		})
+	})
+}
