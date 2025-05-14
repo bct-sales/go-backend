0781a36b8a1d036e7504ba290664dda9fa34c364
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-14 12:28:06 +0200

    ContainsHiddenItems tests

--- bctbackend/test/queries/contains_hidden_items_test.go
+++ bctbackend/test/queries/contains_hidden_items_test.go
@@ -43,13 +43,27 @@ func TestContainsHiddenItems(t *testing.T) {
 			require.True(t, result)
 		})
 
-		t.Run("Duplicate items", func(t *testing.T) {
+		t.Run("Duplicate items, no hidden items", func(t *testing.T) {
 			setup, db := NewDatabaseFixture()
 			defer setup.Close()
 
 			seller := setup.Seller()
 			items := setup.Items(seller.UserId, 10, aux.WithHidden(false))
-			items = append(items, setup.Item(seller.UserId, aux.WithHidden(true)))
+			itemIds := algorithms.Map(items, func(item *models.Item) models.Id { return item.ItemId })
+			itemIds = append(itemIds, itemIds...)
+
+			result, err := queries.ContainsHiddenItems(db, itemIds)
+			require.NoError(t, err)
+			require.False(t, result)
+		})
+
+		t.Run("Duplicate items, hidden item", func(t *testing.T) {
+			setup, db := NewDatabaseFixture()
+			defer setup.Close()
+
+			seller := setup.Seller()
+			items := setup.Items(seller.UserId, 10, aux.WithHidden(false))
+			items = append(setup.Items(seller.UserId, 10, aux.WithHidden(true)))
 			itemIds := algorithms.Map(items, func(item *models.Item) models.Id { return item.ItemId })
 			itemIds = append(itemIds, itemIds...)
 
