749e19701e3b9b34c516a221becfcd4d47059170
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-14 23:58:00 +0200

    closes #246

--- bctbackend/test/queries/get_items_test.go
+++ bctbackend/test/queries/get_items_test.go
@@ -16,31 +16,53 @@ import (
 
 func TestGetItems(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
-		for _, itemCount := range []int{0, 1, 2, 10} {
-			testLabel := fmt.Sprintf("Item count: %d", itemCount)
-			t.Run(testLabel, func(t *testing.T) {
-				setup, db := NewDatabaseFixture()
-				defer setup.Close()
+		t.Run("No hidden items included", func(t *testing.T) {
+			t.Run("No hidden items", func(t *testing.T) {
+				for _, itemCount := range []int{0, 1, 2, 10} {
+					testLabel := fmt.Sprintf("Item count: %d", itemCount)
+					t.Run(testLabel, func(t *testing.T) {
+						setup, db := NewDatabaseFixture()
+						defer setup.Close()
 
-				seller := setup.Seller()
-				items := setup.Items(seller.UserId, itemCount, aux.WithHidden(false))
+						seller := setup.Seller()
+						items := setup.Items(seller.UserId, itemCount, aux.WithHidden(false))
 
-				actualItems := []*models.Item{}
-				err := queries.GetItems(db, queries.CollectTo(&actualItems), true)
-				require.NoError(t, err)
-				require.Equal(t, itemCount, len(items))
+						actualItems := []*models.Item{}
+						err := queries.GetItems(db, queries.CollectTo(&actualItems), false)
+						require.NoError(t, err)
+						require.Equal(t, itemCount, len(actualItems))
 
-				for i, item := range items {
-					require.Equal(t, item.ItemId, actualItems[i].ItemId)
-					require.Equal(t, item.Description, actualItems[i].Description)
-					require.Equal(t, item.PriceInCents, actualItems[i].PriceInCents)
-					require.Equal(t, item.SellerId, actualItems[i].SellerId)
-					require.Equal(t, item.CategoryId, actualItems[i].CategoryId)
-					require.Equal(t, item.AddedAt, actualItems[i].AddedAt)
-					require.Equal(t, item.Frozen, actualItems[i].Frozen)
-					require.Equal(t, item.Hidden, actualItems[i].Hidden)
+						for i, item := range items {
+							require.Equal(t, item.ItemId, actualItems[i].ItemId)
+							require.Equal(t, item.Description, actualItems[i].Description)
+							require.Equal(t, item.PriceInCents, actualItems[i].PriceInCents)
+							require.Equal(t, item.SellerId, actualItems[i].SellerId)
+							require.Equal(t, item.CategoryId, actualItems[i].CategoryId)
+							require.Equal(t, item.AddedAt, actualItems[i].AddedAt)
+							require.Equal(t, item.Frozen, actualItems[i].Frozen)
+							require.Equal(t, item.Hidden, actualItems[i].Hidden)
+						}
+					})
 				}
 			})
-		}
+
+			t.Run("With hidden items", func(t *testing.T) {
+				for _, itemCount := range []int{0, 1, 2, 10} {
+					testLabel := fmt.Sprintf("Item count: %d", itemCount)
+					t.Run(testLabel, func(t *testing.T) {
+						setup, db := NewDatabaseFixture()
+						defer setup.Close()
+
+						seller := setup.Seller()
+						setup.Items(seller.UserId, itemCount, aux.WithHidden(true))
+
+						actualItems := []*models.Item{}
+						err := queries.GetItems(db, queries.CollectTo(&actualItems), false)
+						require.NoError(t, err)
+						require.Equal(t, 0, len(actualItems))
+					})
+				}
+			})
+		})
 	})
 }
