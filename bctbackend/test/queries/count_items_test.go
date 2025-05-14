fcd2fe2153333c290ae2668d559f73ae47ee27f0
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-14 19:16:43 +0200

    refactoring

--- bctbackend/test/queries/count_items_test.go
+++ bctbackend/test/queries/count_items_test.go
@@ -13,37 +13,39 @@ import (
 )
 
 func TestCountItems(t *testing.T) {
-	t.Run("Empty", func(t *testing.T) {
-		setup, db := NewDatabaseFixture()
-		defer setup.Close()
+	t.Run("Success", func(t *testing.T) {
+		t.Run("Empty", func(t *testing.T) {
+			setup, db := NewDatabaseFixture()
+			defer setup.Close()
 
-		count, err := queries.CountItems(db)
-		require.NoError(t, err)
-		require.Equal(t, 0, count)
-	})
+			count, err := queries.CountItems(db)
+			require.NoError(t, err)
+			require.Equal(t, 0, count)
+		})
 
-	t.Run("One item", func(t *testing.T) {
-		setup, db := NewDatabaseFixture()
-		defer setup.Close()
+		t.Run("One item", func(t *testing.T) {
+			setup, db := NewDatabaseFixture()
+			defer setup.Close()
 
-		seller := setup.Seller()
-		setup.Item(seller.UserId, aux.WithDummyData(1))
+			seller := setup.Seller()
+			setup.Item(seller.UserId, aux.WithDummyData(1))
 
-		count, err := queries.CountItems(db)
-		require.NoError(t, err)
-		require.Equal(t, 1, count)
-	})
+			count, err := queries.CountItems(db)
+			require.NoError(t, err)
+			require.Equal(t, 1, count)
+		})
 
-	t.Run("Two items", func(t *testing.T) {
-		setup, db := NewDatabaseFixture()
-		defer setup.Close()
+		t.Run("Two items", func(t *testing.T) {
+			setup, db := NewDatabaseFixture()
+			defer setup.Close()
 
-		seller := setup.Seller()
-		setup.Item(seller.UserId, aux.WithDummyData(1))
-		setup.Item(seller.UserId, aux.WithDummyData(2))
+			seller := setup.Seller()
+			setup.Item(seller.UserId, aux.WithDummyData(1))
+			setup.Item(seller.UserId, aux.WithDummyData(2))
 
-		count, err := queries.CountItems(db)
-		require.NoError(t, err)
-		require.Equal(t, 2, count)
+			count, err := queries.CountItems(db)
+			require.NoError(t, err)
+			require.Equal(t, 2, count)
+		})
 	})
 }
