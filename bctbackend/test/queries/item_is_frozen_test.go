db987379d7c1628a62d00060c8351954178bf521
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-15 21:59:00 +0200

    refactoring

--- bctbackend/test/queries/item_is_frozen_test.go
+++ bctbackend/test/queries/item_is_frozen_test.go
@@ -15,28 +15,32 @@ import (
 )
 
 func TestItemIsFrozen(t *testing.T) {
-	for _, frozen := range []bool{true, false} {
-		t.Run("Frozen "+strconv.FormatBool(frozen), func(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		for _, frozen := range []bool{true, false} {
+			t.Run("Frozen "+strconv.FormatBool(frozen), func(t *testing.T) {
+				setup, db := NewDatabaseFixture()
+				defer setup.Close()
+
+				seller := setup.Seller()
+				item := setup.Item(seller.UserId, aux.WithDummyData(1), aux.WithFrozen(frozen), aux.WithHidden(false))
+
+				actual, err := queries.IsItemFrozen(db, item.ItemId)
+				require.NoError(t, err)
+				require.Equal(t, frozen, actual)
+			})
+		}
+	})
+
+	t.Run("Failure", func(t *testing.T) {
+		t.Run("No such item", func(t *testing.T) {
 			setup, db := NewDatabaseFixture()
 			defer setup.Close()
 
-			seller := setup.Seller()
-			item := setup.Item(seller.UserId, aux.WithDummyData(1), aux.WithFrozen(frozen), aux.WithHidden(false))
+			invalidId := models.Id(1)
 
-			actual, err := queries.IsItemFrozen(db, item.ItemId)
-			require.NoError(t, err)
-			require.Equal(t, frozen, actual)
+			_, err := queries.IsItemFrozen(db, invalidId)
+			var noSuchItemError *queries.NoSuchItemError
+			require.ErrorAs(t, err, &noSuchItemError)
 		})
-	}
-
-	t.Run("No such item", func(t *testing.T) {
-		setup, db := NewDatabaseFixture()
-		defer setup.Close()
-
-		invalidId := models.Id(1)
-
-		_, err := queries.IsItemFrozen(db, invalidId)
-		var noSuchItemError *queries.NoSuchItemError
-		require.ErrorAs(t, err, &noSuchItemError)
 	})
 }
