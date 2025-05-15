fe3d682f59222aa05c424b6de2b01e58a27dfb10
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-15 21:34:13 +0200

    closes #237

---
+++ bctbackend/test/queries/item_is_hidden_test.go
@@ -0,0 +1,47 @@
+//go:build test
+
+package queries
+
+import (
+	"bctbackend/database/models"
+	"bctbackend/database/queries"
+	aux "bctbackend/test/helpers"
+	. "bctbackend/test/setup"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	_ "modernc.org/sqlite"
+)
+
+func TestItemIsHidden(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		for _, hidden := range []bool{true, false} {
+			testLabel := fmt.Sprintf("Hidden %t", hidden)
+			t.Run(testLabel, func(t *testing.T) {
+				setup, db := NewDatabaseFixture()
+				defer setup.Close()
+
+				seller := setup.Seller()
+				item := setup.Item(seller.UserId, aux.WithDummyData(1), aux.WithFrozen(false), aux.WithHidden(hidden))
+
+				actual, err := queries.IsItemHidden(db, item.ItemId)
+				require.NoError(t, err)
+				require.Equal(t, hidden, actual)
+			})
+		}
+	})
+
+	t.Run("Failure", func(t *testing.T) {
+		t.Run("No such item", func(t *testing.T) {
+			setup, db := NewDatabaseFixture()
+			defer setup.Close()
+
+			invalidId := models.Id(1)
+
+			_, err := queries.IsItemHidden(db, invalidId)
+			var noSuchItemError *queries.NoSuchItemError
+			require.ErrorAs(t, err, &noSuchItemError)
+		})
+	})
+}
