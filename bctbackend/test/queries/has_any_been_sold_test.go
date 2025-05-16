9699eea65a0eff3f5308b041595cb58117586595
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-16 18:01:44 +0200

    has any been sold tests

---
+++ bctbackend/test/queries/has_any_been_sold_test.go
@@ -0,0 +1,76 @@
+//go:build test
+
+package queries
+
+import (
+	"bctbackend/algorithms"
+	models "bctbackend/database/models"
+	"bctbackend/database/queries"
+	aux "bctbackend/test/helpers"
+	. "bctbackend/test/setup"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	_ "modernc.org/sqlite"
+)
+
+func TestHasAnyBeenSold(t *testing.T) {
+	t.Run("Single unsold item", func(t *testing.T) {
+		setup, db := NewDatabaseFixture()
+		defer setup.Close()
+
+		seller := setup.Seller()
+
+		item := setup.Item(seller.UserId, aux.WithDummyData(1), aux.WithHidden(false))
+
+		actual, err := queries.HasAnyBeenSold(db, []models.Id{item.ItemId})
+		require.NoError(t, err)
+		require.False(t, actual)
+	})
+
+	t.Run("Single sold item", func(t *testing.T) {
+		setup, db := NewDatabaseFixture()
+		defer setup.Close()
+
+		seller := setup.Seller()
+		cashier := setup.Cashier()
+
+		item := setup.Item(seller.UserId, aux.WithDummyData(1), aux.WithHidden(false))
+		setup.Sale(cashier.UserId, []models.Id{item.ItemId})
+
+		actual, err := queries.HasAnyBeenSold(db, []models.Id{item.ItemId})
+		require.NoError(t, err)
+		require.True(t, actual)
+	})
+
+	t.Run("Multiple unsold items", func(t *testing.T) {
+		setup, db := NewDatabaseFixture()
+		defer setup.Close()
+
+		seller := setup.Seller()
+
+		items := setup.Items(seller.UserId, 10, aux.WithHidden(false))
+		itemIds := algorithms.Map(items, func(item *models.Item) models.Id { return item.ItemId })
+
+		actual, err := queries.HasAnyBeenSold(db, itemIds)
+		require.NoError(t, err)
+		require.False(t, actual)
+	})
+
+	t.Run("Multiple unsold items", func(t *testing.T) {
+		setup, db := NewDatabaseFixture()
+		defer setup.Close()
+
+		seller := setup.Seller()
+		cashier := setup.Cashier()
+
+		items := setup.Items(seller.UserId, 10, aux.WithHidden(false))
+		itemIds := algorithms.Map(items, func(item *models.Item) models.Id { return item.ItemId })
+
+		setup.Sale(cashier.UserId, itemIds)
+
+		actual, err := queries.HasAnyBeenSold(db, itemIds)
+		require.NoError(t, err)
+		require.True(t, actual)
+	})
+}
