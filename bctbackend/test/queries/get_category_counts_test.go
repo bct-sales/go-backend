d2b6f8c8c0aa8d927b5f6eea6aa8dc85df54b893
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-20 14:46:51 +0200

    updated get category counts test

--- bctbackend/test/queries/get_category_counts_test.go
+++ bctbackend/test/queries/get_category_counts_test.go
@@ -3,6 +3,7 @@
 package queries
 
 import (
+	"bctbackend/database/models"
 	"bctbackend/database/queries"
 	"bctbackend/defs"
 	aux "bctbackend/test/helpers"
@@ -18,7 +19,7 @@ func TestGetCategoryCounts(t *testing.T) {
 
 	t.Run("Success", func(t *testing.T) {
 		t.Run("Without hidden items", func(t *testing.T) {
-			countTables := []map[defs.Id]int64{
+			countTables := []map[models.Id]int64{
 				{},
 				{
 					CategoryId_Clothing50_56: 1,
@@ -131,28 +132,28 @@ func TestGetCategoryCounts(t *testing.T) {
 				countTables := []map[defs.Id]int64{
 					{},
 					{
-						defs.Clothing50_56: 1,
+						CategoryId_Clothing50_56: 1,
 					},
 					{
-						defs.Clothing50_56: 2,
+						CategoryId_Clothing50_56: 2,
 					},
 					{
-						defs.Clothing50_56: 2,
-						defs.Toys:          3,
+						CategoryId_Clothing50_56: 2,
+						CategoryId_Toys:          3,
 					},
 					{
-						defs.Clothing50_56:      1,
-						defs.Clothing56_62:      2,
-						defs.Clothing68_80:      3,
-						defs.Clothing86_92:      4,
-						defs.Clothing92_98:      5,
-						defs.Clothing104_116:    6,
-						defs.Clothing122_128:    7,
-						defs.Clothing128_140:    8,
-						defs.Clothing140_152:    9,
-						defs.Shoes:              10,
-						defs.Toys:               11,
-						defs.BabyChildEquipment: 12,
+						CategoryId_Clothing50_56:      1,
+						CategoryId_Clothing56_62:      2,
+						CategoryId_Clothing68_80:      3,
+						CategoryId_Clothing86_92:      4,
+						CategoryId_Clothing92_98:      5,
+						CategoryId_Clothing104_116:    6,
+						CategoryId_Clothing122_128:    7,
+						CategoryId_Clothing128_140:    8,
+						CategoryId_Clothing140_152:    9,
+						CategoryId_Shoes:              10,
+						CategoryId_Toys:               11,
+						CategoryId_BabyChildEquipment: 12,
 					},
 				}
 
@@ -170,10 +171,10 @@ func TestGetCategoryCounts(t *testing.T) {
 
 					counts, err := queries.GetCategoryCounts(db, true)
 					require.NoError(t, err)
-					require.Equal(t, len(defs.ListCategoryIds()), len(counts))
+					require.Equal(t, len(defaultCategoryTable), len(counts))
 
 					for _, count := range counts {
-						require.Contains(t, defs.ListCategoryIds(), count.CategoryId)
+						require.Contains(t, defaultCategoryTable, count.CategoryId)
 
 						expectedCount := countTable[count.CategoryId]
 						actualCount := count.Count
