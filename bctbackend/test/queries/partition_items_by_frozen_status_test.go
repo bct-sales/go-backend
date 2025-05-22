8d60f86f31db89d67d21327f2d309f92b8739a70
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-23 00:53:19 +0200

    closes #264

--- bctbackend/test/queries/partition_items_by_frozen_status_test.go
+++ bctbackend/test/queries/partition_items_by_frozen_status_test.go
@@ -70,5 +70,25 @@ func TestPartitionItemsByFrozenStatus(t *testing.T) {
 			require.Equal(t, len(frozenItems), actualFrozen.Len())
 			require.Equal(t, len(unfrozenItems), actualUnfrozen.Len())
 		})
+
+		t.Run("All items are hidden, nonexistent items in the list", func(t *testing.T) {
+			setup, db := NewDatabaseFixture(WithDefaultCategories)
+			defer setup.Close()
+
+			seller := setup.Seller()
+			frozenItems := setup.Items(seller.UserId, 10, aux.WithFrozen(true), aux.WithHidden(true))
+			unfrozenItems := setup.Items(seller.UserId, 5, aux.WithFrozen(false), aux.WithHidden(true))
+			nonexistentItems := []models.Id{999, 1000, 1001}
+			// setup.RequireNoSuchItem(t, nonexistentItems)
+			allItems := slices.Concat(
+				algorithms.Map(frozenItems, func(i *models.Item) models.Id { return i.ItemId }),
+				algorithms.Map(unfrozenItems, func(i *models.Item) models.Id { return i.ItemId }),
+				nonexistentItems)
+
+			actualUnfrozen, actualFrozen, err := queries.PartitionItemsByFrozenStatus(db, allItems)
+			require.NoError(t, err)
+			require.Equal(t, len(frozenItems), actualFrozen.Len())
+			require.Equal(t, len(unfrozenItems), actualUnfrozen.Len())
+		})
 	})
 }
