5c0f2536e0bdd8030fc276f9fb67d4f45a5e98c4
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-23 01:20:16 +0200

    refactoring

--- bctbackend/test/rest/list_categories.go
+++ bctbackend/test/rest/list_categories.go
@@ -23,41 +23,43 @@ type GetCategoriesSuccessResponse struct {
 
 func TestGetCategories(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
-		t.Run("Without counts", func(t *testing.T) {
-			setup, router, writer := NewRestFixture(WithDefaultCategories)
-			defer setup.Close()
+		t.Run("As admin", func(t *testing.T) {
+			t.Run("Without counts", func(t *testing.T) {
+				setup, router, writer := NewRestFixture(WithDefaultCategories)
+				defer setup.Close()
 
-			_, sessionId := setup.LoggedIn(setup.Admin())
+				_, sessionId := setup.LoggedIn(setup.Admin())
 
-			url := path.Categories().String()
-			request := CreateGetRequest(url, WithSessionCookie(sessionId))
-			router.ServeHTTP(writer, request)
-			require.Equal(t, http.StatusOK, writer.Code)
+				url := path.Categories().String()
+				request := CreateGetRequest(url, WithSessionCookie(sessionId))
+				router.ServeHTTP(writer, request)
+				require.Equal(t, http.StatusOK, writer.Code)
 
-			actual := FromJson[GetCategoriesSuccessResponse](t, writer.Body.String())
+				actual := FromJson[GetCategoriesSuccessResponse](t, writer.Body.String())
 
-			for _, category := range actual.categories {
-				require.Nil(t, category.Count)
-			}
-		})
+				for _, category := range actual.categories {
+					require.Nil(t, category.Count)
+				}
+			})
 
-		t.Run("With counts", func(t *testing.T) {
-			setup, router, writer := NewRestFixture(WithDefaultCategories)
-			defer setup.Close()
+			t.Run("With counts", func(t *testing.T) {
+				setup, router, writer := NewRestFixture(WithDefaultCategories)
+				defer setup.Close()
 
-			_, sessionId := setup.LoggedIn(setup.Admin())
+				_, sessionId := setup.LoggedIn(setup.Admin())
 
-			url := path.Categories().String()
-			request := CreateGetRequest(url, WithSessionCookie(sessionId))
-			router.ServeHTTP(writer, request)
-			require.Equal(t, http.StatusOK, writer.Code)
+				url := path.Categories().String()
+				request := CreateGetRequest(url, WithSessionCookie(sessionId))
+				router.ServeHTTP(writer, request)
+				require.Equal(t, http.StatusOK, writer.Code)
 
-			actual := FromJson[GetCategoriesSuccessResponse](t, writer.Body.String())
+				actual := FromJson[GetCategoriesSuccessResponse](t, writer.Body.String())
 
-			for _, category := range actual.categories {
-				require.NotNil(t, category.Count)
-				require.Equal(t, int64(0), *category.Count)
-			}
+				for _, category := range actual.categories {
+					require.NotNil(t, category.Count)
+					require.Equal(t, int64(0), *category.Count)
+				}
+			})
 		})
 	})
 }
