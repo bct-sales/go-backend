6650f0435fd4b91d8865da634a97cdc1b7277d0e
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-23 01:16:19 +0200

    closes #263

---
+++ bctbackend/test/queries/partition_items_by_hidden_status_test.go
@@ -0,0 +1,57 @@
+//go:build test
+
+package queries
+
+import (
+	"bctbackend/algorithms"
+	"bctbackend/database/models"
+	"bctbackend/database/queries"
+	aux "bctbackend/test/helpers"
+	. "bctbackend/test/setup"
+	"slices"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	_ "modernc.org/sqlite"
+)
+
+func TestPartitionItemsByHiddenStatus(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		t.Run("No nonexistent items in list", func(t *testing.T) {
+			setup, db := NewDatabaseFixture(WithDefaultCategories)
+			defer setup.Close()
+
+			seller := setup.Seller()
+			visibleItems := setup.Items(seller.UserId, 10, aux.WithHidden(false))
+			hiddenItems := setup.Items(seller.UserId, 5, aux.WithHidden(true))
+			allItems := slices.Concat(
+				algorithms.Map(visibleItems, func(i *models.Item) models.Id { return i.ItemId }),
+				algorithms.Map(hiddenItems, func(i *models.Item) models.Id { return i.ItemId }))
+
+			actualVisible, actualHidden, err := queries.PartitionItemsByHiddenStatus(db, allItems)
+			require.NoError(t, err)
+			require.Equal(t, len(visibleItems), actualVisible.Len())
+			require.Equal(t, len(hiddenItems), actualHidden.Len())
+		})
+
+		t.Run("All items are hidden, no nonexistent items in list", func(t *testing.T) {
+			setup, db := NewDatabaseFixture(WithDefaultCategories)
+			defer setup.Close()
+
+			seller := setup.Seller()
+			visibleItems := setup.Items(seller.UserId, 10, aux.WithHidden(false))
+			hiddenItems := setup.Items(seller.UserId, 5, aux.WithHidden(true))
+			nonexistentItemIds := []models.Id{999, 1000}
+			setup.RequireNoSuchItems(t, nonexistentItemIds...)
+			allItems := slices.Concat(
+				algorithms.Map(visibleItems, func(i *models.Item) models.Id { return i.ItemId }),
+				algorithms.Map(hiddenItems, func(i *models.Item) models.Id { return i.ItemId }),
+				nonexistentItemIds)
+
+			actualVisible, actualHidden, err := queries.PartitionItemsByHiddenStatus(db, allItems)
+			require.NoError(t, err)
+			require.Equal(t, len(visibleItems), actualVisible.Len())
+			require.Equal(t, len(hiddenItems), actualHidden.Len())
+		})
+	})
+}
