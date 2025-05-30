8c20f6490fdf4c4544c9ea03e5bbe9fe36d2953e
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-30 20:04:08 +0200

    closes #339

--- bctbackend/test/queries/delete_session_test.go
+++ bctbackend/test/queries/delete_session_test.go
@@ -14,17 +14,28 @@ import (
 )
 
 func TestDeleteSession(t *testing.T) {
-	setup, db := NewDatabaseFixture(WithDefaultCategories)
-	defer setup.Close()
+	t.Run("Success", func(t *testing.T) {
+		setup, db := NewDatabaseFixture(WithDefaultCategories)
+		defer setup.Close()
 
-	user := setup.Admin()
-	expirationTime := models.Timestamp(0)
-	sessionId, err := queries.AddSession(db, user.UserId, expirationTime)
-	require.NoError(t, err)
+		user := setup.Admin()
+		expirationTime := models.Timestamp(0)
+		sessionId, err := queries.AddSession(db, user.UserId, expirationTime)
+		require.NoError(t, err)
 
-	err = queries.DeleteSession(db, sessionId)
-	require.NoError(t, err)
+		err = queries.DeleteSession(db, sessionId)
+		require.NoError(t, err)
 
-	_, err = queries.GetSessionById(db, sessionId)
-	require.ErrorIs(t, err, database.ErrNoSuchSession)
+		_, err = queries.GetSessionById(db, sessionId)
+		require.ErrorIs(t, err, database.ErrNoSuchSession)
+	})
+
+	t.Run("Failure", func(t *testing.T) {
+		setup, db := NewDatabaseFixture(WithDefaultCategories)
+		defer setup.Close()
+
+		nonexistentSessionId := "nonexistent-session-id"
+		err := queries.DeleteSession(db, nonexistentSessionId)
+		require.ErrorIs(t, err, database.ErrNoSuchSession)
+	})
 }
