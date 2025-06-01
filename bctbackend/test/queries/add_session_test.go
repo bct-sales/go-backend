e586ff3aa6979a747b81f9522ce7e3786b9aea7e
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-01 20:03:56 +0200

    refactoring

--- bctbackend/test/queries/add_session_test.go
+++ bctbackend/test/queries/add_session_test.go
@@ -13,19 +13,21 @@ import (
 )
 
 func TestAddSession(t *testing.T) {
-	for _, roleId := range []models.Id{models.AdminRoleId, models.CashierRoleId, models.SellerRoleId} {
-		setup, db := NewDatabaseFixture(WithDefaultCategories)
-		defer setup.Close()
+	t.Run("Success", func(t *testing.T) {
+		for _, roleId := range []models.Id{models.AdminRoleId, models.CashierRoleId, models.SellerRoleId} {
+			setup, db := NewDatabaseFixture(WithDefaultCategories)
+			defer setup.Close()
 
-		user := setup.User(roleId)
-		expirationTime := models.Timestamp(0)
-		sessionId, err := queries.AddSession(db, user.UserId, expirationTime)
-		require.NoError(t, err)
+			user := setup.User(roleId)
+			expirationTime := models.Timestamp(0)
+			sessionId, err := queries.AddSession(db, user.UserId, expirationTime)
+			require.NoError(t, err)
 
-		session, err := queries.GetSessionById(db, sessionId)
-		require.NoError(t, err)
-		require.Equal(t, sessionId, session.SessionId)
-		require.Equal(t, user.UserId, session.UserId)
-		require.Equal(t, expirationTime, session.ExpirationTime)
-	}
+			session, err := queries.GetSessionById(db, sessionId)
+			require.NoError(t, err)
+			require.Equal(t, sessionId, session.SessionId)
+			require.Equal(t, user.UserId, session.UserId)
+			require.Equal(t, expirationTime, session.ExpirationTime)
+		}
+	})
 }
