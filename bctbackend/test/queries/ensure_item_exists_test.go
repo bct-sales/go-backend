1ef3ca04e236d6983ff10c98b7ec8f36727606df
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-01 18:14:31 +0200

    closes #307

---
+++ bctbackend/test/queries/ensure_item_exists_test.go
@@ -0,0 +1,58 @@
+//go:build test
+
+package queries
+
+import (
+	"bctbackend/algorithms"
+	"bctbackend/database"
+	"bctbackend/database/models"
+	"bctbackend/database/queries"
+	aux "bctbackend/test/helpers"
+	. "bctbackend/test/setup"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	_ "modernc.org/sqlite"
+)
+
+func TestEnsureItemsExist(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		t.Run("Visible items", func(t *testing.T) {
+			setup, db := NewDatabaseFixture(WithDefaultCategories)
+			defer setup.Close()
+
+			seller := setup.Seller()
+			items := setup.Items(seller.UserId, 10, aux.WithFrozen(false), aux.WithHidden(false))
+			itemIds := algorithms.Map(items, func(item *models.Item) models.Id { return item.ItemId })
+
+			err := queries.EnsureItemsExist(db, itemIds)
+			require.NoError(t, err)
+		})
+
+		t.Run("Hidden items", func(t *testing.T) {
+			setup, db := NewDatabaseFixture(WithDefaultCategories)
+			defer setup.Close()
+
+			seller := setup.Seller()
+			items := setup.Items(seller.UserId, 10, aux.WithFrozen(false), aux.WithHidden(true))
+			itemIds := algorithms.Map(items, func(item *models.Item) models.Id { return item.ItemId })
+
+			err := queries.EnsureItemsExist(db, itemIds)
+			require.NoError(t, err)
+		})
+	})
+
+	t.Run("Failure", func(t *testing.T) {
+		setup, db := NewDatabaseFixture(WithDefaultCategories)
+		defer setup.Close()
+
+		seller := setup.Seller()
+		items := setup.Items(seller.UserId, 10, aux.WithFrozen(false), aux.WithHidden(false))
+		nonexistentItemId := models.Id(150)
+		setup.RequireNoSuchItems(t, nonexistentItemId)
+		itemIds := append(algorithms.Map(items, func(item *models.Item) models.Id { return item.ItemId }), nonexistentItemId)
+
+		err := queries.EnsureItemsExist(db, itemIds)
+		require.ErrorIs(t, err, database.ErrNoSuchItem)
+	})
+}
