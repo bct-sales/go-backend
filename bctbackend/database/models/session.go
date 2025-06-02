06e5c3685106ccd8e2d7b532cdaf4165aa7b34b7
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-02 14:51:25 +0200

    closes #371

--- bctbackend/database/models/session.go
+++ bctbackend/database/models/session.go
@@ -7,11 +7,3 @@ type Session struct {
 	UserId         Id
 	ExpirationTime Timestamp
 }
-
-func NewSession(sessionId SessionId, userId Id, expirationTime Timestamp) *Session {
-	return &Session{
-		SessionId:      sessionId,
-		UserId:         userId,
-		ExpirationTime: expirationTime,
-	}
-}
