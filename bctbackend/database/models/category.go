90d128e4bc19f6d5319e7df3dcc6e039d92761fd
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-02 14:46:43 +0200

    closes #367

--- bctbackend/database/models/category.go
+++ bctbackend/database/models/category.go
@@ -5,13 +5,6 @@ type ItemCategory struct {
 	Name       string
 }
 
-func NewCategory(id Id, name string) *ItemCategory {
-	return &ItemCategory{
-		CategoryId: id,
-		Name:       name,
-	}
-}
-
 func IsValidCategoryName(name string) bool {
 	return len(name) > 0
 }
