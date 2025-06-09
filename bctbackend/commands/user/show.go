b913bc2b8672b3cd2f5d44bfd6ca1bfab298e13b
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-09 21:45:06 +0200

    refactoring

--- bctbackend/commands/user/show.go
+++ bctbackend/commands/user/show.go
@@ -15,55 +15,72 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+type showUserCommand struct {
+	common.Command
+}
+
 func NewUserShowCommand() *cobra.Command {
-	command := cobra.Command{
-		Use:   "show <user-id>",
-		Short: "Show user info",
-		Long:  `This command shows detailed information about a specified user.`,
-		Args:  cobra.ExactArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return common.WithOpenedDatabase(cmd.ErrOrStderr(), func(db *sql.DB) error {
-				// Parse the user ID from the first argument
-				userId, err := models.ParseId(args[0])
-				if err != nil {
-					fmt.Fprintf(cmd.ErrOrStderr(), "Invalid user ID: %s\n", args[0])
-					return err
-				}
+	var command *showUserCommand
 
-				// Fetch user information from the database
-				user, err := queries.GetUserWithId(db, userId)
-				if err != nil {
-					if errors.Is(err, database.ErrNoSuchUser) {
-						return cli.Exit("User with the given id does not exist", 1)
-					}
-
-					return cli.Exit(fmt.Sprintf("Failed to get user: %s", err.Error()), 1)
-				}
-
-				// Display user information based on their role
-				switch user.RoleId {
-				case models.AdminRoleId:
-					return showAdmin(user)
-				case models.SellerRoleId:
-					return showSeller(db, user)
-				case models.CashierRoleId:
-					return showCashier(db, user)
-				default:
-					return cli.Exit(fmt.Sprintf("Bug encountered: user has unrecognized role %d", user.RoleId), 1)
-				}
-			})
+	command = &showUserCommand{
+		Command: common.Command{
+			CobraCommand: &cobra.Command{
+				Use:   "show <user-id>",
+				Short: "Show user info",
+				Long:  `This command shows detailed information about a specified user.`,
+				Args:  cobra.ExactArgs(1),
+				RunE: func(cmd *cobra.Command, args []string) error {
+					return command.execute(args)
+				},
+			},
 		},
 	}
 
-	return &command
+	return command.AsCobraCommand()
 }
 
-func showAdmin(user *models.User) error {
+func (c *showUserCommand) execute(args []string) error {
+	return c.WithOpenedDatabase(func(db *sql.DB) error {
+		// Parse the user ID from the first argument
+		userId, err := models.ParseId(args[0])
+		if err != nil {
+			c.PrintErrorf("Invalid user ID: %s\n", args[0])
+			return err
+		}
+
+		// Fetch user information from the database
+		user, err := queries.GetUserWithId(db, userId)
+		if err != nil {
+			if errors.Is(err, database.ErrNoSuchUser) {
+				c.PrintErrorf("User with ID %d does not exist.\n", userId)
+				return err
+			}
+
+			c.PrintErrorf("Failed to get user with ID %d: %s\n", userId, err.Error())
+			return err
+		}
+
+		// Display user information based on their role
+		switch user.RoleId {
+		case models.AdminRoleId:
+			return c.showAdmin(user)
+		case models.SellerRoleId:
+			return c.showSeller(db, user)
+		case models.CashierRoleId:
+			return c.showCashier(db, user)
+		default:
+			c.PrintErrorf("Bug encountered: user has unrecognized role %d\n", user.RoleId)
+			return database.ErrNoSuchRole
+		}
+	})
+}
+
+func (c *showUserCommand) showAdmin(user *models.User) error {
 	pterm.DefaultSection.Println("User Data")
 	return formatting.PrintUser(user)
 }
 
-func showSeller(db *sql.DB, user *models.User) error {
+func (c *showUserCommand) showSeller(db *sql.DB, user *models.User) error {
 	pterm.DefaultSection.Println("User Data")
 
 	err := formatting.PrintUser(user)
@@ -91,7 +108,7 @@ func showSeller(db *sql.DB, user *models.User) error {
 	return nil
 }
 
-func showCashier(db *sql.DB, user *models.User) error {
+func (c *showUserCommand) showCashier(db *sql.DB, user *models.User) error {
 	pterm.DefaultSection.Println("User Data")
 	if err := formatting.PrintUser(user); err != nil {
 		return cli.Exit(fmt.Sprintf("Failed to print user data: %s", err.Error()), 1)
