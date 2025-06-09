8363ecc8ba4dcc85a0da152f90892317e07d7b88
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-09 22:06:22 +0200

    RoleId.Name

--- bctbackend/database/models/id.go
+++ bctbackend/database/models/id.go
@@ -23,3 +23,13 @@ func (id Id) String() string {
 func (id Id) Int64() int64 {
 	return int64(id)
 }
+
+func (id Id) Name() string {
+	name, err := NameOfRole(id)
+
+	if err != nil {
+		panic("unknown role id: " + id.String())
+	}
+
+	return name
+}
