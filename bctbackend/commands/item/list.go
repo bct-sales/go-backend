ffb7ebe8f53a2fb26af666430ea6af15e79c9062
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-10 20:18:21 +0200

    refactoring

--- bctbackend/commands/item/list.go
+++ bctbackend/commands/item/list.go
@@ -13,37 +13,50 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func NewItemListCommand() *cobra.Command {
-	var showHidden bool
-	var format string
+type listItemsCommand struct {
+	common.Command
+	showHidden bool
+	format     string
+}
 
-	command := cobra.Command{
-		Use:   "list",
-		Short: "List all items",
-		Long:  `This command lists all items in the database.`,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			switch format {
-			case "table":
-				return listItemsInTableFormat(cmd, showHidden)
-			case "csv":
-				return listItemsInCSVFormat(cmd, showHidden)
-			default:
-				fmt.Fprintf(cmd.ErrOrStderr(), "Invalid format: %s\n", format)
-				return fmt.Errorf("unknown format: %s", format)
-			}
+func NewItemListCommand() *cobra.Command {
+	var command *listItemsCommand
+
+	command = &listItemsCommand{
+		Command: common.Command{
+			CobraCommand: &cobra.Command{
+				Use:   "list",
+				Short: "List all items",
+				Long:  `This command lists all items in the database.`,
+				RunE: func(cmd *cobra.Command, args []string) error {
+					return command.execute()
+				},
+			},
 		},
 	}
 
-	command.Flags().BoolVar(&showHidden, "show-hidden", false, "Show hidden items")
-	command.Flags().StringVar(&format, "format", "table", "Output format (format, csv)")
+	command.CobraCommand.Flags().BoolVar(&command.showHidden, "show-hidden", false, "Show hidden items")
+	command.CobraCommand.Flags().StringVar(&command.format, "format", "table", "Output format (format, csv)")
 
-	return &command
+	return command.AsCobraCommand()
 }
 
-func listItemsInTableFormat(cmd *cobra.Command, showHidden bool) error {
-	return common.WithOpenedDatabase(cmd.ErrOrStderr(), func(db *sql.DB) error {
+func (c *listItemsCommand) execute() error {
+	switch c.format {
+	case "table":
+		return c.listItemsInTableFormat()
+	case "csv":
+		return c.listItemsInCSVFormat()
+	default:
+		c.PrintErrorf("Invalid format: %s\n", c.format)
+		return fmt.Errorf("unknown format: %s", c.format)
+	}
+}
+
+func (c *listItemsCommand) listItemsInTableFormat() error {
+	return c.WithOpenedDatabase(func(db *sql.DB) error {
 		var hiddenStrategy queries.ItemSelection
-		if showHidden {
+		if c.showHidden {
 			hiddenStrategy = queries.AllItems
 		} else {
 			hiddenStrategy = queries.OnlyVisibleItems
@@ -51,51 +64,49 @@ func listItemsInTableFormat(cmd *cobra.Command, showHidden bool) error {
 
 		items := []*models.Item{}
 		if err := queries.GetItems(db, queries.CollectTo(&items), hiddenStrategy); err != nil {
-			fmt.Fprintf(cmd.ErrOrStderr(), "Error while getting items: %v\n", err)
+			c.PrintErrorf("Error while getting items: %v\n", err)
 			return err
 		}
 
 		itemCount := len(items)
 
 		if itemCount > 0 {
-			categoryNameTable, err := queries.GetCategoryNameTable(db)
+			categoryNameTable, err := c.GetCategoryNameTable(db)
 			if err != nil {
-				fmt.Fprintf(cmd.ErrOrStderr(), "An error occurred while trying to get the category table: %v\n", err)
 				return err
 			}
 
 			if err := formatting.PrintItems(categoryNameTable, items); err != nil {
-				fmt.Fprintf(cmd.ErrOrStderr(), "Error while printing items: %v\n", err)
+				c.PrintErrorf("Error while printing items\n")
 				return err
 			}
 
-			fmt.Printf("Number of items listed: %d\n", itemCount)
-
+			c.Printf("Number of items listed: %d\n", itemCount)
 			return nil
 		} else {
-			fmt.Println("No items found")
-
+			c.Printf("No items found")
 			return nil
 		}
 	})
 }
 
-func listItemsInCSVFormat(cmd *cobra.Command, showHidden bool) error {
-	return common.WithOpenedDatabase(cmd.ErrOrStderr(), func(db *sql.DB) error {
-		itemSelection := queries.ItemSelectionFromBool(showHidden)
+func (c *listItemsCommand) listItemsInCSVFormat() error {
+	return c.WithOpenedDatabase(func(db *sql.DB) error {
+		itemSelection := queries.ItemSelectionFromBool(c.showHidden)
 
 		items := []*models.Item{}
 		if err := queries.GetItems(db, queries.CollectTo(&items), itemSelection); err != nil {
 			return fmt.Errorf("failed to get items: %w", err)
 		}
 
-		categoryNameTable, err := queries.GetCategoryNameTable(db)
+		categoryNameTable, err := c.GetCategoryNameTable(db)
 		if err != nil {
-			return fmt.Errorf("failed to get category name table: %w", err)
+			return err
 		}
 
 		err = dbcsv.FormatItemsAsCSV(items, categoryNameTable, os.Stdout)
 		if err != nil {
+			c.PrintErrorf("Error while formatting items as CSV\n")
 			return fmt.Errorf("failed to format items as a CSV: %w", err)
 		}
 
