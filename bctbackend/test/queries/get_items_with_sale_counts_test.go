7ea16e12d6fdfbf0fa35683098667894b407e1f2
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-14 13:27:24 +0200

    GetItemsWithSaleCounts tests

--- bctbackend/test/queries/get_items_with_sale_counts_test.go
+++ bctbackend/test/queries/get_items_with_sale_counts_test.go
@@ -24,14 +24,70 @@ func TestGetItemsWithSaleCounts(t *testing.T) {
 			seller2 := setup.Seller()
 			seller3 := setup.Seller()
 			visibleItems1 := setup.Items(seller1.UserId, 1, aux.WithHidden(false), aux.WithFrozen(false))
-			hiddenItems1 := setup.Items(seller1.UserId, 1, aux.WithHidden(true), aux.WithFrozen(false))
-			visibleItems2 := setup.Items(seller2.UserId, 2, aux.WithHidden(false), aux.WithFrozen(false))
-			hiddenItems2 := setup.Items(seller2.UserId, 2, aux.WithHidden(true), aux.WithFrozen(false))
-			visibleItems3 := setup.Items(seller3.UserId, 4, aux.WithHidden(false), aux.WithFrozen(false))
-			hiddenItems3 := setup.Items(seller3.UserId, 4, aux.WithHidden(true), aux.WithFrozen(false))
+			hiddenItems1 := setup.Items(seller1.UserId, 2, aux.WithHidden(true), aux.WithFrozen(false))
+			visibleItems2 := setup.Items(seller2.UserId, 4, aux.WithHidden(false), aux.WithFrozen(false))
+			hiddenItems2 := setup.Items(seller2.UserId, 8, aux.WithHidden(true), aux.WithFrozen(false))
+			visibleItems3 := setup.Items(seller3.UserId, 16, aux.WithHidden(false), aux.WithFrozen(false))
+			hiddenItems3 := setup.Items(seller3.UserId, 32, aux.WithHidden(true), aux.WithFrozen(false))
 
 			expectedItems := slices.Concat(visibleItems1, visibleItems2, visibleItems3, hiddenItems1, hiddenItems2, hiddenItems3)
 			actualItems, err := queries.GetItemsWithSaleCounts(db, queries.AllItems, nil)
+			require.NoError(t, err)
+			require.Equal(t, len(expectedItems), len(actualItems))
+
+			expectedItemIds := algorithms.Map(expectedItems, func(item *models.Item) models.Id { return item.ItemID })
+			actualItemIds := algorithms.Map(actualItems, func(item *queries.ItemWithSaleCount) models.Id { return item.ItemID })
+			require.ElementsMatch(t, expectedItemIds, actualItemIds)
+			for _, item := range actualItems {
+				require.Equal(t, 0, item.SaleCount) // No sales in the fixture
+			}
+		})
+
+		t.Run("All items from seller 1, zero sales", func(t *testing.T) {
+			setup, db := NewDatabaseFixture(WithDefaultCategories)
+			defer setup.Close()
+
+			seller1 := setup.Seller()
+			seller2 := setup.Seller()
+			seller3 := setup.Seller()
+			visibleItems1 := setup.Items(seller1.UserId, 1, aux.WithHidden(false), aux.WithFrozen(false))
+			hiddenItems1 := setup.Items(seller1.UserId, 2, aux.WithHidden(true), aux.WithFrozen(false))
+			setup.Items(seller2.UserId, 4, aux.WithHidden(false), aux.WithFrozen(false))
+			setup.Items(seller2.UserId, 8, aux.WithHidden(true), aux.WithFrozen(false))
+			setup.Items(seller3.UserId, 16, aux.WithHidden(false), aux.WithFrozen(false))
+			setup.Items(seller3.UserId, 32, aux.WithHidden(true), aux.WithFrozen(false))
+
+			expectedItems := slices.Concat(visibleItems1, hiddenItems1)
+			sellerId := models.Id(1)
+			actualItems, err := queries.GetItemsWithSaleCounts(db, queries.AllItems, &sellerId)
+			require.NoError(t, err)
+			require.Equal(t, len(expectedItems), len(actualItems))
+
+			expectedItemIds := algorithms.Map(expectedItems, func(item *models.Item) models.Id { return item.ItemID })
+			actualItemIds := algorithms.Map(actualItems, func(item *queries.ItemWithSaleCount) models.Id { return item.ItemID })
+			require.ElementsMatch(t, expectedItemIds, actualItemIds)
+			for _, item := range actualItems {
+				require.Equal(t, 0, item.SaleCount) // No sales in the fixture
+			}
+		})
+
+		t.Run("All items from seller 2, zero sales", func(t *testing.T) {
+			setup, db := NewDatabaseFixture(WithDefaultCategories)
+			defer setup.Close()
+
+			seller1 := setup.Seller()
+			seller2 := setup.Seller()
+			seller3 := setup.Seller()
+			setup.Items(seller1.UserId, 1, aux.WithHidden(false), aux.WithFrozen(false))
+			setup.Items(seller1.UserId, 2, aux.WithHidden(true), aux.WithFrozen(false))
+			visibleItems2 := setup.Items(seller2.UserId, 4, aux.WithHidden(false), aux.WithFrozen(false))
+			hiddenItems2 := setup.Items(seller2.UserId, 8, aux.WithHidden(true), aux.WithFrozen(false))
+			setup.Items(seller3.UserId, 16, aux.WithHidden(false), aux.WithFrozen(false))
+			setup.Items(seller3.UserId, 32, aux.WithHidden(true), aux.WithFrozen(false))
+
+			expectedItems := slices.Concat(visibleItems2, hiddenItems2)
+			sellerId := models.Id(2)
+			actualItems, err := queries.GetItemsWithSaleCounts(db, queries.AllItems, &sellerId)
 			require.NoError(t, err)
 			require.Equal(t, len(expectedItems), len(actualItems))
 
