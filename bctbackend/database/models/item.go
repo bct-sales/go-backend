686080c4c8d6230f8ca6f352d3f6219a79c7ba0d
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-18 19:25:43 +0200

    CollectItemIds

--- bctbackend/database/models/item.go
+++ bctbackend/database/models/item.go
@@ -1,5 +1,7 @@
 package models
 
+import "bctbackend/algorithms"
+
 type Item struct {
 	ItemID       Id
 	AddedAt      Timestamp
@@ -15,4 +17,9 @@ type Item struct {
 
 func IsValidItemDescription(description string) bool {
 	return len(description) > 0
+}
+
+// CollectItemIds extracts the ItemID from each Item in the slice and returns a slice of Ids.
+func CollectItemIds(items []*Item) []Id {
+	return algorithms.Map(items, func(item *Item) Id { return item.ItemID })
 }
