d29ace927e4ec218153cae2fc061aeceea3ed432
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-18 17:57:17 +0200

    improved dummy data generation

--- bctbackend/commands/database/dummy.go
+++ bctbackend/commands/database/dummy.go
@@ -8,13 +8,146 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"math/rand/v2"
 
 	"github.com/MakeNowJust/heredoc"
 	"github.com/spf13/cobra"
 )
 
+const (
+	zoneCount = 12
+)
+
+var colors = [...]string{"red", "green", "blue", "yellow", "orange", "purple", "black", "white"}
+
+var clothing = [...]string{"t-shirt", "hoodie", "jacket", "sweater", "jeans", "pants", "shorts", "skirt", "dress", "hat", "scarf", "socks", "gloves"}
+
+var clothingCategories = [...]models.Id{
+	CategoryId_Clothing50_56,
+	CategoryId_Clothing56_62,
+	CategoryId_Clothing68_80,
+	CategoryId_Clothing86_92,
+	CategoryId_Clothing92_98,
+	CategoryId_Clothing104_116,
+	CategoryId_Clothing122_128,
+	CategoryId_Clothing128_140,
+	CategoryId_Clothing140_152,
+}
+
+var shoeTypes = [...]string{
+	"Nike Air Max 90 sneakers",
+	"Nike Air Max 95 sneakers",
+	"Nike Air Max 97 sneakers",
+	"Nike TN sneakers",
+	"Nike P-6000 sneakers",
+	"Nike Vomero sneakers",
+	"Nike Pegasus sneakers",
+	"Nike Air Force sneakers",
+	"Adidas Superstar sneakers",
+	"Adidas Gazelle sneakers",
+	"Adidas Stan Smith sneakers",
+	"Adidas Samba sneakers",
+	"Adidas Ultraboost sneakers",
+	"Adidas NMD sneakers",
+	"Adidas Yeezy sneakers",
+	"Adidas Ozweego sneakers",
+	"Puma Suede sneakers",
+	"Puma Speedcat sneakers",
+	"Fila Disruptor sneakers",
+	"Fila Bubbles sneakers",
+	"Reebok Instapump Fury sneakers",
+	"Reebok Kamikaze sneakers",
+	"Converse All-Star sneakers",
+	"Vans slip-ons",
+	"Vans Old Skool",
+	"Vans Sk8-Hi",
+	"New Balance 530 sneakers",
+	"New Balance 9060 sneakers",
+	"Asics Kayano 14 sneakers",
+	"Hoka sneakers",
+	"Saucony sneakers",
+	"Brooks sneakers",
+	"Mizuno sneakers",
+	"On Cloudmonster sneakers",
+	"On Cloudsurfer sneakers",
+	"Jordan sneakers",
+	"Under Armour sneakers",
+	"Crocs Classic clogs",
+	"Crocs Echo Clogs",
+	"Crocs Crush Clogs",
+}
+
+var bootBrands = [...]string{
+	"Sendra", "Timberland", "Dr. Martens", "Solovair", "New Rock", "Red Wing", "Frye", "Ariat", "Justin", "Lucchese", "Carmina", "Tony Mora",
+}
+
+var bootTypes = [...]string{
+	"Combat boots", "Cowboy boots", "Harness boots", "Engineer boots", "Jodhpur boots", "Chelsea boots",
+}
+
+var toys = [...]string{
+	"LEGO set",
+	"Barbie doll",
+	"Action figure",
+	"Puzzle",
+	"Board game",
+	"Stuffed animal",
+	"Nintendo Switch",
+	"Nintendo Switch 2",
+	"Nintendo DS",
+	"Nintendo 3DS",
+	"Nintendo Wii",
+	"Nintendo Wii U",
+	"PlayStation 1",
+	"PlayStation 2",
+	"PlayStation 3",
+	"PlayStation 4",
+	"PlayStation 5",
+	"Xbox Series X",
+	"Zelda: Breath of the Wild",
+	"Zelda: Tears of the Kingdom",
+	"Zelda: Ocarina of Time",
+	"Zelda: Majora's Mask",
+	"Zelda: Wind Waker",
+	"Zelda: Twilight Princess",
+	"Zelda: Skyward Sword",
+	"Zelda: A Link to the Past",
+	"Zelda: A Link Between Worlds",
+	"Zelda: Oracle of Seasons",
+	"Zelda: Oracle of Ages",
+	"Zelda: Four Swords Adventures",
+	"Zelda: Spirit Tracks",
+	"Zelda: Phantom Hourglass",
+	"Zelda: Link's Awakening",
+	"Zelda: A Link Between Worlds",
+	"Zelda: Tri Force Heroes",
+	"Zelda: Hyrule Warriors",
+	"Super Mario Odyssey",
+	"Super Mario Galaxy",
+	"Super Mario 3D World",
+	"Super Mario 64",
+	"Super Mario Sunshine",
+	"Super Mario Maker",
+	"Super Mario Party",
+	"Super Paper Mario",
+	"Smash Bros. Ultimate",
+	"Call of Duty",
+	"Disco Elysium",
+	"The Witcher 3",
+	"The Last of Us",
+	"Uncharted 4",
+	"God of War",
+	"Chess set",
+	"Checkers set",
+	"Poker set",
+}
+
 type dummyDatabaseCommand struct {
 	common.Command
+	seed       uint64
+	rng        *rand.Rand
+	cashierIds *[]models.Id
+	sellerIds  *[]models.Id
 }
 
 func NewDatabaseDummyCommand() *cobra.Command {
@@ -36,10 +169,14 @@ func NewDatabaseDummyCommand() *cobra.Command {
 		},
 	}
 
+	command.CobraCommand.Flags().Uint64Var(&command.seed, "seed", 0, "Seed for random number generation")
+
 	return command.AsCobraCommand()
 }
 
 func (c *dummyDatabaseCommand) execute() error {
+	c.rng = rand.New(rand.NewPCG(0, c.seed))
+
 	return c.WithOpenedDatabase(func(db *sql.DB) error {
 		c.Printf("Resetting database\n")
 		if err := database.ResetDatabase(db); err != nil {
@@ -54,316 +191,8 @@ func (c *dummyDatabaseCommand) execute() error {
 			return err
 		}
 
-		{
-			now := models.Now()
-
-			c.Printf("Adding some items\n")
-			err := errors.Join(
-				c.addDummyItem(
-					db,
-					now,
-					"T-Shirt",
-					1000,
-					CategoryId_Clothing140_152,
-					100,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Jeans",
-					1000,
-					CategoryId_Clothing140_152,
-					100,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Nike sneakers",
-					2000,
-					CategoryId_Shoes,
-					100,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Adidas sneakers",
-					2000,
-					CategoryId_Shoes,
-					200,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Puma sneakers",
-					2000,
-					CategoryId_Shoes,
-					200,
-					false,
-					false,
-					true,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Reebok sneakers",
-					2000,
-					CategoryId_Shoes,
-					200,
-					false,
-					true,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Converse sneakers",
-					2000,
-					CategoryId_Shoes,
-					200,
-					true,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Vans sneakers",
-					2000,
-					CategoryId_Shoes,
-					200,
-					true,
-					true,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"New Balance sneakers",
-					2000,
-					CategoryId_Shoes,
-					200,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Asics sneakers",
-					2000,
-					CategoryId_Shoes,
-					200,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Hoka sneakers",
-					2000,
-					CategoryId_Shoes,
-					200,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Saucony sneakers",
-					2000,
-					CategoryId_Shoes,
-					200,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Brooks sneakers",
-					2000,
-					CategoryId_Shoes,
-					200,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Mizuno sneakers",
-					2000,
-					CategoryId_Shoes,
-					200,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"On sneakers",
-					2000,
-					CategoryId_Shoes,
-					200,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Combat boots",
-					2000,
-					CategoryId_Shoes,
-					300,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Hiking boots",
-					2000,
-					CategoryId_Shoes,
-					300,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Winter boots",
-					2000,
-					CategoryId_Shoes,
-					300,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Rain boots",
-					2000,
-					CategoryId_Shoes,
-					300,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Snow boots",
-					2000,
-					CategoryId_Shoes,
-					300,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Bean boots",
-					2000,
-					CategoryId_Shoes,
-					300,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Cowboy boots",
-					2000,
-					CategoryId_Shoes,
-					300,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Harness boots",
-					2000,
-					CategoryId_Shoes,
-					300,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Engineer boots",
-					2000,
-					CategoryId_Shoes,
-					300,
-					false,
-					false,
-					false,
-					false,
-				),
-				c.addDummyItem(
-					db,
-					now,
-					"Jodhpur boots",
-					2000,
-					CategoryId_Shoes,
-					300,
-					false,
-					false,
-					false,
-					false,
-				),
-			)
-
-			if err != nil {
-				return fmt.Errorf("failed to add items: %w", err)
-			}
+		if err := c.addItems(db); err != nil {
+			return err
 		}
 
 		return nil
@@ -406,7 +235,7 @@ func (c *dummyDatabaseCommand) addCategories(db *sql.DB) error {
 func (c *dummyDatabaseCommand) addUsers(db *sql.DB) error {
 	return errors.Join(
 		c.addAdmin(db),
-		c.addCashier(db),
+		c.addCashiers(db),
 		c.addSellers(db),
 	)
 }
@@ -427,34 +256,51 @@ func (c *dummyDatabaseCommand) addAdmin(db *sql.DB) error {
 	return nil
 }
 
-func (c *dummyDatabaseCommand) addCashier(db *sql.DB) error {
-	c.Printf("Adding cashier user\n")
+func (c *dummyDatabaseCommand) addCashiers(db *sql.DB) error {
+	c.Printf("Adding cashier users\n")
 
-	id := models.Id(2)
-	roleId := models.NewCashierRoleId()
-	createdAt := models.Now()
-	var lastActivity *models.Timestamp = nil
-	password := "abc"
+	cashierCount := c.rng.IntN(10) + 1
+	cashierIDs := make([]models.Id, 0, cashierCount)
 
-	if err := queries.AddUserWithId(db, id, roleId, createdAt, lastActivity, password); err != nil {
-		return fmt.Errorf("failed to add cashier: %w", err)
+	for range cashierCount {
+		roleId := models.NewCashierRoleId()
+		createdAt := models.Now()
+		var lastActivity *models.Timestamp = nil
+		password := "abc"
+
+		cashierId, err := queries.AddUser(db, roleId, createdAt, lastActivity, password)
+
+		if err != nil {
+			return fmt.Errorf("failed to add cashier: %w", err)
+		}
+
+		cashierIDs = append(cashierIDs, cashierId)
 	}
+
+	c.cashierIds = &cashierIDs
 
 	return nil
 }
 
 func (c *dummyDatabaseCommand) addSellers(db *sql.DB) error {
 	c.Printf("Adding sellers\n")
+
+	sellerIds := make([]models.Id, 0, zoneCount*10)
+
 	addSellers := func(addUser func(userId models.Id, roleId models.RoleId, createdAt models.Timestamp, lastActivity *models.Timestamp, password string)) {
-		for area := 1; area <= 12; area++ {
-			for offset := 0; offset != 4; offset++ {
-				userId := models.Id(area*100 + offset)
+		for area := 1; area <= zoneCount; area++ {
+			sellerCount := c.rng.IntN(10) + 1
+
+			for offset := 0; offset != sellerCount; offset++ {
+				userId := c.getSellerId(area, offset)
 				roleId := models.NewSellerRoleId()
 				createdAt := models.Now()
 				var lastActivity *models.Timestamp = nil
 				password := fmt.Sprintf("%d", userId)
 
 				addUser(userId, roleId, createdAt, lastActivity, password)
+
+				sellerIds = append(sellerIds, userId)
 			}
 		}
 	}
@@ -462,5 +308,92 @@ func (c *dummyDatabaseCommand) addSellers(db *sql.DB) error {
 		return fmt.Errorf("failed to add sellers: %w", err)
 	}
 
+	c.sellerIds = &sellerIds
+
 	return nil
+}
+
+func (c *dummyDatabaseCommand) getSellerId(zone int, offset int) models.Id {
+	return models.Id(zone*100 + offset)
+}
+
+func (c *dummyDatabaseCommand) addItems(db *sql.DB) error {
+	c.Printf("Adding items\n")
+
+	for _, sellerId := range *c.sellerIds {
+		itemCount := c.rng.IntN(20)
+
+		for range itemCount {
+			description, category := c.generateRandomItemDescriptionAndCategory()
+			priceInCents := models.MoneyInCents(c.rng.IntN(100)*50 + 50)
+			donation := c.rng.IntN(20) == 0
+			charity := c.rng.IntN(20) == 0
+			frozen := c.rng.IntN(20) == 0
+			hidden := false
+			addedAt := models.Now()
+
+			if _, err := queries.AddItem(db, addedAt, description, priceInCents, category, sellerId, donation, charity, frozen, hidden); err != nil {
+				return fmt.Errorf("failed to add item: %w", err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (c *dummyDatabaseCommand) generateRandomItemDescriptionAndCategory() (string, models.Id) {
+	switch c.rng.IntN(3) {
+	case 0:
+		return c.generateRandomClothing()
+	case 1:
+		return c.generateRandomShoes()
+	default:
+		return c.generateRandomToys()
+	}
+}
+
+func (c *dummyDatabaseCommand) generateRandomColor() string {
+	return colors[c.rng.IntN(len(colors))]
+}
+
+func (c *dummyDatabaseCommand) generateRandomClothingCategoryId() models.Id {
+	return pickRandom(c.rng, clothingCategories[:])
+}
+
+func (c *dummyDatabaseCommand) generateRandomClothing() (string, models.Id) {
+	color := c.generateRandomColor()
+	categoryId := c.generateRandomClothingCategoryId()
+	clothingType := pickRandom(c.rng, clothing[:])
+	description := fmt.Sprintf("%s %s", color, clothingType)
+
+	return description, categoryId
+}
+
+func pickRandom[T any](rng *rand.Rand, items []T) T {
+	if len(items) == 0 {
+		panic("cannot pick from empty slice")
+	}
+	return items[rng.IntN(len(items))]
+}
+
+func (c *dummyDatabaseCommand) generateRandomShoes() (string, models.Id) {
+	switch c.rng.IntN(2) {
+	case 0:
+		color := c.generateRandomColor()
+		shoeType := pickRandom(c.rng, shoeTypes[:])
+		description := fmt.Sprintf("%s %s", color, shoeType)
+		return description, CategoryId_Shoes
+	default:
+		brand := pickRandom(c.rng, bootBrands[:])
+		bootType := pickRandom(c.rng, bootTypes[:])
+		description := fmt.Sprintf("%s %s", brand, bootType)
+		return description, CategoryId_Shoes
+	}
+}
+
+func (c *dummyDatabaseCommand) generateRandomToys() (string, models.Id) {
+	description := pickRandom(c.rng, toys[:])
+	categoryId := CategoryId_Toys
+
+	return description, categoryId
 }
