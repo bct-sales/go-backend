78ecaa4606c43e8f5268196e9d34b1ffc022f5c3
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-19 01:02:42 +0200

    fixed name of function

--- bctbackend/rest/path/cashier_sales.go
+++ bctbackend/rest/path/cashier_sales.go
@@ -15,6 +15,6 @@ func (path *cashierSalesPath) WithRawCashierId(id string) string {
 	return fmt.Sprintf("/api/v1/cashiers/%s/sales", id)
 }
 
-func (path *cashierSalesPath) WithSellerId(cashierId models.Id) string {
+func (path *cashierSalesPath) WithCashierId(cashierId models.Id) string {
 	return path.WithRawCashierId(cashierId.String())
 }
