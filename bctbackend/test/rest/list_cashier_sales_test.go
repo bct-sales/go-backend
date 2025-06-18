bd6224dc44b0f709ff4326cead33dca55bf1d89f
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-19 01:40:04 +0200

    ListCashierSales test

--- bctbackend/test/rest/list_cashier_sales_test.go
+++ bctbackend/test/rest/list_cashier_sales_test.go
@@ -85,5 +85,24 @@ func TestListCashierSales(t *testing.T) {
 			router.ServeHTTP(writer, request)
 			require.Equal(t, http.StatusForbidden, writer.Code)
 		})
+
+		t.Run("Other cashier views sales", func(t *testing.T) {
+			setup, router, writer := NewRestFixture(WithDefaultCategories)
+			defer setup.Close()
+
+			seller := setup.Seller()
+			cashier := setup.Cashier()
+			cashier2, sessionId := setup.LoggedIn(setup.Cashier())
+
+			items := setup.Items(seller.UserId, 10, aux.WithHidden(false))
+			algorithms.Map(items, func(item *models.Item) *models.Sale { return setup.Sale(cashier.UserId, []models.Id{item.ItemID}) })
+			items2 := setup.Items(seller.UserId, 20, aux.WithHidden(false))
+			algorithms.Map(items2, func(item *models.Item) *models.Sale { return setup.Sale(cashier2.UserId, []models.Id{item.ItemID}) })
+
+			url := path.CashierSales().WithCashierId(cashier.UserId)
+			request := CreateGetRequest(url, WithSessionCookie(sessionId))
+			router.ServeHTTP(writer, request)
+			require.Equal(t, http.StatusForbidden, writer.Code)
+		})
 	})
 }
