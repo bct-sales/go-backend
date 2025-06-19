1d4f921036a53fe87a16e8b875d5ed437bb360e6
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-19 12:37:54 +0200

    item unhide update

--- bctbackend/commands/item/unhide.go
+++ bctbackend/commands/item/unhide.go
@@ -2,42 +2,52 @@ package item
 
 import (
 	"bctbackend/commands/common"
-	"bctbackend/database/models"
 	"bctbackend/database/queries"
 	"database/sql"
-	"fmt"
 
 	"github.com/MakeNowJust/heredoc"
 	"github.com/spf13/cobra"
 )
 
+type unhideItemCommand struct {
+	common.Command
+}
+
 func NewUnhideItemCommand() *cobra.Command {
-	command := cobra.Command{
-		Use:   "unhide <item-id>",
-		Short: "Unhides an item",
-		Long: heredoc.Doc(`
-				This command unhides an item.
+	var command *unhideItemCommand
+
+	command = &unhideItemCommand{
+		Command: common.Command{
+			CobraCommand: &cobra.Command{
+				Use:   "unhide <item-id> ...",
+				Short: "Unhides a items",
+				Long: heredoc.Doc(`
+				This command unhides items.
 			   `),
-		Args: cobra.ExactArgs(1), // Expect exactly one argument (the item ID)
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return common.WithOpenedDatabase(cmd.ErrOrStderr(), func(db *sql.DB) error {
-				// Parse the item ID from the first argument
-				itemId, err := models.ParseId(args[0])
-				if err != nil {
-					fmt.Fprintf(cmd.ErrOrStderr(), "Invalid item ID: %s\n", args[0])
-					return err
-				}
-
-				if err := queries.UpdateHiddenStatusOfItems(db, []models.Id{itemId}, false); err != nil {
-					fmt.Fprintf(cmd.ErrOrStderr(), "Failed to unhide item: %v\n", err)
-					return fmt.Errorf("failed to update database: %w", err)
-				}
-
-				fmt.Fprint(cmd.OutOrStdout(), "Item unhidden successfully\n")
-				return nil
-			})
+				Args: cobra.MinimumNArgs(1), // Expect exactly one argument (the item ID)
+				RunE: func(cmd *cobra.Command, args []string) error {
+					return command.execute(args)
+				},
+			},
 		},
 	}
 
-	return &command
+	return command.AsCobraCommand()
+}
+
+func (c *unhideItemCommand) execute(args []string) error {
+	return c.WithOpenedDatabase(func(db *sql.DB) error {
+		itemIds, err := c.ParseItemIds(args)
+		if err != nil {
+			return err
+		}
+
+		if err := queries.UpdateHiddenStatusOfItems(db, itemIds, false); err != nil {
+			c.PrintErrorf("Failed to unhide items: %v\n", err)
+			return err
+		}
+
+		c.Printf("Items unhidden successfully\n")
+		return nil
+	})
 }
