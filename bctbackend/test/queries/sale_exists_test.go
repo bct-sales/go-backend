c4f0883ef06c468865bf158d2db3f3b7e41a0bb5
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-19 14:51:24 +0200

    refactoring

--- bctbackend/test/queries/sale_exists_test.go
+++ bctbackend/test/queries/sale_exists_test.go
@@ -13,26 +13,28 @@ import (
 )
 
 func TestSaleExists(t *testing.T) {
-	t.Run("Sale exists", func(t *testing.T) {
-		setup, db := NewDatabaseFixture(WithDefaultCategories)
-		defer setup.Close()
+	t.Run("Success", func(t *testing.T) {
+		t.Run("Sale exists", func(t *testing.T) {
+			setup, db := NewDatabaseFixture(WithDefaultCategories)
+			defer setup.Close()
 
-		seller := setup.Seller()
-		cashier := setup.Cashier()
-		itemId := setup.Item(seller.UserId, aux.WithDummyData(1), aux.WithHidden(false)).ItemID
+			seller := setup.Seller()
+			cashier := setup.Cashier()
+			itemId := setup.Item(seller.UserId, aux.WithDummyData(1), aux.WithHidden(false)).ItemID
 
-		sale := setup.Sale(cashier.UserId, []models.Id{itemId})
-		saleExists, err := queries.SaleWithIdExists(db, sale.SaleID)
-		require.NoError(t, err)
-		require.True(t, saleExists)
-	})
+			sale := setup.Sale(cashier.UserId, []models.Id{itemId})
+			saleExists, err := queries.SaleWithIdExists(db, sale.SaleID)
+			require.NoError(t, err)
+			require.True(t, saleExists)
+		})
 
-	t.Run("Sale does not exist", func(t *testing.T) {
-		setup, db := NewDatabaseFixture(WithDefaultCategories)
-		defer setup.Close()
+		t.Run("Sale does not exist", func(t *testing.T) {
+			setup, db := NewDatabaseFixture(WithDefaultCategories)
+			defer setup.Close()
 
-		saleExists, err := queries.SaleWithIdExists(db, 1)
-		require.NoError(t, err)
-		require.False(t, saleExists)
+			saleExists, err := queries.SaleWithIdExists(db, 1)
+			require.NoError(t, err)
+			require.False(t, saleExists)
+		})
 	})
 }
