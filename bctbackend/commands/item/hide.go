8401fb0dcb8855b2d1b4d6bbf28163fcb71f95a6
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-19 12:34:45 +0200

    item hide update

--- bctbackend/commands/item/hide.go
+++ bctbackend/commands/item/hide.go
@@ -2,43 +2,52 @@ package item
 
 import (
 	"bctbackend/commands/common"
-	"bctbackend/database/models"
 	"bctbackend/database/queries"
 	"database/sql"
-	"fmt"
 
 	"github.com/MakeNowJust/heredoc"
 	"github.com/spf13/cobra"
 )
 
+type hideItemCommand struct {
+	common.Command
+}
+
 func NewHideItemCommand() *cobra.Command {
-	command := cobra.Command{
-		Use:   "hide <item-id>",
-		Short: "Hides an item",
-		Long: heredoc.Doc(`
-				This command hides an item.
-				You can unhide it later if needed.
+	var command *hideItemCommand
+	command = &hideItemCommand{
+		Command: common.Command{
+			CobraCommand: &cobra.Command{
+				Use:   "hide <item-id> ...",
+				Short: "Hides items",
+				Long: heredoc.Doc(`
+				This command hides items.
+				You can unhide them later if needed.
 			   `),
-		Args: cobra.ExactArgs(1), // Expect exactly one argument (the item ID)
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return common.WithOpenedDatabase(cmd.ErrOrStderr(), func(db *sql.DB) error {
-				// Parse the item ID from the first argument
-				itemId, err := models.ParseId(args[0])
-				if err != nil {
-					fmt.Fprintf(cmd.ErrOrStderr(), "Invalid item ID: %s\n", args[0])
-					return err
-				}
-
-				if err := queries.UpdateHiddenStatusOfItems(db, []models.Id{itemId}, true); err != nil {
-					fmt.Fprintf(cmd.ErrOrStderr(), "Failed to hide item: %v\n", err)
-					return err
-				}
-
-				fmt.Fprint(cmd.OutOrStdout(), "Item hidden successfully\n")
-				return nil
-			})
+				Args: cobra.MinimumNArgs(1),
+				RunE: func(cmd *cobra.Command, args []string) error {
+					return command.execute(args)
+				},
+			},
 		},
 	}
 
-	return &command
+	return command.AsCobraCommand()
+}
+
+func (c *hideItemCommand) execute(args []string) error {
+	return c.WithOpenedDatabase(func(db *sql.DB) error {
+		itemIds, err := c.ParseItemIds(args)
+		if err != nil {
+			return err
+		}
+
+		if err := queries.UpdateHiddenStatusOfItems(db, itemIds, true); err != nil {
+			c.PrintErrorf("Failed to hide items: %v\n", err)
+			return err
+		}
+
+		c.Printf("Items hidden successfully\n")
+		return nil
+	})
 }
