8c86ef9f43b785e406b4df4738a6478e6003c797
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-19 18:03:15 +0200

    can unset donation and charity in items

--- bctbackend/commands/item/update.go
+++ bctbackend/commands/item/update.go
@@ -5,8 +5,11 @@ import (
 	"bctbackend/database/models"
 	"bctbackend/database/queries"
 	"database/sql"
+	"fmt"
+	"strconv"
 
 	"github.com/MakeNowJust/heredoc"
+	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
 )
 
@@ -18,6 +21,8 @@ type updateItemCommand struct {
 	categoryId   uint64
 	donation     bool
 	charity      bool
+	noDonation   bool
+	noCharity    bool
 }
 
 func NewUpdateItemCommand() *cobra.Command {
@@ -43,59 +48,123 @@ func NewUpdateItemCommand() *cobra.Command {
 	command.CobraCommand.Flags().Uint64Var(&command.priceInCents, "price", 0, "New price in cents for the item")
 	command.CobraCommand.Flags().Uint64Var(&command.categoryId, "category", 0, "New category ID for the item")
 	command.CobraCommand.Flags().BoolVar(&command.donation, "donation", false, "Set item as a donation")
+	command.CobraCommand.Flags().BoolVar(&command.donation, "no-donation", false, "Unset item as a donation")
 	command.CobraCommand.Flags().BoolVar(&command.charity, "charity", false, "Set item as a charity item")
+	command.CobraCommand.Flags().BoolVar(&command.charity, "no-charity", false, "Unset item as a charity item")
 
 	command.CobraCommand.MarkFlagRequired("id")
+	command.CobraCommand.MarkFlagsMutuallyExclusive("donation", "no-donation")
+	command.CobraCommand.MarkFlagsMutuallyExclusive("charity", "no-charity")
 
 	return command.AsCobraCommand()
 }
 
 func (c *updateItemCommand) execute(args []string) error {
 	return c.WithOpenedDatabase(func(db *sql.DB) error {
-		var description *string
-		var priceInCents *models.MoneyInCents
-		var categoryId *models.Id
-		var donation *bool
-		var charity *bool
-
-		if c.CobraCommand.Flags().Changed("description") {
-			description = &c.description
-		}
-
-		if c.CobraCommand.Flags().Changed("price") {
-			value := models.MoneyInCents(c.priceInCents)
-			priceInCents = &value
-		}
-
-		if c.CobraCommand.Flags().Changed("category") {
-			value := models.Id(c.categoryId)
-			categoryId = &value
-		}
-
-		if c.CobraCommand.Flags().Changed("donation") {
-			donation = &c.donation
-		}
-
-		if c.CobraCommand.Flags().Changed("charity") {
-			charity = &c.charity
-		}
-
-		itemUpdate := queries.ItemUpdate{
-			Description:  description,
-			PriceInCents: priceInCents,
-			CategoryId:   categoryId,
-			Charity:      charity,
-			Donation:     donation,
-			AddedAt:      nil,
-		}
-
-		err := queries.UpdateItem(db, models.Id(c.itemId), &itemUpdate)
-		if err != nil {
-			c.PrintErrorf("Failed to update item")
+		if err := c.updateItem(db); err != nil {
 			return err
 		}
 
-		c.Printf("Item %d updated successfully", c.itemId)
+		if err := c.showUpdatedItem(db); err != nil {
+			return err
+		}
+
 		return nil
 	})
+}
+
+func (c *updateItemCommand) updateItem(db *sql.DB) error {
+	var description *string
+	var priceInCents *models.MoneyInCents
+	var categoryId *models.Id
+	var donation *bool
+	var charity *bool
+
+	if c.CobraCommand.Flags().Changed("description") {
+		description = &c.description
+	}
+
+	if c.CobraCommand.Flags().Changed("price") {
+		value := models.MoneyInCents(c.priceInCents)
+		priceInCents = &value
+	}
+
+	if c.CobraCommand.Flags().Changed("category") {
+		value := models.Id(c.categoryId)
+		categoryId = &value
+	}
+
+	if c.CobraCommand.Flags().Changed("donation") {
+		donation = &c.donation
+	}
+
+	if c.CobraCommand.Flags().Changed("charity") {
+		charity = &c.charity
+	}
+
+	if c.CobraCommand.Flags().Changed("no-donation") {
+		value := false
+		donation = &value
+	}
+
+	if c.CobraCommand.Flags().Changed("no-charity") {
+		value := false
+		charity = &value
+	}
+
+	itemUpdate := queries.ItemUpdate{
+		Description:  description,
+		PriceInCents: priceInCents,
+		CategoryId:   categoryId,
+		Charity:      charity,
+		Donation:     donation,
+		AddedAt:      nil,
+	}
+
+	err := queries.UpdateItem(db, models.Id(c.itemId), &itemUpdate)
+	if err != nil {
+		c.PrintErrorf("Failed to update item")
+		return err
+	}
+
+	c.Printf("Item updated successfully\n")
+	return nil
+}
+
+func (c *updateItemCommand) showUpdatedItem(db *sql.DB) error {
+	itemId := models.Id(c.itemId)
+	categoryNameTable, err := c.GetCategoryNameTable(db)
+	if err != nil {
+		c.PrintErrorf("Failed to get category name table")
+		return err
+	}
+
+	item, err := queries.GetItemWithId(db, itemId)
+	if err != nil {
+		c.PrintErrorf("Failed to get item back from database")
+		return fmt.Errorf("failed to get item with id %d: %w", itemId, err)
+	}
+
+	categoryName, ok := categoryNameTable[item.CategoryID]
+	if !ok {
+		panic("Bug: item has nonexistent category")
+	}
+
+	tableData := pterm.TableData{
+		{"Property", "Value"},
+		{"Description", item.Description},
+		{"Price", item.PriceInCents.DecimalNotation()},
+		{"Category", categoryName},
+		{"Seller", item.SellerID.String()},
+		{"Donation", strconv.FormatBool(item.Donation)},
+		{"Charity", strconv.FormatBool(item.Charity)},
+		{"Added At", item.AddedAt.FormattedDateTime()},
+	}
+
+	err = pterm.DefaultTable.WithHasHeader().WithHeaderRowSeparator("-").WithData(tableData).Render()
+	if err != nil {
+		return fmt.Errorf("failed to render table: %w", err)
+	}
+
+	return nil
 }
