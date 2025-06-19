47b4f7bf770242b18f7e86389f718d3ec509f91f
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-19 12:18:27 +0200

    item freeze update

--- bctbackend/commands/item/freeze.go
+++ bctbackend/commands/item/freeze.go
@@ -2,7 +2,6 @@ package item
 
 import (
 	"bctbackend/commands/common"
-	"bctbackend/database/models"
 	"bctbackend/database/queries"
 	"database/sql"
 
@@ -20,14 +19,14 @@ func NewFreezeItemCommand() *cobra.Command {
 	command = &freezeItemCommand{
 		Command: common.Command{
 			CobraCommand: &cobra.Command{
-				Use:   "freeze <item-id>",
-				Short: "Freezes an item",
+				Use:   "freeze <item-id> ...",
+				Short: "Freezes items",
 				Long: heredoc.Doc(`
-				This command freezes an item so that it cannot be edited anymore.
-				You can unfreeze it later if needed.
+				This command freezes items so that they cannot be edited anymore.
+				You can unfreeze them later if needed.
 				Items are automatically frozen when labels are generated for them.
 			   `),
-				Args: cobra.ExactArgs(1), // Expect exactly one argument (the item ID)
+				Args: cobra.MinimumNArgs(1),
 				RunE: func(cmd *cobra.Command, args []string) error {
 					return command.execute(args)
 				},
@@ -40,18 +39,17 @@ func NewFreezeItemCommand() *cobra.Command {
 
 func (c *freezeItemCommand) execute(args []string) error {
 	return c.WithOpenedDatabase(func(db *sql.DB) error {
-		itemId, err := models.ParseId(args[0])
+		itemIds, err := c.ParseItemIds(args)
 		if err != nil {
-			c.PrintErrorf("Invalid item ID: %s\n", args[0])
 			return err
 		}
 
-		if err := queries.UpdateFreezeStatusOfItems(db, []models.Id{itemId}, true); err != nil {
-			c.PrintErrorf("Failed to freeze item: %v\n", err)
+		if err := queries.UpdateFreezeStatusOfItems(db, itemIds, true); err != nil {
+			c.PrintErrorf("Failed to freeze items: %v\n", err)
 			return err
 		}
 
-		c.Printf("Item frozen successfully\n")
+		c.Printf("Items frozen successfully\n")
 		return nil
 	})
 }
