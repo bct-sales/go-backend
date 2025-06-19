345479a4d81218f4239753bde002437820a874e4
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-19 10:55:46 +0200

    refactoring

--- bctbackend/commands/item/unfreeze.go
+++ bctbackend/commands/item/unfreeze.go
@@ -11,36 +11,45 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type unfreezeItemCommand struct {
+	common.Command
+}
+
 func NewUnfreezeItemCommand() *cobra.Command {
-	command := cobra.Command{
-		Use:   "unfreeze <item-id>",
-		Short: "Unfreezes an item",
-		Long: heredoc.Doc(`
+	var command *unfreezeItemCommand
+
+	command = &unfreezeItemCommand{
+		Command: common.Command{
+			CobraCommand: &cobra.Command{
+				Use:   "unfreeze <item-id>",
+				Short: "Unfreezes an item",
+				Long: heredoc.Doc(`
 				This command unfreezes an item so that it can be edited again.
 				Use with care: if labels were printed for the item, editing the item would make them inaccurate.
 				It is highly recommended to not unfreeze items and instead create a new item with the updated information.
 				See the item copy command.
 			   `),
-		Args: cobra.ExactArgs(1), // Expect exactly one argument (the item ID)
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return common.WithOpenedDatabase(cmd.ErrOrStderr(), func(db *sql.DB) error {
-				// Parse the item ID from the first argument
-				itemId, err := models.ParseId(args[0])
-				if err != nil {
-					fmt.Fprintf(cmd.ErrOrStderr(), "Invalid item ID: %s\n", args[0])
-					return err
-				}
+				Args: cobra.ExactArgs(1), // Expect exactly one argument (the item ID)
+				RunE: func(cmd *cobra.Command, args []string) error {
+					return common.WithOpenedDatabase(cmd.ErrOrStderr(), func(db *sql.DB) error {
+						itemId, err := models.ParseId(args[0])
+						if err != nil {
+							fmt.Fprintf(cmd.ErrOrStderr(), "Invalid item ID: %s\n", args[0])
+							return err
+						}
 
-				if err := queries.UpdateFreezeStatusOfItems(db, []models.Id{itemId}, false); err != nil {
-					fmt.Fprintf(cmd.ErrOrStderr(), "Failed to unfreeze item: %v\n", err)
-					return err
-				}
+						if err := queries.UpdateFreezeStatusOfItems(db, []models.Id{itemId}, false); err != nil {
+							fmt.Fprintf(cmd.ErrOrStderr(), "Failed to unfreeze item: %v\n", err)
+							return err
+						}
 
-				fmt.Fprint(cmd.OutOrStdout(), "Item unfrozen successfully\n")
-				return nil
-			})
+						fmt.Fprint(cmd.OutOrStdout(), "Item unfrozen successfully\n")
+						return nil
+					})
+				},
+			},
 		},
 	}
 
-	return &command
+	return command.AsCobraCommand()
 }
