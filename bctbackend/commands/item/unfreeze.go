ceaaca2143613c9d39658ccca127c1e4f8257086
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-19 11:29:03 +0200

    improved item unfreeze command

--- bctbackend/commands/item/unfreeze.go
+++ bctbackend/commands/item/unfreeze.go
@@ -5,6 +5,7 @@ import (
 	"bctbackend/database/models"
 	"bctbackend/database/queries"
 	"database/sql"
+	"fmt"
 
 	"github.com/MakeNowJust/heredoc"
 	"github.com/spf13/cobra"
@@ -23,12 +24,12 @@ func NewUnfreezeItemCommand() *cobra.Command {
 				Use:   "unfreeze <item-id>",
 				Short: "Unfreezes an item",
 				Long: heredoc.Doc(`
-				This command unfreezes an item so that it can be edited again.
+				This command unfreezes items so that they can be edited again.
 				Use with care: if labels were printed for the item, editing the item would make them inaccurate.
-				It is highly recommended to not unfreeze items and instead create a new item with the updated information.
+				It is highly recommended not to unfreeze items but instead create a new item with the updated information.
 				See the item copy command.
 			   `),
-				Args: cobra.ExactArgs(1), // Expect exactly one argument (the item ID)
+				Args: cobra.MinimumNArgs(1),
 				RunE: func(cmd *cobra.Command, args []string) error {
 					return command.execute(args)
 				},
@@ -41,18 +42,31 @@ func NewUnfreezeItemCommand() *cobra.Command {
 
 func (c *unfreezeItemCommand) execute(args []string) error {
 	return c.WithOpenedDatabase(func(db *sql.DB) error {
-		itemId, err := models.ParseId(args[0])
+		itemIds, err := c.parseIds(args)
 		if err != nil {
-			c.PrintErrorf("Invalid item ID: %s\n", args[0])
 			return err
 		}
 
-		if err := queries.UpdateFreezeStatusOfItems(db, []models.Id{itemId}, false); err != nil {
+		if err := queries.UpdateFreezeStatusOfItems(db, itemIds, false); err != nil {
 			c.PrintErrorf("Failed to unfreeze item: %v\n", err)
 			return err
 		}
 
-		c.Printf("Item unfrozen successfully\n")
+		c.Printf("Items unfrozen successfully\n")
 		return nil
 	})
+}
+
+func (c *unfreezeItemCommand) parseIds(args []string) ([]models.Id, error) {
+	ids := make([]models.Id, len(args))
+
+	for i, arg := range args {
+		id, err := models.ParseId(arg)
+		if err != nil {
+			c.PrintErrorf("Invalid item ID: %s\n", arg)
+			return nil, fmt.Errorf("failed to parse %d-nth id \"%s\": %w", i, arg, err)
+		}
+		ids[i] = id
+	}
+	return ids, nil
 }
