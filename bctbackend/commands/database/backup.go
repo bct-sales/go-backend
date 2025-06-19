9cd9e443d943a0f06daa2b880ef8072011c194a7
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-19 14:41:50 +0200

    refactoring

--- bctbackend/commands/database/backup.go
+++ bctbackend/commands/database/backup.go
@@ -9,27 +9,42 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type backupDatabaseCommand struct {
+	common.Command
+}
+
 func NewDatabaseBackupCommand() *cobra.Command {
-	command := cobra.Command{
-		Use:   "backup <filename>",
-		Short: "Backup the database",
-		Args:  cobra.ExactArgs(1),
-		Long: heredoc.Doc(`
+	var command *backupDatabaseCommand
+
+	command = &backupDatabaseCommand{
+		Command: common.Command{
+			CobraCommand: &cobra.Command{
+				Use:   "backup <filename>",
+				Short: "Backup the database",
+				Args:  cobra.ExactArgs(1),
+				Long: heredoc.Doc(`
 			This command makes a copy of the current database.
 		`),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			targetPath := args[0]
-
-			return common.WithOpenedDatabase(cmd.ErrOrStderr(), func(db *sql.DB) error {
-				if _, err := db.Exec("VACUUM INTO ?", targetPath); err != nil {
-					return fmt.Errorf("failed to backup database %s: %w", targetPath, err)
-				}
-
-				fmt.Fprintln(cmd.OutOrStdout(), "Database backup completed successfully")
-				return nil
-			})
+				RunE: func(cmd *cobra.Command, args []string) error {
+					return command.execute(args)
+				},
+			},
 		},
 	}
 
-	return &command
+	return command.AsCobraCommand()
+}
+
+func (c *backupDatabaseCommand) execute(args []string) error {
+	targetPath := args[0]
+
+	return c.WithOpenedDatabase(func(db *sql.DB) error {
+		if _, err := db.Exec("VACUUM INTO ?", targetPath); err != nil {
+			c.PrintErrorf("Failed to backup database")
+			return fmt.Errorf("failed to backup database %s: %w", targetPath, err)
+		}
+
+		c.Printf("Database backup completed successfully")
+		return nil
+	})
 }
