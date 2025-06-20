f8b3f0516a29fdd8d59a87105c83b5545f1b6ebf
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-20 13:31:34 +0200

    extra logging

--- bctbackend/barcode/generation.go
+++ bctbackend/barcode/generation.go
@@ -3,17 +3,20 @@ package barcode
 import (
 	"fmt"
 	"image"
+	"log/slog"
 
 	bclib "github.com/boombuler/barcode"
 	"github.com/boombuler/barcode/code128"
 )
 
 func GenerateBarcode(data string, width int, height int) (image.Image, error) {
+	slog.Debug("Generating barcode", "data", data)
 	barcode, err := code128.Encode(data)
 	if err != nil {
 		return nil, fmt.Errorf("failed to encode barcode: %w", err)
 	}
 
+	slog.Debug("Scaling barcode", "width", width, "height", height)
 	scaledBarcode, err := bclib.Scale(barcode, width, height)
 	if err != nil {
 		return nil, fmt.Errorf("failed to scale barcode: %w", err)
