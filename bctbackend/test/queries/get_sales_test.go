ff69d42e2f617badd7aed99486786023c5fcdab1
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-21 21:19:57 +0200

    refactoring

--- bctbackend/test/queries/get_sales_test.go
+++ bctbackend/test/queries/get_sales_test.go
@@ -13,34 +13,36 @@ import (
 )
 
 func TestGetSales(t *testing.T) {
-	setup, db := NewDatabaseFixture(WithDefaultCategories)
-	defer setup.Close()
+	t.Run("Success", func(t *testing.T) {
+		setup, db := NewDatabaseFixture(WithDefaultCategories)
+		defer setup.Close()
 
-	seller := setup.Seller()
-	cashier := setup.Cashier()
+		seller := setup.Seller()
+		cashier := setup.Cashier()
 
-	itemIds := []models.Id{
-		setup.Item(seller.UserId, aux.WithDummyData(1), aux.WithHidden(false)).ItemID,
-		setup.Item(seller.UserId, aux.WithDummyData(2), aux.WithHidden(false)).ItemID,
-		setup.Item(seller.UserId, aux.WithDummyData(3), aux.WithHidden(false)).ItemID,
-		setup.Item(seller.UserId, aux.WithDummyData(4), aux.WithHidden(false)).ItemID,
-	}
+		itemIds := []models.Id{
+			setup.Item(seller.UserId, aux.WithDummyData(1), aux.WithHidden(false)).ItemID,
+			setup.Item(seller.UserId, aux.WithDummyData(2), aux.WithHidden(false)).ItemID,
+			setup.Item(seller.UserId, aux.WithDummyData(3), aux.WithHidden(false)).ItemID,
+			setup.Item(seller.UserId, aux.WithDummyData(4), aux.WithHidden(false)).ItemID,
+		}
 
-	saleIds := make([]models.Id, len(itemIds))
-	for _, itemId := range itemIds {
-		setup.Sale(cashier.UserId, []models.Id{itemId})
-	}
+		saleIds := make([]models.Id, len(itemIds))
+		for _, itemId := range itemIds {
+			setup.Sale(cashier.UserId, []models.Id{itemId})
+		}
 
-	actualSales := []*models.SaleSummary{}
-	err := queries.NewGetSalesQuery().Execute(db, queries.CollectTo(&actualSales))
-	require.NoError(t, err)
-	require.Len(t, actualSales, len(saleIds))
-
-	for _, actualSale := range actualSales {
-		require.Equal(t, cashier.UserId, actualSale.CashierID)
-
-		saleItems, err := queries.GetSaleItems(db, actualSale.SaleID)
+		actualSales := []*models.SaleSummary{}
+		err := queries.NewGetSalesQuery().Execute(db, queries.CollectTo(&actualSales))
 		require.NoError(t, err)
-		require.Equal(t, 1, len(saleItems))
-	}
+		require.Len(t, actualSales, len(saleIds))
+
+		for _, actualSale := range actualSales {
+			require.Equal(t, cashier.UserId, actualSale.CashierID)
+
+			saleItems, err := queries.GetSaleItems(db, actualSale.SaleID)
+			require.NoError(t, err)
+			require.Equal(t, 1, len(saleItems))
+		}
+	})
 }
