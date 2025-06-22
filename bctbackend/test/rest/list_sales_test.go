b9e76a254ecc876cdb4d9fbf74933a953796b293
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-22 11:37:07 +0200

    added test

--- bctbackend/test/rest/list_sales_test.go
+++ bctbackend/test/rest/list_sales_test.go
@@ -51,6 +51,46 @@ func TestGetAllSales(t *testing.T) {
 			require.Equal(t, expected, actual)
 		})
 
+		t.Run("Two sale", func(t *testing.T) {
+			setup, router, writer := NewRestFixture(WithDefaultCategories)
+			defer setup.Close()
+
+			_, sessionId := setup.LoggedIn(setup.Admin())
+			seller := setup.Seller()
+			cashier := setup.Cashier()
+			items := setup.Items(seller.UserId, 5, aux.WithHidden(false))
+
+			sale1 := setup.Sale(cashier.UserId, []models.Id{items[0].ItemID, items[1].ItemID})
+			sale2 := setup.Sale(cashier.UserId, []models.Id{items[2].ItemID, items[3].ItemID, items[4].ItemID})
+
+			url := path.Sales().String()
+			request := CreateGetRequest(url, WithSessionCookie(sessionId))
+			router.ServeHTTP(writer, request)
+			require.Equal(t, http.StatusOK, writer.Code)
+
+			actual := FromJson[rest.ListSalesSuccessResponse](t, writer.Body.String())
+			expected := &rest.ListSalesSuccessResponse{
+				Sales: []*rest.ListSalesSaleData{
+					{
+						SaleID:            sale1.SaleID,
+						CashierID:         cashier.UserId,
+						TransactionTime:   shared.ConvertTimestampToDateTime(sale1.TransactionTime),
+						ItemCount:         2,
+						TotalPriceInCents: items[0].PriceInCents + items[1].PriceInCents,
+					},
+					{
+						SaleID:            sale2.SaleID,
+						CashierID:         cashier.UserId,
+						TransactionTime:   shared.ConvertTimestampToDateTime(sale2.TransactionTime),
+						ItemCount:         3,
+						TotalPriceInCents: items[2].PriceInCents + items[3].PriceInCents + items[4].PriceInCents,
+					},
+				},
+				SaleCount: 2,
+			}
+			require.Equal(t, expected, actual)
+		})
+
 		t.Run("List all sales with startId", func(t *testing.T) {
 			for _, k := range []int{1, 2, 5, 25} {
 				testLabel := fmt.Sprintf("k = %d", k)
