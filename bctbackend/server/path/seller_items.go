a5e165cb58d997654f72f6127e0230737fba9d43
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-26 10:54:11 +0200

    refactoring

--- bctbackend/server/path/seller_items.go
+++ bctbackend/server/path/seller_items.go
@@ -5,16 +5,20 @@ import (
 	"fmt"
 )
 
-type sellerItemsPath struct{}
-
-func SellerItems() *sellerItemsPath {
-	return &sellerItemsPath{}
+type SellerItemsPath struct {
+	Path[*SellerItemsPath]
 }
 
-func (path *sellerItemsPath) WithRawSellerId(id string) string {
+func SellerItems() *SellerItemsPath {
+	path := SellerItemsPath{}
+	path.owner = &path
+	return &path
+}
+
+func (path *SellerItemsPath) WithRawSellerId(id string) string {
 	return fmt.Sprintf("/api/v1/sellers/%s/items", id)
 }
 
-func (path *sellerItemsPath) WithSellerId(sellerId models.Id) string {
+func (path *SellerItemsPath) WithSellerId(sellerId models.Id) string {
 	return path.WithRawSellerId(sellerId.String())
 }
