c33f233d97beed2eecb6a919bec4ac80f8d647b9
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-30 16:13:50 +0200

    closes #455

--- bctbackend/commands/database/init.go
+++ bctbackend/commands/database/init.go
@@ -5,57 +5,69 @@ import (
 	"bctbackend/database"
 	dberr "bctbackend/database/errors"
 	"errors"
-	"fmt"
 
 	"github.com/MakeNowJust/heredoc"
 	"github.com/spf13/cobra"
 )
 
+type InitializeDatabaseCommand struct {
+	common.Command
+	noCategories bool
+}
+
 func NewDatabaseInitCommand() *cobra.Command {
-	var noCategories bool
+	var command *InitializeDatabaseCommand
 
-	command := cobra.Command{
-		Use:   "init",
-		Short: "Initialize the database",
-		Long: heredoc.Doc(`
-			This command makes creates a new database file and initializes the database.
-			It will also create the default categories unless the --no-categories flag is set.
-			If a database file already exists, it will NOT be overwritten.
-			If you need to create a fresh database, either use a different path
-			or delete the existing database.
-		`),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			databasePath, err := common.GetDatabasePath()
-			if err != nil {
-				// TODO Write to correct output stream
-				fmt.Fprintf(cmd.ErrOrStderr(), "Failed to get database path: %s\n", err.Error())
-				return err
-			}
-
-			db, err := database.CreateDatabase(databasePath)
-
-			if err != nil {
-				if errors.Is(err, dberr.ErrDatabaseAlreadyExists) {
-					fmt.Fprint(cmd.
-						ErrOrStderr(),
-						heredoc.Docf(`
-							Database file %s already exists.
-							To create a new database, either use a different path or delete the existing file.
-						`, databasePath))
-					return err
-				}
-				fmt.Fprintf(cmd.ErrOrStderr(), "Failed to create database file: %v\n", err)
-				return err
-			}
-
-			db.Close()
-
-			fmt.Fprintf(cmd.OutOrStdout(), "Database file successfully created at %s\n", databasePath)
-			return nil
+	command = &InitializeDatabaseCommand{
+		Command: common.Command{
+			CobraCommand: &cobra.Command{
+				Use:   "init",
+				Short: "Initialize the database",
+				Long: heredoc.Doc(`
+					This command makes creates a new database file and initializes the database.
+					It will also create the default categories unless the --no-categories flag is set.
+					If a database file already exists, it will NOT be overwritten.
+					If you need to create a fresh database, either use a different path
+					or delete the existing database.
+				`),
+				RunE: func(cmd *cobra.Command, args []string) error {
+					return command.execute()
+				},
+			},
 		},
 	}
 
-	command.Flags().BoolVar(&noCategories, "no-categories", false, "Do not add default categories")
+	command.CobraCommand.Flags().BoolVar(&command.noCategories, "no-categories", false, "Do not add default categories")
 
-	return &command
+	return command.AsCobraCommand()
+}
+
+func (c *InitializeDatabaseCommand) execute() error {
+	databasePath, err := common.GetDatabasePath()
+	if err != nil {
+		c.PrintErrorf("Failed to get database path: %s\n", err.Error())
+		return err
+	}
+
+	db, err := database.CreateDatabase(databasePath)
+
+	if err != nil {
+		if errors.Is(err, dberr.ErrDatabaseAlreadyExists) {
+			c.PrintErrorf(heredoc.Docf(
+				`
+					Database file %s already exists.
+					To create a new database, either use a different path or delete the existing file.
+				`, databasePath))
+
+			return err
+		}
+
+		c.PrintErrorf("Failed to create database file: %v\n", err)
+		return err
+	}
+
+	defer db.Close()
+
+	c.Printf("Database file successfully created at %s\n", databasePath)
+	return nil
 }
