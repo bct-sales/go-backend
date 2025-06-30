dff3a6d16befd5321049e133e00bb8c149face2f
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-30 20:20:16 +0200

    closes #458

--- bctbackend/commands/user/remove.go
+++ bctbackend/commands/user/remove.go
@@ -5,43 +5,56 @@ import (
 	"bctbackend/database/models"
 	"bctbackend/database/queries"
 	"database/sql"
-	"fmt"
 
 	"github.com/MakeNowJust/heredoc"
 	"github.com/spf13/cobra"
 )
 
+type RemoveUserCommand struct {
+	common.Command
+}
+
 func NewUserRemoveCommand() *cobra.Command {
-	command := cobra.Command{
-		Use:   "remove <user-id>",
-		Short: "Removes a user",
-		Long: heredoc.Doc(`
+	var command *RemoveUserCommand
+
+	command = &RemoveUserCommand{
+		Command: common.Command{
+			CobraCommand: &cobra.Command{
+				Use:   "remove <user-id>",
+				Short: "Removes a user",
+				Long: heredoc.Doc(`
 				This command permanently removes a user from the database.
 				This command is only provided for completeness but is
 				not intended to be actually used.
 
 				It is not possible to remove a user that has items associated with them.
 			   `),
-		Args: cobra.ExactArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return common.WithOpenedDatabase(cmd.ErrOrStderr(), func(db *sql.DB) error {
-				// Parse the user ID from the first argument
-				userId, err := models.ParseId(args[0])
-				if err != nil {
-					fmt.Fprintf(cmd.ErrOrStderr(), "Invalid user ID: %s\n", args[0])
-					return err
-				}
-
-				if err = queries.RemoveUserWithId(db, userId); err != nil {
-					fmt.Fprintln(cmd.ErrOrStderr(), "Failed to remove user")
-					return err
-				}
-
-				fmt.Fprintln(cmd.OutOrStdout(), "User removed successfully")
-				return nil
-			})
+				Args: cobra.ExactArgs(1),
+				RunE: func(cmd *cobra.Command, args []string) error {
+					return command.execute(args)
+				},
+			},
 		},
 	}
 
-	return &command
+	return command.AsCobraCommand()
+}
+
+func (c *RemoveUserCommand) execute(args []string) error {
+	return c.WithOpenedDatabase(func(db *sql.DB) error {
+		userId, err := models.ParseId(args[0])
+		if err != nil {
+			c.PrintErrorf("Invalid user ID: %s\n", args[0])
+			return err
+		}
+
+		if err = queries.RemoveUserWithId(db, userId); err != nil {
+			c.PrintErrorf("Failed to remove user")
+			return err
+		}
+
+		c.Printf("User removed successfully")
+
+		return nil
+	})
 }
