2e82fba810ae465e2bd8e9f16947bfb1826e29b8
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-30 20:18:15 +0200

    closes #459

--- bctbackend/commands/user/set-password.go
+++ bctbackend/commands/user/set-password.go
@@ -10,33 +10,47 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type SetUserPasswordCommand struct {
+	common.Command
+}
+
 func NewUserSetPasswordCommand() *cobra.Command {
-	command := cobra.Command{
-		Use:   "set-password <user-id> <new-password>",
-		Short: "Sets user password",
-		Long:  `This command updates a user's password.`,
-		Args:  cobra.ExactArgs(2),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return common.WithOpenedDatabase(cmd.ErrOrStderr(), func(db *sql.DB) error {
-				// Parse the user ID from the first argument
-				userId, err := models.ParseId(args[0])
-				if err != nil {
-					fmt.Fprintf(cmd.ErrOrStderr(), "Invalid user ID: %s\n", args[0])
-					return err
-				}
-				newPassword := args[1]
+	var command *SetUserPasswordCommand
 
-				err = queries.UpdateUserPassword(db, userId, newPassword)
-				if err != nil {
-					fmt.Fprintln(cmd.ErrOrStderr(), "Failed to update user password")
-					return fmt.Errorf("failed to update database: %w", err)
-				}
-
-				fmt.Fprintln(cmd.OutOrStdout(), "Password updated successfully")
-				return nil
-			})
+	command = &SetUserPasswordCommand{
+		Command: common.Command{
+			CobraCommand: &cobra.Command{
+				Use:   "set-password <user-id> <new-password>",
+				Short: "Sets user password",
+				Long:  `This command updates a user's password.`,
+				Args:  cobra.ExactArgs(2),
+				RunE: func(cmd *cobra.Command, args []string) error {
+					return command.execute(args)
+				},
+			},
 		},
 	}
 
-	return &command
+	return command.AsCobraCommand()
+}
+
+func (c *SetUserPasswordCommand) execute(args []string) error {
+	return c.WithOpenedDatabase(func(db *sql.DB) error {
+		// Parse the user ID from the first argument
+		userId, err := models.ParseId(args[0])
+		if err != nil {
+			c.PrintErrorf("Invalid user ID: %s\n", args[0])
+			return err
+		}
+		newPassword := args[1]
+
+		err = queries.UpdateUserPassword(db, userId, newPassword)
+		if err != nil {
+			c.PrintErrorf("Failed to update user password")
+			return fmt.Errorf("failed to update database: %w", err)
+		}
+
+		c.Printf("Password updated successfully")
+		return nil
+	})
 }
