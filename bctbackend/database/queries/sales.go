38e7b9d923cfaf29e3c3ae596e43a54a7d6b6576
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-07-20 19:54:39 +0200

    GetSaleWithId wraps error

--- bctbackend/database/queries/sales.go
+++ bctbackend/database/queries/sales.go
@@ -263,7 +263,11 @@ func (q *GetSalesQuery) orderClause() string {
 
 // GetSaleWithId returns the sale with the given saleId.
 // A ErrNoSuchSale is returned if no sale with the given saleId exists.
-func GetSaleWithId(db *sql.DB, saleId models.Id) (*models.Sale, error) {
+func GetSaleWithId(db *sql.DB, saleId models.Id) (r_result *models.Sale, r_err error) {
+	defer func() {
+		r_err = dberr.WrapError(r_err)
+	}()
+
 	var cashierId models.Id
 	var transactionTime models.Timestamp
 	err := db.QueryRow(
