c0cabd954c9c13969ee484ce95b8bc30ace7c4f6
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-07-20 23:32:53 +0200

    updated TestAddSession

--- bctbackend/test/queries/add_session_test.go
+++ bctbackend/test/queries/add_session_test.go
@@ -14,7 +14,7 @@ import (
 
 func TestAddSession(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
-		for _, roleId := range []models.RoleId{models.NewAdminRoleId(), models.NewCashierRoleId(), models.NewSellerRoleId()} {
+		for _, roleId := range models.ListRoles() {
 			setup, db := NewDatabaseFixture(WithDefaultCategories)
 			defer setup.Close()
 
