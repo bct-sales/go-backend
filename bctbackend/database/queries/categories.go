b356ed13b30373d4e214b2b7f1a0c6bb684d2823
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-07-20 19:29:54 +0200

    CategoryWithIdExists wraps error

--- bctbackend/database/queries/categories.go
+++ bctbackend/database/queries/categories.go
@@ -67,9 +67,10 @@ func AddCategoryWithId(db *sql.DB, categoryId models.Id, categoryName string) (r
 	return nil
 }
 
-func CategoryWithIdExists(
-	db *sql.DB,
-	categoryId models.Id) (bool, error) {
+func CategoryWithIdExists(db *sql.DB, categoryId models.Id) (r_result bool, r_err error) {
+	defer func() {
+		r_err = dberr.WrapError(r_err)
+	}()
 
 	row := db.QueryRow(
 		`
