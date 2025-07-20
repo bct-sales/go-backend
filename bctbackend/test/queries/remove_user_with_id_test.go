86d2c6c2c1c6edf4e36c970ed4a35624dd1a956b
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-07-21 00:30:04 +0200

    closes #479

---
+++ bctbackend/test/queries/remove_user_with_id_test.go
@@ -0,0 +1,61 @@
+//go:build test
+
+package queries
+
+import (
+	dberr "bctbackend/database/errors"
+	"bctbackend/database/models"
+	"bctbackend/database/queries"
+	. "bctbackend/test/setup"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRemoveUserWithId(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		setup, db := NewDatabaseFixture(WithDefaultCategories)
+		defer setup.Close()
+
+		seller := setup.Seller()
+		cashier := setup.Cashier()
+
+		{
+			sellerExists, err := queries.UserWithIdExists(db, seller.UserId)
+			require.NoError(t, err)
+			require.True(t, sellerExists)
+		}
+		{
+			cashierExists, err := queries.UserWithIdExists(db, cashier.UserId)
+			require.NoError(t, err)
+			require.True(t, cashierExists)
+		}
+
+		err := queries.RemoveUserWithId(db, seller.UserId)
+		require.NoError(t, err)
+
+		{
+			sellerExists, err := queries.UserWithIdExists(db, seller.UserId)
+			require.NoError(t, err)
+			require.False(t, sellerExists)
+		}
+		{
+			cashierExists, err := queries.UserWithIdExists(db, cashier.UserId)
+			require.NoError(t, err)
+			require.True(t, cashierExists)
+		}
+	})
+
+	t.Run("Failure", func(t *testing.T) {
+		t.Run("Removing non-existing user", func(t *testing.T) {
+			setup, db := NewDatabaseFixture(WithDefaultCategories)
+			defer setup.Close()
+
+			userId := models.Id(99999)
+			setup.RequireNoSuchUsers(t, userId)
+
+			err := queries.RemoveUserWithId(db, userId)
+			requireDatabaseWrappedError(t, err, dberr.ErrNoSuchUser)
+		})
+	})
+}
