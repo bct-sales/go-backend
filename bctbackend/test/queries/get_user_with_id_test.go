280a5e44c995879840de776d4d61919ad36f056f
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-07-21 00:32:17 +0200

    closes #478

--- bctbackend/test/queries/get_user_with_id_test.go
+++ bctbackend/test/queries/get_user_with_id_test.go
@@ -3,6 +3,7 @@
 package queries
 
 import (
+	dberr "bctbackend/database/errors"
 	models "bctbackend/database/models"
 	"bctbackend/database/queries"
 	aux "bctbackend/test/helpers"
@@ -13,21 +14,34 @@ import (
 )
 
 func TestGetUser(t *testing.T) {
-	setup, db := NewDatabaseFixture(WithDefaultCategories)
-	defer setup.Close()
+	t.Run("Success", func(t *testing.T) {
+		setup, db := NewDatabaseFixture(WithDefaultCategories)
+		defer setup.Close()
 
-	lastActivity := models.Timestamp(2)
-	user := models.User{
-		Password:     "xyz",
-		UserId:       models.Id(1),
-		RoleId:       models.NewSellerRoleId(),
-		CreatedAt:    models.Timestamp(1),
-		LastActivity: &lastActivity,
-	}
+		lastActivity := models.Timestamp(2)
+		user := models.User{
+			Password:     "xyz",
+			UserId:       models.Id(1),
+			RoleId:       models.NewSellerRoleId(),
+			CreatedAt:    models.Timestamp(1),
+			LastActivity: &lastActivity,
+		}
 
-	user.UserId = setup.User(user.RoleId, aux.WithCreatedAt(user.CreatedAt), aux.WithLastActivity(*user.LastActivity), aux.WithPassword(user.Password)).UserId
+		user.UserId = setup.User(user.RoleId, aux.WithCreatedAt(user.CreatedAt), aux.WithLastActivity(*user.LastActivity), aux.WithPassword(user.Password)).UserId
 
-	actual, err := queries.GetUserWithId(db, user.UserId)
-	require.NoError(t, err)
-	require.Equal(t, user, *actual)
+		actual, err := queries.GetUserWithId(db, user.UserId)
+		require.NoError(t, err)
+		require.Equal(t, user, *actual)
+	})
+
+	t.Run("Failure", func(t *testing.T) {
+		setup, db := NewDatabaseFixture(WithDefaultCategories)
+		defer setup.Close()
+
+		userId := models.Id(999)
+		setup.RequireNoSuchUsers(t, userId)
+
+		_, err := queries.GetUserWithId(db, userId)
+		requireDatabaseWrappedError(t, err, dberr.ErrNoSuchUser)
+	})
 }
