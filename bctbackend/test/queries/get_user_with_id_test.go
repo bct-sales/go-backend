fc4281a32253a5ad7a01e1eea0782eefd8edd941
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-07-21 00:32:27 +0200

    renamed test

--- bctbackend/test/queries/get_user_with_id_test.go
+++ bctbackend/test/queries/get_user_with_id_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGetUser(t *testing.T) {
+func TestGetUserWithId(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
 		setup, db := NewDatabaseFixture(WithDefaultCategories)
 		defer setup.Close()
