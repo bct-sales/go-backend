2fffe1cfc3ae983b0778afd051915e79f9e9e766
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-07-21 11:32:42 +0200

    closes #462

--- bctbackend/test/queries/remove_sale_test.go
+++ bctbackend/test/queries/remove_sale_test.go
@@ -14,39 +14,43 @@ import (
 )
 
 func TestRemoveSale(t *testing.T) {
-	setup, db := NewDatabaseFixture(WithDefaultCategories)
-	defer setup.Close()
+	t.Run("Success", func(t *testing.T) {
+		setup, db := NewDatabaseFixture(WithDefaultCategories)
+		defer setup.Close()
 
-	seller := setup.Seller()
-	cashier := setup.Cashier()
-	sale1ItemIds := []models.Id{
-		setup.Item(seller.UserId, aux.WithDummyData(1), aux.WithHidden(false)).ItemID,
-		setup.Item(seller.UserId, aux.WithDummyData(2), aux.WithHidden(false)).ItemID,
-	}
-	sale2ItemIds := []models.Id{
-		setup.Item(seller.UserId, aux.WithDummyData(3), aux.WithHidden(false)).ItemID,
-		setup.Item(seller.UserId, aux.WithDummyData(4), aux.WithHidden(false)).ItemID,
-	}
+		seller := setup.Seller()
+		cashier := setup.Cashier()
+		sale1ItemIds := []models.Id{
+			setup.Item(seller.UserId, aux.WithDummyData(1), aux.WithHidden(false)).ItemID,
+			setup.Item(seller.UserId, aux.WithDummyData(2), aux.WithHidden(false)).ItemID,
+		}
+		sale2ItemIds := []models.Id{
+			setup.Item(seller.UserId, aux.WithDummyData(3), aux.WithHidden(false)).ItemID,
+			setup.Item(seller.UserId, aux.WithDummyData(4), aux.WithHidden(false)).ItemID,
+		}
 
-	sale1 := setup.Sale(cashier.UserId, sale1ItemIds)
-	sale2 := setup.Sale(cashier.UserId, sale2ItemIds)
+		sale1 := setup.Sale(cashier.UserId, sale1ItemIds)
+		sale2 := setup.Sale(cashier.UserId, sale2ItemIds)
 
-	err := queries.RemoveSale(db, sale1.SaleID)
-	require.NoError(t, err)
+		err := queries.RemoveSale(db, sale1.SaleID)
+		require.NoError(t, err)
 
-	sale1Exists, err := queries.SaleWithIdExists(db, sale1.SaleID)
-	require.NoError(t, err)
-	require.False(t, sale1Exists)
+		sale1Exists, err := queries.SaleWithIdExists(db, sale1.SaleID)
+		require.NoError(t, err)
+		require.False(t, sale1Exists)
 
-	sale2Exists, err := queries.SaleWithIdExists(db, sale2.SaleID)
-	require.NoError(t, err)
-	require.True(t, sale2Exists)
-}
+		sale2Exists, err := queries.SaleWithIdExists(db, sale2.SaleID)
+		require.NoError(t, err)
+		require.True(t, sale2Exists)
+	})
 
-func TestRemoveNonexistentSale(t *testing.T) {
-	setup, db := NewDatabaseFixture(WithDefaultCategories)
-	defer setup.Close()
+	t.Run("Failure", func(t *testing.T) {
+		t.Run("Nonexistent sale", func(t *testing.T) {
+			setup, db := NewDatabaseFixture(WithDefaultCategories)
+			defer setup.Close()
 
-	err := queries.RemoveSale(db, 0)
-	requireDatabaseWrappedError(t, err, dberr.ErrNoSuchSale)
+			err := queries.RemoveSale(db, 0)
+			requireDatabaseWrappedError(t, err, dberr.ErrNoSuchSale)
+		})
+	})
 }
