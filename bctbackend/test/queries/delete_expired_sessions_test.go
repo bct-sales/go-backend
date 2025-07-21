2bcde57def925c59689efc599f7e0b89b11cde1a
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-07-21 13:42:11 +0200

    refactoring

--- bctbackend/test/queries/delete_expired_sessions_test.go
+++ bctbackend/test/queries/delete_expired_sessions_test.go
@@ -13,42 +13,44 @@ import (
 )
 
 func TestDeleteExpiredSessions(t *testing.T) {
-	for cutoff := 0; cutoff < 100; cutoff += 10 {
-		testLabel := fmt.Sprintf("cutoff=%d", cutoff)
-		t.Run(testLabel, func(t *testing.T) {
-			t.Parallel()
+	t.Run("Success", func(t *testing.T) {
+		for cutoff := 0; cutoff < 100; cutoff += 10 {
+			testLabel := fmt.Sprintf("cutoff=%d", cutoff)
+			t.Run(testLabel, func(t *testing.T) {
+				t.Parallel()
 
-			setup, db := NewDatabaseFixture(WithDefaultCategories)
-			defer setup.Close()
+				setup, db := NewDatabaseFixture(WithDefaultCategories)
+				defer setup.Close()
 
-			user := setup.Admin()
-			expiredSessions := []models.SessionId{}
-			unexpiredSessions := []models.SessionId{}
+				user := setup.Admin()
+				expiredSessions := []models.SessionId{}
+				unexpiredSessions := []models.SessionId{}
 
-			for i := 0; i < 100; i++ {
-				expirationTime := models.Timestamp(0)
-				sessionId, err := queries.AddSession(db, user.UserId, expirationTime)
-				require.NoError(t, err)
+				for i := 0; i < 100; i++ {
+					expirationTime := models.Timestamp(0)
+					sessionId, err := queries.AddSession(db, user.UserId, expirationTime)
+					require.NoError(t, err)
 
-				if expirationTime < models.Timestamp(cutoff) {
-					expiredSessions = append(expiredSessions, sessionId)
-				} else {
-					unexpiredSessions = append(unexpiredSessions, sessionId)
+					if expirationTime < models.Timestamp(cutoff) {
+						expiredSessions = append(expiredSessions, sessionId)
+					} else {
+						unexpiredSessions = append(unexpiredSessions, sessionId)
+					}
 				}
-			}
 
-			err := queries.DeleteExpiredSessions(db, models.Timestamp(cutoff))
-			require.NoError(t, err)
-
-			for _, sessionId := range expiredSessions {
-				_, err := queries.GetSessionById(db, sessionId)
-				require.Error(t, err)
-			}
-
-			for _, sessionId := range unexpiredSessions {
-				_, err := queries.GetSessionById(db, sessionId)
+				err := queries.DeleteExpiredSessions(db, models.Timestamp(cutoff))
 				require.NoError(t, err)
-			}
-		})
-	}
+
+				for _, sessionId := range expiredSessions {
+					_, err := queries.GetSessionById(db, sessionId)
+					require.Error(t, err)
+				}
+
+				for _, sessionId := range unexpiredSessions {
+					_, err := queries.GetSessionById(db, sessionId)
+					require.NoError(t, err)
+				}
+			})
+		}
+	})
 }
