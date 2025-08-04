7e3ef48e0629ebce7dc145bfd06ff34593225eb8
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-08-04 12:26:40 +0200

    added GetSoldItemsCount test

---
+++ bctbackend/test/queries/get_sold_items_count_test.go
@@ -0,0 +1,28 @@
+//go:build test
+
+package queries
+
+import (
+	"bctbackend/database/queries"
+	aux "bctbackend/test/helpers"
+	. "bctbackend/test/setup"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetSoldItemsCount(t *testing.T) {
+	t.Run("Only unsold items", func(t *testing.T) {
+		setup, db := NewDatabaseFixture(WithDefaultCategories)
+		defer setup.Close()
+
+		seller := setup.Seller()
+
+		setup.Items(seller.UserId, 1, aux.WithHidden(false))
+
+		actual, err := queries.GetSoldItemsCount(db)
+		require.NoError(t, err)
+		require.Equal(t, 0, actual.Distinct)
+		require.Equal(t, 0, actual.IncludeMultiples)
+	})
+}
