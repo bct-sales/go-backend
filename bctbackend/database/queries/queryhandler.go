d44a714e48707828cc19207239bbe1a8441a257c
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-08-06 15:53:23 +0200

    moved code

--- bctbackend/database/queries/queryhandler.go
+++ bctbackend/database/queries/queryhandler.go
@@ -2,10 +2,61 @@ package queries
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 type QueryHandler interface {
 	Exec(query string, args ...any) (sql.Result, error)
 	Query(query string, args ...any) (*sql.Rows, error)
 	QueryRow(query string, args ...any) *sql.Row
+}
+
+type Transaction struct {
+	transaction *sql.Tx
+	committed   bool
+}
+
+func NewTransaction(db *sql.DB) (*Transaction, error) {
+	tx, err := db.Begin()
+	if err != nil {
+		return nil, fmt.Errorf("failed to start new transaction: %w", err)
+	}
+
+	return &Transaction{
+		transaction: tx,
+		committed:   false,
+	}, nil
+}
+
+func (t *Transaction) Commit() error {
+	if err := t.transaction.Commit(); err != nil {
+		return fmt.Errorf("failed to commit transaction: %w", err)
+	}
+
+	t.committed = true
+	return nil
+}
+
+func (t *Transaction) Rollback() error {
+	if t.committed {
+		return nil
+	}
+
+	if err := t.transaction.Rollback(); err != nil {
+		return fmt.Errorf("failed to roll back transaction: %w", err)
+	}
+
+	return nil
+}
+
+func (t *Transaction) Exec(query string, args ...any) (sql.Result, error) {
+	return t.transaction.Exec(query, args...)
+}
+
+func (t *Transaction) Query(query string, args ...any) (*sql.Rows, error) {
+	return t.transaction.Query(query, args...)
+}
+
+func (t *Transaction) QueryRow(query string, args ...any) *sql.Row {
+	return t.transaction.QueryRow(query, args...)
 }
