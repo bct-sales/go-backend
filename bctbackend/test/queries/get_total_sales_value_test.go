decd71663e4d6b8ead4ccbd0c009ed47eb8cf03e
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-08-06 15:26:46 +0200

    added GetTotalSalesValue test

--- bctbackend/test/queries/get_total_sales_value_test.go
+++ bctbackend/test/queries/get_total_sales_value_test.go
@@ -93,4 +93,22 @@ func TestGetTotalSalesValue(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, items[0].PriceInCents*3, total)
 	})
+
+	t.Run("Multiple sales, multiple cashiers, no shared items among sales", func(t *testing.T) {
+		setup, db := NewDatabaseFixture(WithDefaultCategories)
+		defer setup.Close()
+
+		seller := setup.Seller()
+		cashier1 := setup.Cashier()
+		cashier2 := setup.Cashier()
+		items := setup.Items(seller.UserId, 10, aux.WithHidden(false))
+		setup.Sale(cashier1.UserId, []models.Id{items[0].ItemID})
+		setup.Sale(cashier2.UserId, []models.Id{items[1].ItemID})
+		setup.Sale(cashier2.UserId, []models.Id{items[2].ItemID})
+		setup.Sale(cashier2.UserId, []models.Id{items[3].ItemID})
+
+		total, err := queries.GetTotalSalesValue(db)
+		require.NoError(t, err)
+		require.Equal(t, items[0].PriceInCents+items[1].PriceInCents+items[2].PriceInCents+items[3].PriceInCents, total)
+	})
 }
