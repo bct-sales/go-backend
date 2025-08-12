5391ebbc09c796c06225a07de142dd8d6c4f3ac2
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-08-12 09:44:38 +0200

    code layout

--- bctbackend/database/queries/sales.go
+++ bctbackend/database/queries/sales.go
@@ -899,10 +899,14 @@ func GetTotalSalesValue(db DatabaseQuerier) (r_result models.MoneyInCents, r_err
 	var totalValue models.MoneyInCents
 	err := db.QueryRow(
 		`
-			SELECT COALESCE(SUM(items.price_in_cents), 0) as total
-			FROM sales
-			INNER JOIN sale_items ON sales.sale_id = sale_items.sale_id
-			INNER JOIN items ON sale_items.item_id = items.item_id
+			SELECT
+				COALESCE(SUM(items.price_in_cents), 0) as total
+			FROM
+				sales
+			INNER JOIN
+				sale_items ON sales.sale_id = sale_items.sale_id
+			INNER JOIN
+				items ON sale_items.item_id = items.item_id
 		`,
 	).Scan(&totalValue)
 
