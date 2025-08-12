036ac5dfad3377d0a3ff65a95d4d7f3341357ab6
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-08-12 09:57:44 +0200

    no args for user add command

--- bctbackend/commands/user/add.go
+++ bctbackend/commands/user/add.go
@@ -28,6 +28,7 @@ func NewUserAddCommand() *cobra.Command {
 				RunE: func(cmd *cobra.Command, args []string) error {
 					return command.execute()
 				},
+				Args: cobra.NoArgs,
 			},
 		},
 	}
