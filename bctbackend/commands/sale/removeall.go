eca00f86a310f604e197f753e547cb01583ab1f6
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-08-12 09:56:42 +0200

    no args for sale remove-all command

--- bctbackend/commands/sale/removeall.go
+++ bctbackend/commands/sale/removeall.go
@@ -28,6 +28,7 @@ func NewRemoveAllSalesCommand() *cobra.Command {
 				RunE: func(cmd *cobra.Command, args []string) error {
 					return command.execute()
 				},
+				Args: cobra.NoArgs,
 			},
 		},
 	}
