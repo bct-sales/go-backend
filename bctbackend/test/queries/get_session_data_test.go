d75722524fb819c6415d6f24e4f0d6a0f552a2f2
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-08-14 11:38:09 +0200

    updated GetSessionData tests

--- bctbackend/test/queries/get_session_data_test.go
+++ bctbackend/test/queries/get_session_data_test.go
@@ -6,6 +6,7 @@ import (
 	dberr "bctbackend/database/errors"
 	"bctbackend/database/models"
 	"bctbackend/database/queries"
+	aux "bctbackend/test/helpers"
 	. "bctbackend/test/setup"
 	"testing"
 
@@ -23,12 +24,15 @@ func TestGetSessionData(t *testing.T) {
 				setup, db := NewDatabaseFixture(WithDefaultCategories)
 				defer setup.Close()
 
-				seller, sessionId := setup.LoggedIn(setup.User(roleId))
+				seller, sessionId := setup.LoggedIn(setup.User(roleId), aux.WithExpiration(100))
 				sessionData, err := queries.GetSessionData(db, sessionId, setup.Clock.Now())
+				expectedExpirationTime := setup.Clock.Now() + models.Timestamp(100)
+
 				require.NoError(t, err)
 				require.NotNil(t, sessionData)
 				require.Equal(t, seller.UserId, sessionData.UserId)
 				require.Equal(t, roleId, sessionData.RoleId)
+				require.Equal(t, expectedExpirationTime, sessionData.ExpirationTime)
 			})
 		}
 	})
@@ -40,6 +44,20 @@ func TestGetSessionData(t *testing.T) {
 
 			invalidSessionId := models.SessionId("invalid-session-id")
 			_, err := queries.GetSessionData(db, invalidSessionId, setup.Clock.Now())
+			requireDatabaseWrappedError(t, err, dberr.ErrNoSuchSession)
+		})
+
+		t.Run("Expired session", func(t *testing.T) {
+			setup, db := NewDatabaseFixture(WithDefaultCategories)
+			defer setup.Close()
+
+			seller := setup.Seller()
+			sessionId := setup.Session(seller.UserId, aux.WithExpiration(10))
+
+			// Advance time to ensure session is expired
+			setup.Clock.Advance(11)
+
+			_, err := queries.GetSessionData(db, sessionId, setup.Clock.Now())
 			requireDatabaseWrappedError(t, err, dberr.ErrNoSuchSession)
 		})
 	})
