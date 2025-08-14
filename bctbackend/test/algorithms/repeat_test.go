3a13e66d411b943fa6d1acc3ceaeb3509613c651
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-08-14 15:17:55 +0200

    removed parallel

--- bctbackend/test/algorithms/repeat_test.go
+++ bctbackend/test/algorithms/repeat_test.go
@@ -14,8 +14,6 @@ func TestRepeat(t *testing.T) {
 	for _, repeatCount := range []int{0, 1, 2, 3, 10} {
 		testLabel := fmt.Sprintf("Repeat %d times", repeatCount)
 		t.Run(testLabel, func(t *testing.T) {
-			t.Parallel()
-
 			value := 0
 
 			algorithms.Repeat(repeatCount, func() {
