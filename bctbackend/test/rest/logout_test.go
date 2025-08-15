09fe0548b452ab6e952920eff56095eb943688ad
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-08-15 19:47:32 +0200

    added Logout endpoint test

--- bctbackend/test/rest/logout_test.go
+++ bctbackend/test/rest/logout_test.go
@@ -5,6 +5,7 @@ package rest
 import (
 	path "bctbackend/server/paths"
 	"bctbackend/server/rest"
+	aux "bctbackend/test/helpers"
 	. "bctbackend/test/setup"
 	"net/http"
 	"testing"
@@ -23,5 +24,19 @@ func TestLogout(t *testing.T) {
 		request := CreatePostRequest(url, &rest.LogoutPayload{}, WithSessionCookie(sessionId))
 		router.ServeHTTP(writer, request)
 		require.Equal(t, http.StatusOK, writer.Code)
+	})
+
+	t.Run("Expired session", func(t *testing.T) {
+		setup, router, writer := NewRestFixture(WithDefaultCategories)
+		defer setup.Close()
+
+		_, sessionId := setup.LoggedIn(setup.Admin(), aux.WithExpiration(100))
+
+		setup.Clock.Advance(200)
+
+		url := path.Logout()
+		request := CreatePostRequest(url, &rest.LogoutPayload{}, WithSessionCookie(sessionId))
+		router.ServeHTTP(writer, request)
+		require.Equal(t, http.StatusUnauthorized, writer.Code, "body", writer.Body.String())
 	})
 }
