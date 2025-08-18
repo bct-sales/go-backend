a0c8491b788bc03491854aff211533394f361f0c
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-08-18 12:54:47 +0200

    refactoring

--- test/queries/get_items_test.go
+++ test/queries/get_items_test.go
@@ -106,7 +106,8 @@ func TestGetItems(t *testing.T) {
 			items := setup.Items(seller.UserId, 20, aux.WithHidden(false))
 
 			actualItems := []*models.Item{}
-			err := queries.GetItems(db, queries.CollectTo(&actualItems), queries.AllItems, queries.NewRowSelection(&offset, &limit))
+			rowSelection := queries.NewRowSelection(&offset, &limit)
+			err := queries.GetItems(db, queries.CollectTo(&actualItems), queries.AllItems, rowSelection)
 			require.NoError(t, err)
 			require.Equal(t, limit, len(actualItems))
 
