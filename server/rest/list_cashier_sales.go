4ebbb530ab60edc5df8593d177b50ce4f588ecb1
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-08-18 13:29:33 +0200

    refactoring

--- server/rest/list_cashier_sales.go
+++ server/rest/list_cashier_sales.go
@@ -38,27 +38,27 @@ type listCashierSalesEndpoint struct {
 	Endpoint
 }
 
-func (endpoint *listCashierSalesEndpoint) execute() {
-	uriCashierId, ok := endpoint.extractCashierIdFromUri()
+func (ep *listCashierSalesEndpoint) execute() {
+	uriCashierId, ok := ep.extractCashierIdFromUri()
 	if !ok {
 		return
 	}
 
-	saleSummaries, saleSummariesOk := endpoint.getSaleSummariesFromDatabase(uriCashierId)
+	saleSummaries, saleSummariesOk := ep.getSaleSummariesFromDatabase(uriCashierId)
 	if !saleSummariesOk {
 		return
 	}
 
 	successResponse := ListCashierSalesSuccessResponse{
 		Sales: algorithms.Map(saleSummaries, func(saleSummary *models.SaleSummary) *ListCashierSaleData {
-			return endpoint.convertSaleSummaryToData(saleSummary)
+			return ep.convertSaleSummaryToData(saleSummary)
 		}),
 	}
 
-	endpoint.Context.IndentedJSON(http.StatusOK, successResponse)
+	ep.Context.IndentedJSON(http.StatusOK, successResponse)
 }
 
-func (endpoint *listCashierSalesEndpoint) convertSaleSummaryToData(saleSummary *models.SaleSummary) *ListCashierSaleData {
+func (ep *listCashierSalesEndpoint) convertSaleSummaryToData(saleSummary *models.SaleSummary) *ListCashierSaleData {
 	return &ListCashierSaleData{
 		SaleId:            saleSummary.SaleID,
 		TransactionTime:   rest.ConvertTimestampToDateTime(saleSummary.TransactionTime),
@@ -71,40 +71,40 @@ func (endpoint *listCashierSalesEndpoint) convertSaleSummaryToData(saleSummary *
 // It returns the cashier ID and a boolean indicating success or failure.
 // If the extraction or validation fails, it sends an appropriate error response.
 // False indicates failure, true indicates success.
-func (endpoint *listCashierSalesEndpoint) extractCashierIdFromUri() (models.Id, bool) {
+func (ep *listCashierSalesEndpoint) extractCashierIdFromUri() (models.Id, bool) {
 	var uriParameters struct {
 		CashierId string `binding:"required" uri:"id"`
 	}
-	if err := endpoint.Context.ShouldBindUri(&uriParameters); err != nil {
-		endpoint.Logger.InvalidInput("Failed to bind URI parameters: %v", err)
-		failure_response.InvalidUriParameters(endpoint.Context, err.Error())
+	if err := ep.Context.ShouldBindUri(&uriParameters); err != nil {
+		ep.Logger.InvalidInput("Failed to bind URI parameters: %v", err)
+		failure_response.InvalidUriParameters(ep.Context, err.Error())
 		return 0, false
 	}
 
 	uriUserId, err := models.ParseId(uriParameters.CashierId)
 	if err != nil {
-		endpoint.Logger.InvalidInput("Failed to parse cashier ID \"%s\" from URI: %v", uriParameters.CashierId, err)
-		failure_response.InvalidUserId(endpoint.Context, err.Error())
+		ep.Logger.InvalidInput("Failed to parse cashier ID \"%s\" from URI: %v", uriParameters.CashierId, err)
+		failure_response.InvalidUserId(ep.Context, err.Error())
 		return 0, false
 	}
 
-	if !endpoint.ensureUserHasPermission(uriUserId) {
+	if !ep.ensureUserHasPermission(uriUserId) {
 		return 0, false
 	}
 
 	return uriUserId, true
 }
 
-func (endpoint *listCashierSalesEndpoint) ensureUserHasPermission(queriedUser models.Id) bool {
-	user, err := queries.GetUserWithId(endpoint.Database, endpoint.UserId)
+func (ep *listCashierSalesEndpoint) ensureUserHasPermission(queriedUser models.Id) bool {
+	user, err := queries.GetUserWithId(ep.Database, ep.UserId)
 	if err != nil {
 		if errors.Is(err, dberr.ErrNoSuchUser) {
 			// This should not happen, as the userId is from the logged-in user
-			endpoint.Logger.Bug("Logged in user does not exist")
-			failure_response.Unknown(endpoint.Context, "Bug: logged in user does not exist")
+			ep.Logger.Bug("Logged in user does not exist")
+			failure_response.Unknown(ep.Context, "Bug: logged in user does not exist")
 			return false
 		}
-		failure_response.Unknown(endpoint.Context, "Could not retrieve logged in user: "+err.Error())
+		failure_response.Unknown(ep.Context, "Could not retrieve logged in user: "+err.Error())
 		return false
 	}
 
@@ -113,27 +113,27 @@ func (endpoint *listCashierSalesEndpoint) ensureUserHasPermission(queriedUser mo
 	}
 
 	if user.RoleId.IsCashier() {
-		loggedInUser := endpoint.UserId
+		loggedInUser := ep.UserId
 
 		if loggedInUser != queriedUser {
-			endpoint.Logger.InvalidRequest("User tried to access sales of cashier %d, but is not the owning cashier", queriedUser)
-			failure_response.Forbidden(endpoint.Context, "wrong_role", "Only accessible to owning cashiers or admins")
+			ep.Logger.InvalidRequest("User tried to access sales of cashier %d, but is not the owning cashier", queriedUser)
+			failure_response.Forbidden(ep.Context, "wrong_role", "Only accessible to owning cashiers or admins")
 			return false
 		}
 
 		return true
 	}
 
-	endpoint.Logger.InvalidRequest("User tried to access sales of cashier %d, but is not a cashier or admin", queriedUser)
-	failure_response.Forbidden(endpoint.Context, "wrong_role", "Only accessible to owning cashiers or admins")
+	ep.Logger.InvalidRequest("User tried to access sales of cashier %d, but is not a cashier or admin", queriedUser)
+	failure_response.Forbidden(ep.Context, "wrong_role", "Only accessible to owning cashiers or admins")
 	return false
 }
 
-func (endpoint *listCashierSalesEndpoint) getSaleSummariesFromDatabase(uriCashierId models.Id) ([]*models.SaleSummary, bool) {
+func (ep *listCashierSalesEndpoint) getSaleSummariesFromDatabase(uriCashierId models.Id) ([]*models.SaleSummary, bool) {
 	var saleSummaries []*models.SaleSummary
-	if err := queries.GetCashierSales(endpoint.Database, uriCashierId, queries.CollectTo(&saleSummaries), queries.AllRows()); err != nil {
-		endpoint.Logger.InternalError("Failed to retrieve cashier sales for user %d: %v", uriCashierId, err)
-		failure_response.Unknown(endpoint.Context, "Could not retrieve cashier sales: "+err.Error())
+	if err := queries.GetCashierSales(ep.Database, uriCashierId, queries.CollectTo(&saleSummaries), queries.AllRows()); err != nil {
+		ep.Logger.InternalError("Failed to retrieve cashier sales for user %d: %v", uriCashierId, err)
+		failure_response.Unknown(ep.Context, "Could not retrieve cashier sales: "+err.Error())
 		return nil, false
 	}
 
