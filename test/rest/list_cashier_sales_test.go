64f0e5b3dd8367863c425e5d9fb91ff3613d8884
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-08-18 13:41:50 +0200

    reorganized tests

--- test/rest/list_cashier_sales_test.go
+++ test/rest/list_cashier_sales_test.go
@@ -19,26 +19,28 @@ import (
 func TestListCashierSales(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
 		t.Run("Cashier views own sales", func(t *testing.T) {
-			setup, router, writer := NewRestFixture(WithDefaultCategories)
-			defer setup.Close()
+			t.Run("All sales", func(t *testing.T) {
+				setup, router, writer := NewRestFixture(WithDefaultCategories)
+				defer setup.Close()
 
-			seller := setup.Seller()
-			cashier, sessionId := setup.LoggedIn(setup.Cashier())
-			cashier2 := setup.Cashier()
+				seller := setup.Seller()
+				cashier, sessionId := setup.LoggedIn(setup.Cashier())
+				cashier2 := setup.Cashier()
 
-			items := setup.Items(seller.UserId, 10, aux.WithHidden(false))
-			sales := algorithms.Map(items, func(item *models.Item) *models.Sale { return setup.Sale(cashier.UserId, []models.Id{item.ItemID}) })
-			items2 := setup.Items(seller.UserId, 20, aux.WithHidden(false))
-			algorithms.Map(items2, func(item *models.Item) *models.Sale { return setup.Sale(cashier2.UserId, []models.Id{item.ItemID}) })
+				items := setup.Items(seller.UserId, 10, aux.WithHidden(false))
+				sales := algorithms.Map(items, func(item *models.Item) *models.Sale { return setup.Sale(cashier.UserId, []models.Id{item.ItemID}) })
+				items2 := setup.Items(seller.UserId, 20, aux.WithHidden(false))
+				algorithms.Map(items2, func(item *models.Item) *models.Sale { return setup.Sale(cashier2.UserId, []models.Id{item.ItemID}) })
 
-			url := path.CashierSales(cashier.UserId)
-			request := CreateGetRequest(url, WithSessionCookie(sessionId))
-			router.ServeHTTP(writer, request)
-			require.Equal(t, http.StatusOK, writer.Code)
+				url := path.CashierSales(cashier.UserId)
+				request := CreateGetRequest(url, WithSessionCookie(sessionId))
+				router.ServeHTTP(writer, request)
+				require.Equal(t, http.StatusOK, writer.Code)
 
-			actual := FromJson[rest.ListCashierSalesSuccessResponse](t, writer.Body.String())
-			require.NotNil(t, actual)
-			require.Equal(t, len(sales), len(actual.Sales))
+				actual := FromJson[rest.ListCashierSalesSuccessResponse](t, writer.Body.String())
+				require.NotNil(t, actual)
+				require.Equal(t, len(sales), len(actual.Sales))
+			})
 		})
 
 		t.Run("Admin views sales", func(t *testing.T) {
