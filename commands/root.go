33a13864f0d3ddfabea7e761af11d80c79d2c09e
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-09-05 16:14:21 +0200

    removed redundant code

--- commands/root.go
+++ commands/root.go
@@ -10,13 +10,10 @@ import (
 	"bctbackend/commands/server"
 	"bctbackend/commands/session"
 	"bctbackend/commands/user"
-	"fmt"
 	"log/slog"
 	"os"
-	"path/filepath"
 	"strings"
 
-	"github.com/MakeNowJust/heredoc"
 	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -94,36 +91,4 @@ func Execute() {
 		slog.Debug("An error occurred", "error", err.Error())
 		os.Exit(1)
 	}
-}
-
-func handleMissingConfigurationFile() {
-	absolutePathOfConfigurationFile, err := filepath.Abs(viper.ConfigFileUsed())
-
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error determining absolute path of configuration file:", err)
-		os.Exit(1)
-	}
-
-	fmt.Print(heredoc.Docf(`
-		=======
-		WARNING
-		=======
-		I could not find the configuration file.
-		I looked for it here: %s.
-
-		Possible solutions:
-		* Use this tool to generate a configuration file:
-			$ bctbackend init
-		* Specify a different path using the --config flag:
-		    $ bctbackend --config path/to/your/config.yaml ...
-		* You can also set the BCT_CONFIG environment variable to point to your configuration file.
-		    $ BCT_CONFIG=path/to/your/config.yaml bctbackend ...
-		* You can create a configuration file in the current directory with the name "bctconfig.yaml".
-		    $ touch bctconfig.yaml
-			$ bctbackend ...
-
-		For now, I will proceed with the default configuration,
-		but this is strongly discouraged as it may lead to unexpected behavior.
-		`,
-		absolutePathOfConfigurationFile))
 }
