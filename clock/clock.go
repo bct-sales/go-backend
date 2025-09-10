1aff7786a695cba0534b34d6b7103437de95ba32
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-09-10 09:19:08 +0200

    docs

--- clock/clock.go
+++ clock/clock.go
@@ -2,12 +2,20 @@ package clock
 
 import "bctbackend/database/models"
 
+// Clock allows to query the current time and to create tickers.
 type Clock interface {
+	// Now returns the current system time
 	Now() models.Timestamp
+
+	// NewTicker creates a new ticker.
+	// It will cause the given callback to be called at regular intervals.
 	NewTicker(durationInSeconds int, callback func()) Ticker
+
+	// StopAllTickers stops all tickers created by this clock.
 	StopAllTickers()
 }
 
 type Ticker interface {
+	// Stops this ticker.
 	Stop()
 }
