59123b157c1e24e9e04aa9d5c40d743a00eef4ba
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-09-10 09:32:41 +0200

    docs

--- clock/system.go
+++ clock/system.go
@@ -5,22 +5,26 @@ import (
 	"time"
 )
 
+// A SystemClock gets its time from the OS.
 type SystemClock struct {
 	tickers []*SystemTicker
 }
 
+// Now returns the current system time.
 func (c *SystemClock) Now() models.Timestamp {
 	return models.Now()
 }
 
+// NewSystemClock creates a new system clock.
 func NewSystemClock() *SystemClock {
 	return &SystemClock{tickers: nil}
 }
 
 type SystemTicker struct {
-	stopChannel chan int // Channel to signal the ticker to stop
+	stopChannel chan int // channel to signal the ticker to stop
 }
 
+// NewTicker creates a new ticker that will call the given callback function at regular times.
 func (clock *SystemClock) NewTicker(durationInSeconds int, callback func()) Ticker {
 	ticker := time.NewTicker(time.Duration(durationInSeconds) * time.Second)
 	stopChannel := make(chan int)
@@ -42,6 +46,8 @@ func (clock *SystemClock) NewTicker(durationInSeconds int, callback func()) Tick
 	return &systemTicker
 }
 
+// Stop deactivates the ticker.
+// If the callback is running at the moment, it will finish its work.
 func (ticker *SystemTicker) Stop() {
 	if ticker.stopChannel != nil {
 		ticker.stopChannel <- 0
@@ -49,6 +55,8 @@ func (ticker *SystemTicker) Stop() {
 	}
 }
 
+// StopAllTickers deactivates all times associated with the clock.
+// All callbacks will be allowed to finish their work.
 func (clock *SystemClock) StopAllTickers() {
 	for _, ticker := range clock.tickers {
 		ticker.Stop()
