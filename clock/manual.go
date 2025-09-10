6188c4388dc87c6b1594383f3e97ddc547f19a9e
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-09-10 09:27:21 +0200

    docs

--- clock/manual.go
+++ clock/manual.go
@@ -2,16 +2,19 @@ package clock
 
 import "bctbackend/database/models"
 
+// A ManualClock allows to have a clock where the current time can be advanced manually.
+// Meant for testing and debugging.
 type ManualClock struct {
 	currentTime models.Timestamp
 	tickers     []*ManualTicker
 }
 
+// NewManualClock creates a new manual clock which is set to the given initial time.
 func NewManualClock(initialTime models.Timestamp) *ManualClock {
 	return &ManualClock{currentTime: initialTime, tickers: nil}
 }
 
-func (c *ManualClock) NewTicker(duration int, callback func()) Ticker {
+// NewTicker creates a new ticker associated with the clock.
 func (c *ManualClock) NewTicker(durationInSeconds int, callback func()) Ticker {
 	ticker := &ManualTicker{
 		nextTick: c.currentTime + models.Timestamp(durationInSeconds),
@@ -24,21 +27,25 @@ func (c *ManualClock) NewTicker(durationInSeconds int, callback func()) Ticker {
 	return ticker
 }
 
+// Now returns the time the clock is set at.
 func (c *ManualClock) Now() models.Timestamp {
 	return c.currentTime
 }
 
 type ManualTicker struct {
-	nextTick models.Timestamp
-	interval models.Timestamp
-	active   bool
-	callback func()
+	nextTick models.Timestamp // time left to wait until next tick.
+	interval models.Timestamp // interval at which this ticker invokes its callback.
+	active   bool             // whether the timer is active or not
+	callback func()           // callback to be called at every tick
 }
 
+// Stop deactivates the timer.
 func (t *ManualTicker) Stop() {
 	t.active = false
 }
 
+// Advance advances the manual clock's time.
+// Tickers will call their associated callbacks as needed, in order.
 func (c *ManualClock) Advance(duration models.Timestamp) {
 	c.PruneInactiveTickers()
 
@@ -58,6 +65,7 @@ func (c *ManualClock) Advance(duration models.Timestamp) {
 	c.currentTime = target
 }
 
+// PruneInactiveTickers removes stopped tickers from the list of tickers.
 func (c *ManualClock) PruneInactiveTickers() {
 	activeTimers := []*ManualTicker{}
 
@@ -70,6 +78,8 @@ func (c *ManualClock) PruneInactiveTickers() {
 	c.tickers = activeTimers
 }
 
+// findEarliestTicker returns the ticker that will be the first to tick if time is advanced.
+// Returns nil if there is no such ticker.
 func (c *ManualClock) findEarliestTicker() *ManualTicker {
 	var earliestTicker *ManualTicker = nil
 
@@ -82,6 +92,7 @@ func (c *ManualClock) findEarliestTicker() *ManualTicker {
 	return earliestTicker
 }
 
+// StopAllTickers deactivates all tickers associated with the clock.
 func (c *ManualClock) StopAllTickers() {
 	c.tickers = nil
 }
