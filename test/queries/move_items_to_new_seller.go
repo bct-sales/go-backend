36c531a3ba69fb3c1e5d4b956d8861e400c3d245
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-09-16 17:53:44 +0200

    improved MoveItemsToNewSeller test

--- test/queries/move_items_to_new_seller.go
+++ test/queries/move_items_to_new_seller.go
@@ -16,19 +16,32 @@ import (
 func TestMoveItemsToNewSeller(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
 		t.Run("No frozen items", func(t *testing.T) {
-			setup, _ := NewDatabaseFixture(WithDefaultCategories)
+			setup, db := NewDatabaseFixture(WithDefaultCategories)
 			defer setup.Close()
 
 			oldSeller := setup.Seller()
 			newSeller := setup.Seller()
-			setup.Items(oldSeller.UserId, 10, aux.WithFrozen(false), aux.WithHidden(false))
+			items := setup.Items(oldSeller.UserId, 10, aux.WithFrozen(false), aux.WithHidden(false))
 
 			setup.WithTransaction(t, func(db *queries.TransactionalDatabaseQuerier) {
 				err := queries.MoveItemsToNewSeller(db, oldSeller.UserId, newSeller.UserId)
 				require.NoError(t, err)
 			})
-		})
 
+			for _, item := range items {
+				newItem, err := queries.GetItemWithId(db, item.ItemID)
+				require.NoError(t, err)
+
+				require.Equal(t, item.CategoryID, newItem.CategoryID)
+				require.Equal(t, item.Charity, newItem.Charity)
+				require.Equal(t, item.Description, newItem.Description)
+				require.Equal(t, item.Donation, newItem.Donation)
+				require.Equal(t, newSeller, newItem.SellerID)
+				require.Equal(t, item.Frozen, newItem.Frozen)
+				require.Equal(t, item.Hidden, newItem.Hidden)
+				require.Equal(t, item.PriceInCents, newItem.PriceInCents)
+			}
+		})
 	})
 
 	t.Run("Failure", func(t *testing.T) {
