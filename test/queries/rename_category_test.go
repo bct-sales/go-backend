e83ae534e3cef4fc943c74f87c668c26b2e3d433
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-10-02 16:58:32 +0200

    rename category tests

---
+++ test/queries/rename_category_test.go
@@ -0,0 +1,94 @@
+//go:build test
+
+package queries
+
+import (
+	dberr "bctbackend/database/errors"
+	"bctbackend/database/models"
+	"bctbackend/database/queries"
+	. "bctbackend/test/setup"
+
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRenameCategory(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		t.Run("Only one category in existence", func(t *testing.T) {
+			setup, db := NewDatabaseFixture()
+			defer setup.Close()
+
+			categoryId := models.Id(1)
+			newName := "bar"
+			setup.Category(categoryId, "foo")
+
+			err := queries.RenameCategory(db, categoryId, newName)
+			require.NoError(t, err)
+
+			categories, err := queries.GetCategories(db)
+			require.NoError(t, err)
+
+			require.Len(t, categories, 1)
+			require.Equal(t, categoryId, categories[0].CategoryID)
+			require.Equal(t, newName, categories[0].Name)
+		})
+
+		t.Run("Multiple categories in existence", func(t *testing.T) {
+			setup, db := NewDatabaseFixture()
+			defer setup.Close()
+
+			setup.Category(1, "foo")
+			setup.Category(2, "bar")
+			setup.Category(3, "baz")
+			setup.Category(4, "qux")
+
+			err := queries.RenameCategory(db, 3, "bazzie")
+			require.NoError(t, err)
+
+			actualCategories, err := queries.GetCategories(db)
+			require.NoError(t, err)
+
+			require.Len(t, actualCategories, 4)
+
+			expectedCategories := []*models.ItemCategory{
+				&models.ItemCategory{CategoryID: 1, Name: "foo"},
+				&models.ItemCategory{CategoryID: 2, Name: "bar"},
+				&models.ItemCategory{CategoryID: 3, Name: "bazzie"},
+				&models.ItemCategory{CategoryID: 4, Name: "qux"},
+			}
+			require.ElementsMatch(t, expectedCategories, actualCategories)
+		})
+	})
+
+	t.Run("Failure", func(t *testing.T) {
+		t.Run("Invalid name", func(t *testing.T) {
+			setup, db := NewDatabaseFixture()
+			defer setup.Close()
+
+			setup.Category(1, "foo")
+
+			err := queries.RenameCategory(db, 1, "")
+			requireDatabaseWrappedError(t, err, dberr.ErrInvalidCategoryName)
+		})
+
+		t.Run("Duplicate name", func(t *testing.T) {
+			setup, db := NewDatabaseFixture()
+			defer setup.Close()
+
+			setup.Category(1, "foo")
+			setup.Category(1, "bar")
+
+			err := queries.RenameCategory(db, 2, "foo")
+			requireDatabaseWrappedError(t, err, dberr.ErrDuplicateCategoryName)
+		})
+
+		t.Run("Nonexistent category id", func(t *testing.T) {
+			setup, db := NewDatabaseFixture()
+			defer setup.Close()
+
+			err := queries.RenameCategory(db, 1, "foo")
+			requireDatabaseWrappedError(t, err, dberr.ErrNoSuchCategory)
+		})
+	})
+}
