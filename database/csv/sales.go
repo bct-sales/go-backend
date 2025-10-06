a7aac137a6127a1ed12e2eef84dd38df6ab40bfd
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-10-06 11:22:37 +0200

    FormatSoldItemsAsCSV

---
+++ database/csv/sales.go
@@ -0,0 +1,56 @@
+package csv
+
+import (
+	models "bctbackend/database/models"
+	"bctbackend/database/queries"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"strconv"
+)
+
+func FormatSoldItemsAsCSV(soldItems []*queries.SoldItem, categoryNameTable map[models.Id]string, writer io.Writer) error {
+	csvWriter := csv.NewWriter(writer)
+	defer csvWriter.Flush()
+
+	headers := []string{"sale_id", "cashier_id", "transaction_time", "item_id", "description", "price_in_cents", "item_category", "seller_id", "donation", "charity"}
+	err := csvWriter.Write(headers)
+	if err != nil {
+		return fmt.Errorf("failed to write headers: %w", err)
+	}
+
+	for _, soldItem := range soldItems {
+		saleIdStr := soldItem.SaleId.String()
+		cashierIdStr := soldItem.CashierId.String()
+		transactionTimeStr := soldItem.TransactionTime.FormattedDateTime()
+		itemIdStr := soldItem.ItemId.String()
+		descriptionStr := soldItem.Description
+		priceInCentsStr := soldItem.PriceInCents.String()
+		itemCategoryStr, ok := categoryNameTable[soldItem.ItemCategory]
+		if !ok {
+			return fmt.Errorf("unknown category id: %v", soldItem.ItemCategory)
+		}
+		sellerIdStr := soldItem.SellerId.String()
+		donationStr := strconv.FormatBool(soldItem.Donation)
+		charityStr := strconv.FormatBool(soldItem.Charity)
+
+		err = csvWriter.Write([]string{
+			saleIdStr,
+			cashierIdStr,
+			transactionTimeStr,
+			itemIdStr,
+			descriptionStr,
+			priceInCentsStr,
+			itemCategoryStr,
+			sellerIdStr,
+			donationStr,
+			charityStr,
+		})
+
+		if err != nil {
+			return fmt.Errorf("failed to write row: %w", err)
+		}
+	}
+
+	return nil
+}
