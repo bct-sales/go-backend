d6a4ff254c0b4a372f8f395d9dd0269be68cbf12
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-10-06 18:03:31 +0200

    closes #601

--- test/queries/does_seller_have_frozen_items_test.go
+++ test/queries/does_seller_have_frozen_items_test.go
@@ -15,19 +15,19 @@ import (
 
 func TestDoesSellerHaveFrozenItems(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
-		// t.Run("No frozen items", func(t *testing.T) {
-		// 	setup, _ := NewDatabaseFixture(WithDefaultCategories)
-		// 	defer setup.Close()
+		t.Run("No frozen items", func(t *testing.T) {
+			setup, _ := NewDatabaseFixture(WithDefaultCategories)
+			defer setup.Close()
 
-		// 	seller := setup.Seller()
-		// 	setup.Items(seller.UserId, 10, aux.WithFrozen(false), aux.WithHidden(false))
+			seller := setup.Seller()
+			setup.Items(seller.UserID, 10, aux.WithFrozen(false), aux.WithHidden(false))
 
-		// 	setup.WithTransaction(t, func(db *queries.TransactionalDatabaseQuerier) {
-		// 		result, err := queries.DoesSellerHaveFrozenItems(db, seller.UserId)
-		// 		require.NoError(t, err)
-		// 		require.False(t, result)
-		// 	})
-		// })
+			setup.WithTransaction(t, func(db *queries.TransactionalDatabaseQuerier) {
+				result, err := queries.DoesSellerHaveFrozenItems(db, seller.UserID)
+				require.NoError(t, err)
+				require.False(t, result)
+			})
+		})
 
 		t.Run("Seller has one frozen item", func(t *testing.T) {
 			setup, _ := NewDatabaseFixture(WithDefaultCategories)
@@ -44,55 +44,55 @@ func TestDoesSellerHaveFrozenItems(t *testing.T) {
 			})
 		})
 
-		// t.Run("Seller has nothing but frozen items", func(t *testing.T) {
-		// 	setup, _ := NewDatabaseFixture(WithDefaultCategories)
-		// 	defer setup.Close()
+		t.Run("Seller has nothing but frozen items", func(t *testing.T) {
+			setup, _ := NewDatabaseFixture(WithDefaultCategories)
+			defer setup.Close()
 
-		// 	seller := setup.Seller()
-		// 	setup.Items(seller.UserId, 10, aux.WithFrozen(true), aux.WithHidden(false))
+			seller := setup.Seller()
+			setup.Items(seller.UserID, 10, aux.WithFrozen(true), aux.WithHidden(false))
 
-		// 	setup.WithTransaction(t, func(db *queries.TransactionalDatabaseQuerier) {
-		// 		result, err := queries.DoesSellerHaveFrozenItems(db, seller.UserId)
-		// 		require.NoError(t, err)
-		// 		require.True(t, result)
-		// 	})
-		// })
+			setup.WithTransaction(t, func(db *queries.TransactionalDatabaseQuerier) {
+				result, err := queries.DoesSellerHaveFrozenItems(db, seller.UserID)
+				require.NoError(t, err)
+				require.True(t, result)
+			})
+		})
 
-		// t.Run("Other sellers has frozen items", func(t *testing.T) {
-		// 	setup, _ := NewDatabaseFixture(WithDefaultCategories)
-		// 	defer setup.Close()
+		t.Run("Other sellers has frozen items", func(t *testing.T) {
+			setup, _ := NewDatabaseFixture(WithDefaultCategories)
+			defer setup.Close()
 
-		// 	otherSeller1 := setup.Seller()
-		// 	seller := setup.Seller()
-		// 	otherSeller2 := setup.Seller()
-		// 	setup.Items(otherSeller1.UserId, 10, aux.WithFrozen(true), aux.WithHidden(false))
-		// 	setup.Items(seller.UserId, 10, aux.WithFrozen(false), aux.WithHidden(false))
-		// 	setup.Items(otherSeller2.UserId, 10, aux.WithFrozen(true), aux.WithHidden(false))
+			otherSeller1 := setup.Seller()
+			seller := setup.Seller()
+			otherSeller2 := setup.Seller()
+			setup.Items(otherSeller1.UserID, 10, aux.WithFrozen(true), aux.WithHidden(false))
+			setup.Items(seller.UserID, 10, aux.WithFrozen(false), aux.WithHidden(false))
+			setup.Items(otherSeller2.UserID, 10, aux.WithFrozen(true), aux.WithHidden(false))
 
-		// 	setup.WithTransaction(t, func(db *queries.TransactionalDatabaseQuerier) {
-		// 		result, err := queries.DoesSellerHaveFrozenItems(db, seller.UserId)
-		// 		require.NoError(t, err)
-		// 		require.False(t, result)
-		// 	})
-		// })
+			setup.WithTransaction(t, func(db *queries.TransactionalDatabaseQuerier) {
+				result, err := queries.DoesSellerHaveFrozenItems(db, seller.UserID)
+				require.NoError(t, err)
+				require.False(t, result)
+			})
+		})
 
-		// t.Run("Seller has frozen items, other sellers don't", func(t *testing.T) {
-		// 	setup, _ := NewDatabaseFixture(WithDefaultCategories)
-		// 	defer setup.Close()
+		t.Run("Seller has frozen items, other sellers don't", func(t *testing.T) {
+			setup, _ := NewDatabaseFixture(WithDefaultCategories)
+			defer setup.Close()
 
-		// 	otherSeller1 := setup.Seller()
-		// 	seller := setup.Seller()
-		// 	otherSeller2 := setup.Seller()
-		// 	setup.Items(otherSeller1.UserId, 10, aux.WithFrozen(false), aux.WithHidden(false))
-		// 	setup.Items(seller.UserId, 10, aux.WithFrozen(true), aux.WithHidden(false))
-		// 	setup.Items(otherSeller2.UserId, 10, aux.WithFrozen(false), aux.WithHidden(false))
+			otherSeller1 := setup.Seller()
+			seller := setup.Seller()
+			otherSeller2 := setup.Seller()
+			setup.Items(otherSeller1.UserID, 10, aux.WithFrozen(false), aux.WithHidden(false))
+			setup.Items(seller.UserID, 10, aux.WithFrozen(true), aux.WithHidden(false))
+			setup.Items(otherSeller2.UserID, 10, aux.WithFrozen(false), aux.WithHidden(false))
 
-		// 	setup.WithTransaction(t, func(db *queries.TransactionalDatabaseQuerier) {
-		// 		result, err := queries.DoesSellerHaveFrozenItems(db, seller.UserId)
-		// 		require.NoError(t, err)
-		// 		require.False(t, result)
-		// 	})
-		// })
+			setup.WithTransaction(t, func(db *queries.TransactionalDatabaseQuerier) {
+				result, err := queries.DoesSellerHaveFrozenItems(db, seller.UserID)
+				require.NoError(t, err)
+				require.True(t, result)
+			})
+		})
 	})
 
 	t.Run("Failure", func(t *testing.T) {
