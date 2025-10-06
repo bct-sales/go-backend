8f71de7b945e55f2d1855e4526dabda01dcbe58d
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-10-06 15:34:34 +0200

    ListSoldItems endpoint test

--- test/rest/list_sold_items_test.go
+++ test/rest/list_sold_items_test.go
@@ -6,8 +6,10 @@ import (
 	"net/http"
 	"testing"
 
+	"bctbackend/database/models"
 	path "bctbackend/server/paths"
 	"bctbackend/server/rest"
+	util "bctbackend/server/shared"
 	aux "bctbackend/test/helpers"
 	. "bctbackend/test/setup"
 
@@ -32,6 +34,42 @@ func TestListSoldItems(t *testing.T) {
 			actual := FromJson[rest.GetSoldItemsSuccessResponse](t, writer.Body.String())
 			expected := &rest.GetSoldItemsSuccessResponse{
 				SoldItems: []rest.GetSoldItemsEntry{},
+			}
+			require.Equal(t, expected, actual)
+		})
+		t.Run("Single sale with single item", func(t *testing.T) {
+			setup, router, writer := NewRestFixture(WithDefaultCategories)
+			defer setup.Close()
+
+			_, sessionId := setup.LoggedIn(setup.Admin())
+			seller := setup.Seller()
+			cashier := setup.Cashier()
+			items := setup.Items(seller.UserId, 5, aux.WithHidden(false))
+			soldItem := items[0]
+			sale := setup.Sale(cashier.UserId, []models.Id{soldItem.ItemID})
+
+			url := path.SoldItems()
+			request := CreateGetRequest(url, WithSessionCookie(sessionId))
+			router.ServeHTTP(writer, request)
+			require.Equal(t, http.StatusOK, writer.Code)
+
+			actual := FromJson[rest.GetSoldItemsSuccessResponse](t, writer.Body.String())
+			expected := &rest.GetSoldItemsSuccessResponse{
+				SoldItems: []rest.GetSoldItemsEntry{
+					{
+						SaleId:          sale.SaleID,
+						CashierId:       sale.CashierID,
+						TransactionTime: util.ConvertTimestampToDateTime(sale.TransactionTime),
+						ItemId:          soldItem.ItemID,
+						AddedAt:         util.ConvertTimestampToDateTime(soldItem.AddedAt),
+						Description:     soldItem.Description,
+						PriceInCents:    soldItem.PriceInCents,
+						ItemCategory:    soldItem.CategoryID,
+						SellerId:        soldItem.SellerID,
+						Donation:        soldItem.Donation,
+						Charity:         soldItem.Charity,
+					},
+				},
 			}
 			require.Equal(t, expected, actual)
 		})
