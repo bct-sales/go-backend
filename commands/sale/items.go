3bc169b4525f89edacc3ef4705eb85cd9c2d9379
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-10-06 12:56:04 +0200

    sale items command: csv format option

--- commands/sale/items.go
+++ commands/sale/items.go
@@ -2,9 +2,11 @@ package sale
 
 import (
 	"bctbackend/commands/common"
+	dbcsv "bctbackend/database/csv"
 	"bctbackend/database/queries"
 	"database/sql"
 	"fmt"
+	"os"
 	"strconv"
 
 	"github.com/pterm/pterm"
@@ -13,13 +15,14 @@ import (
 
 type ListSoldItemsCommand struct {
 	common.Command
+	format string
 }
 
 func NewListSoldItemsCommand() *cobra.Command {
 	var command *ListSoldItemsCommand
 
 	command = &ListSoldItemsCommand{
-		common.Command{
+		Command: common.Command{
 			CobraCommand: &cobra.Command{
 				Use:   "items",
 				Short: "List all sold items",
@@ -30,90 +33,127 @@ func NewListSoldItemsCommand() *cobra.Command {
 				Args: cobra.NoArgs,
 			},
 		},
+		format: "table",
 	}
+
+	command.CobraCommand.Flags().StringVar(&command.format, "format", "table", "Output format (format, csv)")
 
 	return command.AsCobraCommand()
 }
 
 func (command *ListSoldItemsCommand) execute() error {
 	return command.WithOpenedDatabase(func(db *sql.DB) error {
-		categoryNameTable, err := command.GetCategoryNameTable(db)
-		if err != nil {
-			return err
+		switch command.format {
+		case "table":
+			return command.listSoldItemsInTableFormat(db)
+		case "csv":
+			return command.listSoldItemsInCsvFormat(db)
+		default:
+			command.PrintErrorf("Invalid format: %s\n", command.format)
+			return fmt.Errorf("unknown format: %s", command.format)
 		}
-
-		tableData := pterm.TableData{
-			{
-				"Sale ID",
-				"Cashier ID",
-				"Transaction Time",
-				"Item ID",
-				"Description",
-				"Price",
-				"Item Category",
-				"Seller ID",
-				"Donation",
-				"Charity",
-			},
-		}
-
-		rowCount := 0
-		addToTable := func(soldItem *queries.SoldItem) error {
-			saleIdStr := soldItem.SaleId.String()
-			cashierIdStr := soldItem.CashierId.String()
-			transactionTimeStr := soldItem.TransactionTime.FormattedDateTime()
-			itemIdStr := soldItem.ItemId.String()
-			descriptionStr := soldItem.Description
-			priceStr := soldItem.PriceInCents.DecimalNotation()
-			itemCategoryStr, ok := categoryNameTable[soldItem.ItemCategory]
-			if !ok {
-				return fmt.Errorf("unknown category id: %v", soldItem.ItemCategory)
-			}
-			sellerIdStr := soldItem.SellerId.String()
-			donationStr := strconv.FormatBool(soldItem.Donation)
-			charityStr := strconv.FormatBool(soldItem.Charity)
-
-			tableData = append(tableData, []string{
-				saleIdStr,
-				cashierIdStr,
-				transactionTimeStr,
-				itemIdStr,
-				descriptionStr,
-				priceStr,
-				itemCategoryStr,
-				sellerIdStr,
-				donationStr,
-				charityStr,
-			})
-
-			rowCount++
-
-			return nil
-		}
-
-		soldItems, err := queries.NewGetSoldItemsQuery().Execute(db)
-		if err != nil {
-			command.PrintErrorf("Error while listing sales\n")
-			return fmt.Errorf("error while listing sales: %w", err)
-		}
-		for _, soldItem := range soldItems {
-			if err := addToTable(soldItem); err != nil {
-				command.PrintErrorf("Error while listing sales\n")
-				return fmt.Errorf("error while listing sales: %w", err)
-			}
-		}
-
-		if rowCount == 0 {
-			command.Printf("No sold items found\n")
-			return nil
-		}
-
-		if err := pterm.DefaultTable.WithHasHeader().WithHeaderRowSeparator("-").WithData(tableData).Render(); err != nil {
-			command.PrintErrorf("Error while rendering table\n")
-			return fmt.Errorf("error while rendering table: %w", err)
-		}
-
-		command.Printf("Number of sales listed: %d\n", rowCount)
-		return nil
 	})
+}
+
+func (command *ListSoldItemsCommand) listSoldItemsInCsvFormat(db *sql.DB) error {
+	categoryNameTable, err := command.GetCategoryNameTable(db)
+	if err != nil {
+		return err
+	}
+
+	soldItems, err := queries.NewGetSoldItemsQuery().Execute(db)
+	if err != nil {
+		command.PrintErrorf("Error while listing sales\n")
+		return fmt.Errorf("error while listing sales: %w", err)
+	}
+
+	dbcsv.FormatSoldItemsAsCSV(soldItems, categoryNameTable, os.Stdout)
+	if err != nil {
+		command.PrintErrorf("Error while formatting sold items as CSV\n")
+		return fmt.Errorf("failed to format sold items as a CSV: %w", err)
+	}
+
+	return nil
+}
+
+func (command *ListSoldItemsCommand) listSoldItemsInTableFormat(db *sql.DB) error {
+	categoryNameTable, err := command.GetCategoryNameTable(db)
+	if err != nil {
+		return err
+	}
+
+	tableData := pterm.TableData{
+		{
+			"Sale ID",
+			"Cashier ID",
+			"Transaction Time",
+			"Item ID",
+			"Description",
+			"Price",
+			"Item Category",
+			"Seller ID",
+			"Donation",
+			"Charity",
+		},
+	}
+
+	rowCount := 0
+	addToTable := func(soldItem *queries.SoldItem) error {
+		saleIdStr := soldItem.SaleId.String()
+		cashierIdStr := soldItem.CashierId.String()
+		transactionTimeStr := soldItem.TransactionTime.FormattedDateTime()
+		itemIdStr := soldItem.ItemId.String()
+		descriptionStr := soldItem.Description
+		priceStr := soldItem.PriceInCents.DecimalNotation()
+		itemCategoryStr, ok := categoryNameTable[soldItem.ItemCategory]
+		if !ok {
+			return fmt.Errorf("unknown category id: %v", soldItem.ItemCategory)
+		}
+		sellerIdStr := soldItem.SellerId.String()
+		donationStr := strconv.FormatBool(soldItem.Donation)
+		charityStr := strconv.FormatBool(soldItem.Charity)
+
+		tableData = append(tableData, []string{
+			saleIdStr,
+			cashierIdStr,
+			transactionTimeStr,
+			itemIdStr,
+			descriptionStr,
+			priceStr,
+			itemCategoryStr,
+			sellerIdStr,
+			donationStr,
+			charityStr,
+		})
+
+		rowCount++
+
+		return nil
+	}
+
+	soldItems, err := queries.NewGetSoldItemsQuery().Execute(db)
+	if err != nil {
+		command.PrintErrorf("Error while listing sold items\n")
+		return fmt.Errorf("error while listing sold items: %w", err)
+	}
+
+	for _, soldItem := range soldItems {
+		if err := addToTable(soldItem); err != nil {
+			command.PrintErrorf("Error while listing sold items\n")
+			return fmt.Errorf("error while listing sold items: %w", err)
+		}
+	}
+
+	if rowCount == 0 {
+		command.Printf("No sold items found\n")
+		return nil
+	}
+
+	if err := pterm.DefaultTable.WithHasHeader().WithHeaderRowSeparator("-").WithData(tableData).Render(); err != nil {
+		command.PrintErrorf("Error while rendering table\n")
+		return fmt.Errorf("error while rendering table: %w", err)
+	}
+
+	command.Printf("Number of sales listed: %d\n", rowCount)
+	return nil
 }
