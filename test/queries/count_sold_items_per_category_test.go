6dcc5ddabe6cb647bdb2cecc3446ef235d145499
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-10-07 11:35:06 +0200

    CountSoldItemsPerCategory tests

--- test/queries/count_sold_items_per_category_test.go
+++ test/queries/count_sold_items_per_category_test.go
@@ -54,5 +54,54 @@ func TestCountSoldItemsPerCategory(t *testing.T) {
 			require.Equal(t, 1, actualCounts[categoryID1])
 			require.Equal(t, 0, actualCounts[categoryID2])
 		})
+
+		t.Run("Two sold items in same category", func(t *testing.T) {
+			setup, db := NewDatabaseFixture()
+			defer setup.Close()
+
+			categoryID1 := models.ID(1)
+			categoryID2 := models.ID(2)
+
+			seller := setup.Seller()
+			cashier := setup.Cashier()
+			setup.Category(categoryID1, "foo")
+			setup.Category(categoryID2, "bar")
+			setup.Items(seller.UserID, 10, aux.WithItemCategory(categoryID1), aux.WithHidden(false))
+			soldItem1 := setup.Item(seller.UserID, aux.WithHidden(false), aux.WithItemCategory(categoryID1))
+			soldItem2 := setup.Item(seller.UserID, aux.WithHidden(false), aux.WithItemCategory(categoryID1))
+			setup.Sale(cashier.UserID, []models.ID{soldItem1.ItemID, soldItem2.ItemID})
+
+			actualCounts, err := queries.CountSoldItemsPerCategory(db)
+			require.NoError(t, err)
+			require.Equal(t, len(actualCounts), 2)
+			require.Equal(t, 2, actualCounts[categoryID1])
+			require.Equal(t, 0, actualCounts[categoryID2])
+		})
+
+		t.Run("Two sold items in different categories", func(t *testing.T) {
+			setup, db := NewDatabaseFixture()
+			defer setup.Close()
+
+			categoryID1 := models.ID(1)
+			categoryID2 := models.ID(2)
+			categoryID3 := models.ID(3)
+
+			seller := setup.Seller()
+			cashier := setup.Cashier()
+			setup.Category(categoryID1, "foo")
+			setup.Category(categoryID2, "bar")
+			setup.Category(categoryID3, "baz")
+			setup.Items(seller.UserID, 10, aux.WithItemCategory(categoryID1), aux.WithHidden(false))
+			soldItem1 := setup.Item(seller.UserID, aux.WithHidden(false), aux.WithItemCategory(categoryID1))
+			soldItem2 := setup.Item(seller.UserID, aux.WithHidden(false), aux.WithItemCategory(categoryID2))
+			setup.Sale(cashier.UserID, []models.ID{soldItem1.ItemID, soldItem2.ItemID})
+
+			actualCounts, err := queries.CountSoldItemsPerCategory(db)
+			require.NoError(t, err)
+			require.Equal(t, len(actualCounts), 3)
+			require.Equal(t, 1, actualCounts[categoryID1])
+			require.Equal(t, 1, actualCounts[categoryID2])
+			require.Equal(t, 0, actualCounts[categoryID3])
+		})
 	})
 }
