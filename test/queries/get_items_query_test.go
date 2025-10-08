03cc5a952e0361da22c8ae4f9a6fc30234bb1fe9
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-10-08 23:11:09 +0200

    GetItemsQuery test

--- test/queries/get_items_query_test.go
+++ test/queries/get_items_query_test.go
@@ -168,7 +168,7 @@ func TestGetItemsQuery(t *testing.T) {
 			}
 		})
 
-		t.Run("Get items with description", func(t *testing.T) {
+		t.Run("Get items with description containing a", func(t *testing.T) {
 			setup, db := NewDatabaseFixture(WithDefaultCategories)
 			defer setup.Close()
 
@@ -185,6 +185,27 @@ func TestGetItemsQuery(t *testing.T) {
 			require.Equal(t, 2, len(actualItems))
 			require.Equal(t, item2.ItemID, actualItems[0].ItemID)
 			require.Equal(t, item3.ItemID, actualItems[1].ItemID)
+		})
+
+		t.Run("Get items with description containing a space", func(t *testing.T) {
+			setup, db := NewDatabaseFixture(WithDefaultCategories)
+			defer setup.Close()
+
+			seller := setup.Seller()
+			setup.Item(seller.UserID, aux.WithDescription("foo"), aux.WithHidden(false))
+			item2 := setup.Item(seller.UserID, aux.WithDescription("b ar"), aux.WithHidden(false))
+			item3 := setup.Item(seller.UserID, aux.WithDescription("ba z"), aux.WithHidden(false))
+			setup.Item(seller.UserID, aux.WithDescription("qux"), aux.WithHidden(false))
+			item5 := setup.Item(seller.UserID, aux.WithDescription("q u x"), aux.WithHidden(false))
+
+			actualItems := []*models.Item{}
+			query := queries.NewGetItemsQuery()
+			query.WithDescriptionPattern("% %")
+			require.NoError(t, query.Execute(db, queries.CollectTo(&actualItems)))
+			require.Equal(t, 3, len(actualItems))
+			require.Equal(t, item2.ItemID, actualItems[0].ItemID)
+			require.Equal(t, item3.ItemID, actualItems[1].ItemID)
+			require.Equal(t, item5.ItemID, actualItems[2].ItemID)
 		})
 	})
 
