76b16a4f4a2e55d73eda4e9ad33d2a64c792ebdd
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-10-21 00:01:33 +0200

    closes #657

--- database/csv/items.go
+++ database/csv/items.go
@@ -5,13 +5,14 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
+	"strconv"
 )
 
 func FormatItemsAsCSV(items []*models.Item, categoryNameTable map[models.ID]string, writer io.Writer) error {
 	csvWriter := csv.NewWriter(writer)
 	defer csvWriter.Flush()
 
-	headers := []string{"item_id", "seller_id", "description", "category", "price_in_cents", "donation", "charity"}
+	headers := []string{"item_id", "seller_id", "description", "category", "price_in_cents", "donation", "charity", "large"}
 	err := csvWriter.Write(headers)
 	if err != nil {
 		return fmt.Errorf("failed to write headers: %w", err)
@@ -27,19 +28,9 @@ func FormatItemsAsCSV(items []*models.Item, categoryNameTable map[models.ID]stri
 			return fmt.Errorf("unknown category id: %v", item.CategoryID)
 		}
 
-		var donationString string
-		if item.Donation {
-			donationString = "true"
-		} else {
-			donationString = "false"
-		}
-
-		var charityString string
-		if item.Charity {
-			charityString = "true"
-		} else {
-			charityString = "false"
-		}
+		donationString := strconv.FormatBool(item.Donation)
+		charityString := strconv.FormatBool(item.Charity)
+		largeString := strconv.FormatBool(item.Large)
 
 		err = csvWriter.Write([]string{
 			itemIDString,
@@ -49,6 +40,7 @@ func FormatItemsAsCSV(items []*models.Item, categoryNameTable map[models.ID]stri
 			priceString,
 			donationString,
 			charityString,
+			largeString,
 		})
 
 		if err != nil {
