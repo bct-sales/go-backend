4cb78d3d98f914f20db862b1e4c9d9b4d412606d
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-10-21 10:59:39 +0200

    WithDummyData update

--- test/helpers/add_item.go
+++ test/helpers/add_item.go
@@ -131,7 +131,7 @@ func WithDummyData(k int) func(*AddItemData) {
 		itemCategory := defaultCategoryIDs[k%len(defaultCategoryIDs)]
 		donation := k%2 == 0
 		charity := k%3 == 0
-		large := k%4 == 0
+		large := k%2 == 0
 		frozen := k%2 == 0
 
 		data.AddedAt = &addedAt
