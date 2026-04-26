358b1c9fa2ac0adfa69ff6c90712b69a9b5cda2b
Frederic Vogels <frederic.vogels@gmail.com>
2026-04-26 15:35:08 +0200

    extra GetCategories tests

--- test/queries/get_categories_test.go
+++ test/queries/get_categories_test.go
@@ -11,17 +11,35 @@ import (
 )
 
 func TestGetCategories(t *testing.T) {
-	setup, db := NewDatabaseFixture()
-	defer setup.Close()
+	t.Run("1/2/3", func(t *testing.T) {
+		setup, db := NewDatabaseFixture()
+		defer setup.Close()
 
-	setup.Category(1, "Alpha")
-	setup.Category(2, "Beta")
-	setup.Category(3, "Gamma")
+		setup.Category(1, "Alpha")
+		setup.Category(2, "Beta")
+		setup.Category(3, "Gamma")
 
-	actualCategories, err := queries.GetCategories(db)
-	require.NoError(t, err)
-	require.Equal(t, 3, len(actualCategories))
-	require.Equal(t, "Alpha", actualCategories[0].Name)
-	require.Equal(t, "Beta", actualCategories[1].Name)
-	require.Equal(t, "Gamma", actualCategories[2].Name)
+		actualCategories, err := queries.GetCategories(db)
+		require.NoError(t, err)
+		require.Equal(t, 3, len(actualCategories))
+		require.Equal(t, "Alpha", actualCategories[0].Name)
+		require.Equal(t, "Beta", actualCategories[1].Name)
+		require.Equal(t, "Gamma", actualCategories[2].Name)
+	})
+
+	t.Run("3/2/1", func(t *testing.T) {
+		setup, db := NewDatabaseFixture()
+		defer setup.Close()
+
+		setup.Category(3, "Alpha")
+		setup.Category(2, "Beta")
+		setup.Category(1, "Gamma")
+
+		actualCategories, err := queries.GetCategories(db)
+		require.NoError(t, err)
+		require.Equal(t, 3, len(actualCategories))
+		require.Equal(t, "Gamma", actualCategories[0].Name)
+		require.Equal(t, "Beta", actualCategories[1].Name)
+		require.Equal(t, "Alpha", actualCategories[2].Name)
+	})
 }
